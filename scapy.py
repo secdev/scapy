91fdcabedffe73a03f01d0a54569a9eab0316b15
Phil <phil@secdev.org>
2007-06-04 14:18:01 +0200

    Fixed checksum() computation for big endian architecture!

--- scapy.py
+++ scapy.py
@@ -673,14 +673,24 @@ def hexdiff(x,y):
     
 crc32 = zlib.crc32
 
-def checksum(pkt):
-    if len(pkt) % 2 == 1:
-        pkt += "\0"
-    s = sum(array.array("H", pkt))
-    s = (s >> 16) + (s & 0xffff)
-    s += s >> 16
-    s = ~s
-    return (((s>>8)&0xff)|s<<8) & 0xffff
+if BIG_ENDIAN:
+    def checksum(pkt):
+        if len(pkt) % 2 == 1:
+            pkt += "\0"
+        s = sum(array.array("H", pkt))
+        s = (s >> 16) + (s & 0xffff)
+        s += s >> 16
+        s = ~s
+        return s & 0xffff
+else:
+    def checksum(pkt):
+        if len(pkt) % 2 == 1:
+            pkt += "\0"
+        s = sum(array.array("H", pkt))
+        s = (s >> 16) + (s & 0xffff)
+        s += s >> 16
+        s = ~s
+        return (((s>>8)&0xff)|s<<8) & 0xffff
 
 def warning(x):
     log_runtime.warning(x)
