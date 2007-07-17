a6f82c823e9a8eda6aad5078efacc992aa72cfb0
Phil <phil@secdev.org>
2007-07-17 18:45:29 +0200

    Added RTP protocol (from N. Bareil patch, ticket #45)

--- scapy.py
+++ scapy.py
@@ -7551,9 +7551,35 @@ class Skinny(Packet):
     fields_desc = [ LEIntField("len",0),
                     LEIntField("res",0),
                     LEIntEnumField("msg",0,skinny_messages) ]
+
+_rtp_payload_types = {
+    # http://www.iana.org/assignments/rtp-parameters
+    0:  'G.711 PCMU',    3:  'GSM',
+    4:  'G723',    5:  'DVI4',
+    6:  'DVI4',    7:  'LPC',
+    8:  'PCMA',    9:  'G722',
+    10: 'L16',     11: 'L16',
+    12: 'QCELP',   13: 'CN',
+    14: 'MPA',     15: 'G728',
+    16: 'DVI4',    17: 'DVI4',
+    18: 'G729',    25: 'CelB',
+    26: 'JPEG',    28: 'nv',
+    31: 'H261',    32: 'MPV',
+    33: 'MP2T',    34: 'H263' }
+
+class RTP(Packet):
+    name="RTP"
+    fields_desc = [ BitField('version', 2, 2),
+                    BitField('padding', 0, 1),
+                    BitField('extension', 0, 1),
+                    BitFieldLenField('numsync', None, 4, count_of='sync'),
+                    BitField('marker', 0, 1),
+                    BitEnumField('payload', 0, 7, _rtp_payload_types),
+                    ShortField('sequence', 0),
+                    IntField('timestamp', 0),
+                    IntField('sourcesync', 0),
+                    FieldListField('sync', [], IntField("id",0), count_from=lambda pkt:pkt.numsync) ]
     
-
-
 ### SEBEK
 
 
@@ -11767,7 +11793,7 @@ def voip_play3(lst=None,**kargs):
     try:
         def play(pkt, dsp=dsp):
             if pkt and pkt.haslayer(UDP) and pkt.haslayer(Raw):
-                dsp.write(pkt.getlayer(Raw).load[12:])
+                dsp.write(pkt.getlayer(RTP).load)
         if lst is None:
             sniff(store=0, prn=play, **kargs)
         else:
