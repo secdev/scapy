d496fa6b46ca8aef80962d60397c814f4ef44cd6
Phil <phil@secdev.org>
2008-07-01 17:46:30 +0200

    Added "all" module for scapy and scapy.layers

---
+++ scapy/all.py
@@ -0,0 +1,34 @@
+
+from config import *
+from dadict import *
+from data import *
+from error import *
+from themes import *
+from arch import *
+
+from plist import *
+from fields import *
+from packet import *
+from asn1fields import *
+from asn1packet import *
+
+from utils import *
+from route import *
+from sendrecv import *
+from supersocket import *
+from volatile import *
+
+from ansmachine import *
+from automaton import *
+from autorun import *
+
+from main import *
+
+from layers.all import *
+
+from asn1.asn1 import *
+from asn1.ber import *
+from asn1.mib import *
+
+
+
