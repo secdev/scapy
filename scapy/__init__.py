be0f1bdb1acd309370682867eac9ac8fac06eeb9
Phil <phil@secdev.org>
2008-07-28 16:13:01 +0200

    Small reorganisation of executables
    --HG--
    rename : UTscapy.py => bin/UTscapy
    rename : scapy.py => bin/scapy

--- scapy/__init__.py
+++ scapy/__init__.py
@@ -2,3 +2,9 @@
 ## See http://www.secdev.org/projects/scapy for more informations
 ## Copyright (C) Philippe Biondi <phil@secdev.org>
 ## This program is published under a GPLv2 license
+
+
+
+if __name__ == "__main__":
+    from scapy.main import interact
+    interact()
