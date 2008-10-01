dd8930ad383e734ddadc5d3a911b9c4075616c3b
Phil <phil@secdev.org>
2008-10-01 19:07:08 +0200

    Added ColorOnBlackTheme (ticket #138, D. Loss)

--- scapy/themes.py
+++ scapy/themes.py
@@ -149,6 +149,30 @@ class RastaTheme(AnsiColorTheme):
     style_left = Color.yellow+Color.invert
     style_right = Color.red+Color.invert
 
+class ColorOnBlackTheme(AnsiColorTheme):
+    """Color theme for black backgrounds"""
+    style_normal = Color.normal
+    style_prompt = Color.green+Color.bold
+    style_punct = Color.normal
+    style_id = Color.green
+    style_not_printable = Color.black+Color.bold
+    style_layer_name = Color.yellow+Color.bold
+    style_field_name = Color.cyan
+    style_field_value = Color.purple+Color.bold
+    style_emph_field_name = Color.cyan+Color.bold
+    style_emph_field_value = Color.red+Color.bold
+    style_packetlist_name = Color.black+Color.bold
+    style_packetlist_proto = Color.yellow+Color.bold
+    style_packetlist_value = Color.purple+Color.bold
+    style_fail = Color.red+Color.bold
+    style_success = Color.green
+    style_even = Color.black+Color.bold
+    style_odd = Color.grey
+    style_opening = Color.yellow
+    style_active = Color.grey+Color.bold
+    style_closed = Color.black+Color.bold
+    style_left = Color.cyan+Color.bold
+    style_right = Color.red+Color.bold
 
 class FormatTheme(ColorTheme):
     def __getattr__(self, attr):
