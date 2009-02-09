0a4fdb8d721e405615ae45ed5031389e893be9c6
Phil <phil@secdev.org>
2009-02-09 13:40:35 +0100

    Open 2.0.1-dev

--- scapy/config.py
+++ scapy/config.py
@@ -264,7 +264,7 @@ noenum    : holds list of enum fields for which conversion to string should NOT 
 AS_resolver: choose the AS resolver class to use
 extensions_paths: path or list of paths where extensions are to be looked for
 """
-    version = "2.0.1"
+    version = "2.0.1-dev"
     session = ""
     interactive = False
     stealth = "not implemented"
