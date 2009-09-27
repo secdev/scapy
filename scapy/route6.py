e5b952e84731f0d703f435769867b236880ee818
Phil <phil@secdev.org>
2009-09-27 21:43:32 +0200

    Fixed missing argument to construct_source_candidate_set() (ticket #185)

--- scapy/route6.py
+++ scapy/route6.py
@@ -72,7 +72,7 @@ class Route6:
             # replace that unique address by the list of all addresses
             lifaddr = in6_getifaddr()             
             devaddrs = filter(lambda x: x[2] == dev, lifaddr)
-            ifaddr = construct_source_candidate_set(prefix, plen, devaddrs)
+            ifaddr = construct_source_candidate_set(prefix, plen, devaddrs, LOOPBACK_NAME)
 
         return (prefix, plen, gw, dev, ifaddr)
 
