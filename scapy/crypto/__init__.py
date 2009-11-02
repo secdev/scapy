351ccc7453fb77add075337ba9fa06196638345d
Arnaud Ebalard <arno@natisbad.org>
2009-11-02 22:09:11 +0100

    Added cert.py: X.509 Cert and Key module based on OpenSSL binary

---
+++ scapy/crypto/__init__.py
@@ -0,0 +1,6 @@
+## This file is part of Scapy
+## See http://www.secdev.org/projects/scapy for more informations
+## Copyright (C) Arnaud Ebalard <arno@natisbad.org>
+## This program is published under a GPLv2 license
+
+__all__ = ["cert"]
