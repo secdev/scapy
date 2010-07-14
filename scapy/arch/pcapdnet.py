d8c9326eb996da1d0fc9eff375e1b67c938aabf5
Dirk Loss <mail@dirk-loss.de>
2010-07-14 13:04:52 +0200

    Remove dependency on special 'scapy' string in pcap.__version__
    Newer versions of the pypcap library now include our fixes
    and support the timeout_ms argument. So we should not depend
    on our customized, specially marked, pypcap version anymore.

--- scapy/arch/pcapdnet.py
+++ scapy/arch/pcapdnet.py
@@ -43,11 +43,10 @@ if conf.use_pcap:
         if hasattr(pcap,"pcap"): # python-pypcap
             class _PcapWrapper_pypcap:
                 def __init__(self, device, snaplen, promisc, to_ms):
-                    # Normal pypcap module has no timeout parameter,
-                    # only the specially patched "scapy" variant has.                 
-                    if "scapy" in pcap.__version__.lower():
+                    try:
                         self.pcap = pcap.pcap(device, snaplen, promisc, immediate=1, timeout_ms=to_ms)
-                    else:
+                    except TypeError:
+                        # Older pypcap versions do not support the timeout_ms argument
                         self.pcap = pcap.pcap(device, snaplen, promisc, immediate=1)                    
                 def __getattr__(self, attr):
                     return getattr(self.pcap, attr)
