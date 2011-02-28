a624d23eb1393b87f0dfbccbaae6203b27964f5d
Phil <phil@secdev.org>
2011-02-28 02:03:30 +0100

    Added contrib MPLS (ticket #31)

---
+++ scapy/contrib/mpls.py
@@ -0,0 +1,13 @@
+# http://trac.secdev.org/scapy/ticket/31 
+
+# scapy.contrib.description = MPLS
+# scapy.contrib.status = loads
+
+class MPLS(Packet): 
+   name = "MPLS" 
+   fields_desc =  [ BitField("label", 3, 20), 
+                    BitField("cos", 0, 3), 
+                    BitField("s", 1, 1), 
+                    ByteField("ttl", 0)  ] 
+
+bind_layers(Ether, MPLS, type=0x8847)
