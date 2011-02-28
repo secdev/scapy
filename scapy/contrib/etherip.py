31675bd16f5a6329dd730462832ed3d69bbb599d
Phil <phil@secdev.org>
2011-02-28 02:43:55 +0100

    Added EtherIP as contrib (ticket #297)

---
+++ scapy/contrib/etherip.py
@@ -0,0 +1,14 @@
+
+# http://trac.secdev.org/scapy/ticket/297
+
+# scapy.contrib.description = EtherIP
+# scapy.contrib.status = loads
+
+class EtherIP(Packet):
+    name = "EtherIP / RFC 3378"
+    fields_desc = [ BitField("version", 3, 4),
+                    BitField("reserved", 0, 12)]
+
+bind_layers( IP,            EtherIP,       frag=0, proto=0x61)
+bind_layers( EtherIP,       Ether)
+
