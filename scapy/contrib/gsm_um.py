f6e92411bf0e7e4d00ce3bf2afd258834cfbff2e
Phil <phil@secdev.org>
2011-10-10 08:57:04 +0200

    Added contrib layer marker * * * c

--- scapy/contrib/gsm_um.py
+++ scapy/contrib/gsm_um.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# scapy.contrib.description = PPI
+# scapy.contrib.status = loads
+
 """
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
