968db34f519e2989e36029136436c0b9b314cebc
Guillaume Valadon <guillaume@valadon.net>
2014-02-24 16:20:55 +0100

    Typo in DNS types list
    --HG--
    branch : Issue #500

--- scapy/layers/dns.py
+++ scapy/layers/dns.py
@@ -250,7 +250,7 @@ class DNS(Packet):
         return 'DNS %s%s ' % (type, name)
 
 dnstypes = { 0:"ANY", 255:"ALL",
-             1:"A", 2:"NS", 3:"MD", 4:"MD", 5:"CNAME", 6:"SOA", 7: "MB", 8:"MG",
+             1:"A", 2:"NS", 3:"MD", 4:"MF", 5:"CNAME", 6:"SOA", 7: "MB", 8:"MG",
              9:"MR",10:"NULL",11:"WKS",12:"PTR",13:"HINFO",14:"MINFO",15:"MX",16:"TXT",
              17:"RP",18:"AFSDB",28:"AAAA", 33:"SRV",38:"A6",39:"DNAME",
              41:"OPT", 43:"DS", 46:"RRSIG", 47:"NSEC", 48:"DNSKEY",
