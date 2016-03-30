0ff53fd170d9f6818ba7dd43c0fa64049bd5cc9b
Guillaume Valadon <guillaume@valadon.net>
2016-03-30 15:07:05 +0200

    Functions from the unix module can be imported without importing pcapdnet
    Replacing | with or
    
    Useless line break

--- scapy/arch/__init__.py
+++ scapy/arch/__init__.py
@@ -40,7 +40,6 @@ def str2mac(s):
     return ("%02x:"*6)[:-1] % tuple(map(ord, s)) 
 
 
-    
 def get_if_addr(iff):
     return socket.inet_ntoa(get_if_raw_addr(iff))
     
@@ -52,13 +51,14 @@ def get_if_hwaddr(iff):
         raise Scapy_Exception("Unsupported address family (%i) for interface [%s]" % (addrfamily,iff))
 
 
-LINUX=sys.platform.startswith("linux")
-OPENBSD=sys.platform.startswith("openbsd")
-FREEBSD= "freebsd" in sys.platform
-NETBSD=sys.platform.startswith("netbsd")
-DARWIN=sys.platform.startswith("darwin")
-SOLARIS=sys.platform.startswith("sunos")
-WINDOWS=sys.platform.startswith("win32")
+LINUX = sys.platform.startswith("linux")
+OPENBSD = sys.platform.startswith("openbsd")
+FREEBSD = "freebsd" in sys.platform
+NETBSD = sys.platform.startswith("netbsd")
+DARWIN = sys.platform.startswith("darwin")
+SOLARIS = sys.platform.startswith("sunos")
+WINDOWS = sys.platform.startswith("win32")
+BSD = DARWIN or FREEBSD or OPENBSD or NETBSD
 
 X86_64 = not WINDOWS and (os.uname()[4] == 'x86_64')
 ARM_64 = not WINDOWS and (os.uname()[4] == 'aarch64')
@@ -81,8 +81,12 @@ if LINUX:
     from linux import *
     if scapy.config.conf.use_pcap or scapy.config.conf.use_dnet:
         from pcapdnet import *
-elif OPENBSD or FREEBSD or NETBSD or DARWIN:
-    from bsd import *
+elif BSD:
+    from bsd import LOOPBACK_NAME
+    from unix import read_routes, read_routes6, in6_getifaddr
+    scapy.config.conf.use_pcap = True
+    scapy.config.conf.use_dnet = True
+    from pcapdnet import *
 elif SOLARIS:
     from solaris import *
 elif WINDOWS:
