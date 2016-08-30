9e276ec912db335b151847beac6fd24b49706658
Allain Legacy <allain.legacy@windriver.com>
2016-08-30 16:03:41 -0400

    vxlan: fixed incorrect field layout specification
    The "NextProtocol" field was added as an unconditional field which made the
    VXLAN packet header consume an additional 8-bts when used in a Legacy VXLAN
    deployments (flags=0x08) or in GPID deployments (flags=0x88).  The resulting
    regression caused incorrect parsing of subsequent headers.  For example, if an
    Ethernet header followed the VXLAN header then it would not be interpreted as
    an Ethernet header.  The actually interpretation would depend on what value was
    stored in the Ether.dst field.
    
    This change updates the field layout specification such that the NextProtocol
    field is conditional on flags=0x04, and shortens the reserved field by 8-bits
    if that condition is true.
    
    fixes bug #281
    
    Signed-off-by: Allain Legacy <allain.legacy@windriver.com>

--- scapy/layers/vxlan.py
+++ scapy/layers/vxlan.py
@@ -12,7 +12,7 @@ from scapy.layers.l2 import Ether
 from scapy.layers.inet import IP, UDP
 from scapy.layers.inet6 import IPv6
 from scapy.fields import FlagsField, XByteField, ThreeBytesField, \
-        ConditionalField, ShortField, ByteEnumField, X3BytesField
+    ConditionalField, ShortField, ByteEnumField, X3BytesField
 
 _GP_FLAGS = ["R", "R", "R", "A", "R", "R", "D", "R"]
 
@@ -24,15 +24,22 @@ class VXLAN(Packet):
         FlagsField("flags", 0x8, 8,
                    ['OAM', 'R', 'NextProtocol', 'Instance',
                     'V1', 'V2', 'R', 'G']),
-        ByteEnumField('NextProtocol', 0,
-                      {0: 'NotDefined',
-                       1: 'IPv4',
-                       2: 'IPv6',
-                       3: 'Ethernet',
-                       4: 'NSH'}),
+        ConditionalField(
+            ShortField("reserved0", 0),
+            lambda pkt: pkt.flags & 0x04,
+        ),
+        ConditionalField(
+            ByteEnumField('NextProtocol', 0,
+                          {0: 'NotDefined',
+                           1: 'IPv4',
+                           2: 'IPv6',
+                           3: 'Ethernet',
+                           4: 'NSH'}),
+            lambda pkt: pkt.flags & 0x04,
+        ),
         ConditionalField(
             ThreeBytesField("reserved1", 0x000000),
-            lambda pkt: not pkt.flags & 0x80,
+            lambda pkt: (not pkt.flags & 0x80) and (not pkt.flags & 0x04),
         ),
         ConditionalField(
             FlagsField("gpflags", 0x0, 8, _GP_FLAGS),
@@ -58,9 +65,11 @@ class VXLAN(Packet):
             return self.sprintf("VXLAN (vni=%VXLAN.vni%)")
 
 bind_layers(UDP, VXLAN, dport=4789)  # RFC standard port
-bind_layers(UDP, VXLAN, dport=6633)  # New IANA assigned port when used with NSH
+bind_layers(UDP, VXLAN, dport=6633)  # New IANA assigned port for use with NSH
 bind_layers(UDP, VXLAN, dport=8472)  # Linux implementation port
-bind_layers(VXLAN, Ether, {'flags': 0x8}, NextProtocol=0)
+bind_layers(VXLAN, Ether, {'flags': 0x8})
+bind_layers(VXLAN, Ether, {'flags': 0x88})
+bind_layers(VXLAN, Ether, {'flags': 0xC, 'NextProtocol': 0}, NextProtocol=0)
 bind_layers(VXLAN, IP, {'flags': 0xC, 'NextProtocol': 1}, NextProtocol=1)
 bind_layers(VXLAN, IPv6, {'flags': 0xC, 'NextProtocol': 2}, NextProtocol=2)
 bind_layers(VXLAN, Ether, {'flags': 0xC, 'NextProtocol': 3}, NextProtocol=3)
