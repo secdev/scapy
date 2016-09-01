f24537c31c7fd9b567b12f9d55157f5d49a73fc9
gpotter2 <gpotter2@users.noreply.github.com>
2016-09-01 19:57:35 +0200

    Corrected tests crash on Windows
    The %DATE% param would return the date with / between days, month, years... but then the / would be used as a directory delimiter and would return "Directory not found exception".
    So replaced the / by _

--- test/run_tests.bat
+++ test/run_tests.bat
@@ -2,7 +2,9 @@
 set MYDIR=%cd%\..
 set PYTHONPATH=%MYDIR%
 if [%1]==[] (
-  python %MYDIR%\scapy\tools\UTscapy.py -t regression.uts -f html -o scapy_regression_test_%DATE%.html
+  SET date=%DATE%
+  SET trimed=%date:/=_%
+  python %MYDIR%\scapy\tools\UTscapy.py -t regression.uts -f html -o scapy_regression_test_%trimed%.html
 ) else (
   python %MYDIR%\scapy\tools\UTscapy.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 )
