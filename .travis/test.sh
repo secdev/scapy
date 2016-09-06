95aa7da987f6c866f37aaff5190eb562e11caac7
Daniel Collins <daniel.e.collins1@gmail.com>
2016-09-05 20:14:25 -0700

    Added travis support for pycrypto 2.7a1 combined modes.

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,7 +4,13 @@ python -c "from scapy.all import *; print conf"
 # Don't run tests that requires root privileges
 if [ -z $TRAVIS_SUDO ]
 then
-  UT_FLAGS="-K netaccess"
+  UT_FLAGS="-K netaccess "
+fi
+
+# Test AEAD modes in IPSec if available
+if [ "$TEST_COMBINED_MODES" != "yes" ]
+then
+  UT_FLAGS+="-K combined_modes "
 fi
 
 # Run unit tests
