535c159ca2ff91447ceb20b33ba56b158f6bf973
Guillaume Valadon <guillaume.valadon@ssi.gouv.fr>
2016-10-11 13:10:29 +0200

    Use LOOPBACK_NAME

--- scapy/layers/l2.py
+++ scapy/layers/l2.py
@@ -18,6 +18,7 @@ from scapy.plist import SndRcvList
 from scapy.fields import *
 from scapy.sendrecv import *
 from scapy.arch import get_if_hwaddr
+from scapy.arch.consts import LOOPBACK_NAME
 from scapy.utils import inet_ntoa, inet_aton
 if conf.route is None:
     # unused import, only to initialize conf.route
@@ -61,7 +62,7 @@ def getmacbyip(ip, chainCC=0):
     if (tmp[0] & 0xf0) == 0xe0: # mcast @
         return "01:00:5e:%.2x:%.2x:%.2x" % (tmp[1]&0x7f,tmp[2],tmp[3])
     iff,a,gw = conf.route.route(ip)
-    if ( (iff == "lo") or (ip == conf.route.get_if_bcast(iff)) ):
+    if ( (iff == LOOPBACK_NAME) or (ip == conf.route.get_if_bcast(iff)) ):
         return "ff:ff:ff:ff:ff:ff"
     if gw != "0.0.0.0":
         ip = gw
