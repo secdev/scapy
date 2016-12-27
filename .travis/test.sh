8fc77d5eb7db03eccd7a99fb4afc118e99c0231a
Guillaume Valadon <guillaume@valadon.net>
2016-12-27 20:34:16 +0100

    codecov support

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,15 +1,15 @@
 # Dump environment variables
-echo "TRAVIS_SUDO=" $TRAVIS_SUDO
+echo "SCAPY_SUDO=" $SCAPY_SUDO
 echo "TRAVIS_OS_NAME=" $TRAVIS_OS_NAME
 
 # Dump Scapy config
 python -c "from scapy.all import *; print conf"
 
 # Don't run tests that require root privileges
-if [ -z "$TRAVIS_SUDO" -o "$TRAVIS_SUDO" = "false" ]
+if [ -z "$SCAPY_SUDO" -o "$SCAPY_SUDO" = "false" ]
 then
   UT_FLAGS="-K netaccess -K needs_root"
-  TRAVIS_SUDO=""
+  SCAPY_SUDO=""
 fi
 
 # Test AEAD modes in IPsec if available
@@ -22,6 +22,22 @@ fi
 for _path in /sbin /usr/sbin /usr/local/sbin; do
   [ -d "$_path" ] && echo "$PATH" | grep -qvE "(^|:)$_path(:|$)" && export PATH="$PATH:$_path"
 done
+
+# Create a fake Python executable
+if [ "$SCAPY_COVERAGE" = "yes" ]
+then
+  echo '#!/bin/bash' > test/python
+  echo '[ "$*" = "--version" ] && echo "Python 2 - fake version string"' >> test/python
+  echo '[ "$*" != "--version" ] && coverage run -a $*' >> test/python
+  chmod +x test/python
+  PATH=.:$PATH
+
+  # Copy the fake Python interpreter to bypass /etc/sudoers rules on Ubuntu
+  if [ "$SCAPY_SUDO" = "sudo" ]
+  then
+    $SCAPY_SUDO cp test/python /usr/local/sbin/
+  fi
+fi
 
 # Do we have tcpdump?
 which tcpdump >/dev/null 2>&1 || UT_FLAGS+=" -K tcpdump"
@@ -36,7 +52,7 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]
 then
   if [ -z $SCAPY_USE_PCAPDNET ]
   then
-    $TRAVIS_SUDO ./run_tests -q -F -t bpf.uts $UT_FLAGS || exit $?
+    $SCAPY_SUDO ./run_tests -q -F -t bpf.uts $UT_FLAGS || exit $?
   fi
 fi
 
@@ -46,10 +62,10 @@ do
   then
     continue
   fi
-  $TRAVIS_SUDO ./run_tests -q -F -t $f $UT_FLAGS || exit $?
+  $SCAPY_SUDO ./run_tests -q -F -t $f $UT_FLAGS || exit $?
 done
 
 for f in ../scapy/contrib/*.uts
 do
-  $TRAVIS_SUDO ./run_tests -f text -t $f $UT_FLAGS -P "load_contrib('$(basename ${f/.uts})')" || exit $?
+  $SCAPY_SUDO ./run_tests -f text -t $f $UT_FLAGS -P "load_contrib('$(basename ${f/.uts})')" || exit $?
 done
