a283f7961410fe67fcc1ea2a80d18ee9f4e11198
gpotter2 <gpotter2@users.noreply.github.com>
2017-02-23 23:14:07 +0100

    Fix paths

--- .appveyor/InstallWindump.ps1
+++ .appveyor/InstallWindump.ps1
@@ -1,4 +1,4 @@
-wget https://github.com/hsluoyz/WinDump/releases/download/v0.1/WinDump-for-Npcap-0.1.zip -UseBasicParsing -OutFile .\npcap.zip
+wget https://github.com/hsluoyz/WinDump/releases/download/v0.2/WinDump-for-Npcap-0.2.zip -UseBasicParsing -OutFile $PSScriptRoot"\npcap.zip"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
 {
@@ -7,6 +7,6 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 Unzip $PSScriptRoot"\npcap.zip" $PSScriptRoot"\npcap"
-Remove-Item ".\npcap.zip"
-Move-Item -Force ".\npcap\x64\WinDump.exe" "C:\Windows\System32\windump.exe"
-Remove-Item ".\npcap" -recurse
+Remove-Item $PSScriptRoot"\npcap.zip"
+Move-Item -Force $PSScriptRoot"\npcap\x64\WinDump.exe" "C:\Windows\System32\windump.exe"
+Remove-Item $PSScriptRoot"\npcap" -recurse
