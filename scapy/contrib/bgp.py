18b331793cbc0c0f87288a724e38e866532fab14
Pedro A. Aranda Gutiérrez <paaguti@gmail.com>
2017-05-17 09:06:54 +0200

    Fix checks

--- scapy/contrib/bgp.py
+++ scapy/contrib/bgp.py
@@ -114,7 +114,7 @@ class BGPFieldIPv4(Field):
         #
         # Show IPv4 default prefix as "0/0"
         #
-        if self.af == socket.AF_INET and ip = "0.0.0.0":
+        if self.af == socket.AF_INET and ip == "0.0.0.0":
             ip = "0"
         return ip + "/" + str(mask)
 
