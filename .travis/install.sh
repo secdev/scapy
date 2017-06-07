b870d10e2883bea587498ab2483c9ff1a03173fe
Stephen <stephengroat@users.noreply.github.com>
2017-06-07 09:52:34 -0700

    fix osx install

--- .travis/install.sh
+++ .travis/install.sh
@@ -24,7 +24,6 @@ if [ ! -z $SCAPY_USE_PCAPDNET ]; then
     mkdir -p /Users/travis/Library/Python/2.7/lib/python/site-packages
     echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> /Users/travis/Library/Python/2.7/lib/python/site-packages/homebrew.pth
  
-    brew install libdnet
-    pip install pylibpcap
+    brew install libdnet .travis/pylibpcap.rb
   fi
 fi
