0661efcba3262ca1caee5864486fdbe4b9e01a71
gpotter2 <gabriel@potter.fr>
2017-07-23 18:13:05 +0200

    Fix VRRPv3 checksum

--- scapy/layers/vrrp.py
+++ scapy/layers/vrrp.py
@@ -10,15 +10,17 @@ VRRP (Virtual Router Redundancy Protocol).
 
 from scapy.packet import *
 from scapy.fields import *
-from scapy.layers.inet import IP
+from scapy.layers.inet import *
+from scapy.layers.inet6 import *
+from scapy.error import warning
 
 IPPROTO_VRRP=112
 
 # RFC 3768 - Virtual Router Redundancy Protocol (VRRP)
 class VRRP(Packet):
     fields_desc = [
-        BitField("version" , 2, 4),
-        BitField("type" , 1, 4),
+        BitField("version", 2, 4),
+        BitField("type", 1, 4),
         ByteField("vrid", 1),
         ByteField("priority", 100),
         FieldLenField("ipcount", None, count_of="addrlist", fmt="B"),
@@ -36,4 +38,51 @@ class VRRP(Packet):
             p = p[:6]+chr(ck>>8)+chr(ck&0xff)+p[8:]
         return p
 
+    @classmethod
+    def dispatch_hook(cls, _pkt=None, *args, **kargs):
+        if _pkt and len(_pkt) >= 9:
+            ver_n_type = ord(_pkt[0])
+            if ver_n_type >= 48 and ver_n_type <= 57: # Version == 3
+                return VRRPv3
+        return VRRP
+
+
+# RFC 5798 -  Virtual Router Redundancy Protocol (VRRP) Version 3
+class VRRPv3(Packet):
+    fields_desc = [
+        BitField("version", 3, 4),
+        BitField("type", 1, 4),
+        ByteField("vrid", 1),
+        ByteField("priority", 100),
+        FieldLenField("ipcount", None, count_of="addrlist", fmt="B"),
+        BitField("res", 0, 4),
+        BitField("adv", 100, 12),
+        XShortField("chksum", None),
+        # FIXME: addrlist should also allow IPv6 addresses :/
+        FieldListField("addrlist", [], IPField("", "0.0.0.0"),
+                       count_from = lambda pkt: pkt.ipcount)]
+
+    def post_build(self, p, pay):
+        if self.chksum is None:
+            if isinstance(self.underlayer, IP):
+                ck = in4_chksum(112, self.underlayer, p)
+            elif isinstance(self.underlayer, IPv6):
+                ck = in6_chksum(112, self.underlayer, p)
+            else:
+                warning("No IP(v6) layer to compute checksum on VRRP. Leaving null")
+                ck = 0
+            p = p[:6]+chr(ck>>8)+chr(ck&0xff)+p[8:]
+        return p
+
+    @classmethod
+    def dispatch_hook(cls, _pkt=None, *args, **kargs):
+        if _pkt and len(_pkt) >= 16:
+            ver_n_type = ord(_pkt[0])
+            if ver_n_type < 48 or ver_n_type > 57: # Version != 3
+                return VRRP
+        return VRRPv3
+
+# IPv6 is supported only on VRRPv3
 bind_layers( IP,            VRRP,          proto=IPPROTO_VRRP)
+bind_layers( IP,            VRRPv3,        proto=IPPROTO_VRRP)
+bind_layers( IPv6,          VRRPv3,        nh=IPPROTO_VRRP)
