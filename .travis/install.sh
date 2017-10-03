ec80e3538d78e22df9a8c31dfaa91f23486410ad
Pierre LALET <pierre.lalet@cea.fr>
2017-10-03 13:32:32 +0200

    Enable Python 3 (3.3 to 3.6) tests
    This also fixes the Python version used for the test and to
    install the packages.

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,20 +6,22 @@ then
   then
     PIP_INSTALL_FLAGS="--user"
   fi
+else
+  SCAPY_SUDO="$SCAPY_SUDO -H"
 fi
 
-if python --version 2>&1 | grep -q PyPy; then
+$SCAPY_SUDO `which pip` install $PIP_INSTALL_FLAGS -U mock
+
+if ! python --version 2>&1 | grep -q PyPy; then
   # cryptography requires PyPy >= 2.6, Travis CI uses 2.5.0
-  $SCAPY_SUDO pip install $PIP_INSTALL_FLAGS -U mock
-else
-  $SCAPY_SUDO pip install $PIP_INSTALL_FLAGS -U cryptography mock
+  $SCAPY_SUDO `which pip` install $PIP_INSTALL_FLAGS -U cryptography
 fi
 
 # Install coverage
 if [ "$SCAPY_COVERAGE" = "yes" ]
 then
-  $SCAPY_SUDO pip install $PIP_INSTALL_FLAGS coverage
-  $SCAPY_SUDO apt-get install python-pyx
+  $SCAPY_SUDO `which pip` install $PIP_INSTALL_FLAGS -U coverage
+  $SCAPY_SUDO `which pip` install $PIP_INSTALL_FLAGS -U PyX
 fi
 
 # Install pcap & dnet
@@ -27,7 +29,9 @@ if [ ! -z $SCAPY_USE_PCAPDNET ]
 then
   if [ "$TRAVIS_OS_NAME" = "linux" ]
   then
-    $SCAPY_SUDO apt-get install python-libpcap python-dumbnet openssl
+    $SCAPY_SUDO apt-get install openssl
+    $SCAPY_SUDO `which pip` install $PIP_INSTALL_FLAGS -U pylibpcap
+    $SCAPY_SUDO `which pip` install $PIP_INSTALL_FLAGS -U pydumbnet
   elif [ "$TRAVIS_OS_NAME" = "osx" ]
   then
     mkdir -p /Users/travis/Library/Python/2.7/lib/python/site-packages
--- .travis/test.sh
+++ .travis/test.sh
@@ -13,13 +13,15 @@ echo "TRAVIS_OS_NAME=" $TRAVIS_OS_NAME
 
 # Dump Scapy config
 python --version
-python -c "from scapy.all import *; print conf"
+python -c "from scapy.all import *; print(conf)"
 
 # Don't run tests that require root privileges
 if [ -z "$SCAPY_SUDO" -o "$SCAPY_SUDO" = "false" ]
 then
   UT_FLAGS="-K netaccess -K needs_root -K manufdb"
   SCAPY_SUDO=""
+else
+  SCAPY_SUDO="$SCAPY_SUDO -H"
 fi
 
 if [ "$SCAPY_USE_PCAPDNET" = "yes" ]
@@ -48,13 +50,13 @@ done
 if [ "$SCAPY_COVERAGE" = "yes" ]
 then
   echo '#!/bin/bash' > test/python
-  echo '[ "$*" = "--version" ] && echo "Python 2 - fake version string"' >> test/python
-  echo '[ "$*" != "--version" ] && coverage run --concurrency=multiprocessing -a $*' >> test/python
+  echo '[ "$*" = "--version" ] && echo "Python 2 - fake version string" && exit 0' >> test/python
+  echo "`which coverage` run --concurrency=multiprocessing -a \$*" >> test/python
   chmod +x test/python
   PATH=.:$PATH
 
   # Copy the fake Python interpreter to bypass /etc/sudoers rules on Ubuntu
-  if [ "$SCAPY_SUDO" = "sudo" ]
+  if [ -n "$SCAPY_SUDO" ]
   then
     $SCAPY_SUDO cp test/python /usr/local/sbin/
   fi
@@ -64,6 +66,11 @@ fi
 which tcpdump >/dev/null 2>&1 || UT_FLAGS+=" -K tcpdump"
 which tshark >/dev/null 2>&1 || UT_FLAGS+=" -K tshark"
 
+if [ -n "$SCAPY_SUDO" ]
+then
+  SCAPY_SUDO="$SCAPY_SUDO --preserve-env"
+fi
+
 # Dump Environment (so that we can check PATH, UT_FLAGS, etc.)
 set
 
@@ -72,9 +79,9 @@ cd test/
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]
 then
-  if [ -z $SCAPY_USE_PCAPDNET ]
+  if [ -z "$SCAPY_USE_PCAPDNET" ]
   then
-    $SCAPY_SUDO ./run_tests -q -F -t bpf.uts $UT_FLAGS || exit $?
+    PYTHON=`which python` $SCAPY_SUDO ./run_tests -q -F -t bpf.uts $UT_FLAGS || exit $?
   fi
   UT_FLAGS+=" -K manufdb -K linux"
 fi
@@ -85,11 +92,10 @@ then
 fi
 
 # Run all normal and contrib tests
-$SCAPY_SUDO ./run_tests -c ./configs/travis.utsc -T "bpf.uts" -T "mock_windows.uts" $UT_FLAGS || exit $?
+PYTHON=`which python` $SCAPY_SUDO ./run_tests -c ./configs/travis.utsc -T "bpf.uts" -T "mock_windows.uts" $UT_FLAGS || exit $?
 
 # Run unit tests with openssl if we have root privileges
-if [ "$TRAVIS_OS_NAME" = "linux" ] && [ ! -z $SCAPY_USE_PCAPDNET ] && [ ! -z $SCAPY_SUDO ]
+if [ "$TRAVIS_OS_NAME" = "linux" ] && [ -n "$SCAPY_USE_PCAPDNET" ] && [ -n "$SCAPY_SUDO" ]
 then
-  $SCAPY_SUDO ./run_tests -q -F -t tls/tests_tls_netaccess.uts $UT_FLAGS || exit $?
+  PYTHON=`which python` $SCAPY_SUDO ./run_tests -q -F -t tls/tests_tls_netaccess.uts $UT_FLAGS || exit $?
 fi
-
