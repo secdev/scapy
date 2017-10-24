98b064d91b6042afe1491d320691af4878d3a4d2
speakinghedge <hecke@naberius.de>
2017-10-24 22:07:15 +0200

    fix invalid field name in LLDPDUSystemName PDU
    name was covered by... the class name. damn karl.

--- scapy/contrib/lldp.py
+++ scapy/contrib/lldp.py
@@ -455,8 +455,8 @@ class LLDPDUSystemName(LLDPDU):
     """
     fields_desc = [
         BitEnumField('_type', 0x05, 7, LLDPDU.TYPES),
-        BitFieldLenField('_length', None, 9, length_of='name'),
-        StrLenField('name', '', length_from=lambda pkt: pkt._length)
+        BitFieldLenField('_length', None, 9, length_of='system_name'),
+        StrLenField('system_name', '', length_from=lambda pkt: pkt._length)
     ]
 
 
