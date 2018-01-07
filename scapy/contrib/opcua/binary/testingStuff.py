2872321def2a3bcb0b3636da60d4e41fac0331a6
Mark <mark.giraud@t-online.de>
2018-01-07 18:29:05 +0100

    Add documentation and sensible default values

--- scapy/contrib/opcua/binary/testingStuff.py
+++ scapy/contrib/opcua/binary/testingStuff.py
@@ -1,15 +1,9 @@
 # coding=utf-8
-import functools
-import logging
 import timeit
 
-logging.getLogger("scapy").setLevel(logging.INFO)
 from scapy.contrib.opcua.binary.bindings import *
-from scapy.contrib.opcua.binary.builtinTypes import *
 from scapy.layers.inet import *
-from scapy.layers.all import *
 from scapy.contrib.opcua.binary.secureConversation import *
-from scapy.contrib.opcua.binary.schemaTypes import *
 from scapy.contrib.opcua.binary.clientAutomaton import UaClient
 
 
@@ -21,5 +15,5 @@ def read_pcap():
 if __name__ == '__main__':
     conf.debug_dissector = True
 
-    client = UaClient(debug=5)
+    client = UaClient()
     client.run()
