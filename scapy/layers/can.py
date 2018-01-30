81f8936d41e95144b42c74166957081a53a2a0b0
sebastien mainand <sebastien.mainand@ssi.gouv.fr>
2018-01-30 09:56:08 +0100

    implement wireshark-like 'byte swap' option for CAN layer

--- scapy/layers/can.py
+++ scapy/layers/can.py
@@ -9,13 +9,20 @@ Wireshark dissectors. See https://wiki.wireshark.org/CANopen
 
 """
 
-
+import struct
+import scapy.modules.six as six
+from scapy.compat import *
 from scapy.config import conf
 from scapy.data import DLT_CAN_SOCKETCAN
 from scapy.fields import BitField, FieldLenField, FlagsField, StrLenField, \
     ThreeBytesField, XBitField
-from scapy.packet import Packet, bind_layers
+from scapy.packet import Packet, bind_layers, RawVal
 from scapy.layers.l2 import CookedLinux
+
+
+# Mimics the Wireshark CAN dissector parameter 'Byte-swap the CAN ID/flags field'
+#   set to True when working with PF_CAN sockets
+conf.contribs['CAN'] = {'swap-bytes': False}
 
 
 class CAN(Packet):
@@ -31,6 +38,43 @@ class CAN(Packet):
         StrLenField('data', '', length_from=lambda pkt: pkt.length),
         StrLenField('padding', '', length_from=lambda pkt: 8 - pkt.length),
     ]
+
+    def pre_dissect(self, s):
+        """ Implements the swap-bytes functionality when dissecting """
+        if conf.contribs['CAN']['swap-bytes']:
+            return struct.pack('<I12s', *struct.unpack('>I12s', s))
+        return s
+
+    def self_build(self, field_pos_list=None):
+        """ Implements the swap-bytes functionality when building
+
+        this is based on a copy of the Packet.self_build default method.
+        The goal is to affect only the CAN layer data and keep
+        under layers (e.g LinuxCooked) unchanged
+        """
+        if self.raw_packet_cache is not None:
+            for fname, fval in six.iteritems(self.raw_packet_cache_fields):
+                if self.getfieldval(fname) != fval:
+                    self.raw_packet_cache = None
+                    self.raw_packet_cache_fields = None
+                    break
+            if self.raw_packet_cache is not None:
+                if conf.contribs['CAN']['swap-bytes']:
+                    return struct.pack('<I12s', *struct.unpack('>I12s', self.raw_packet_cache))
+                return self.raw_packet_cache
+        p = b""
+        for f in self.fields_desc:
+            val = self.getfieldval(f.name)
+            if isinstance(val, RawVal):
+                sval = raw(val)
+                p += sval
+                if field_pos_list is not None:
+                    field_pos_list.append((f.name, sval.encode('string_escape'), len(p), len(sval)))
+            else:
+                p = f.addfield(self, p, val)
+        if conf.contribs['CAN']['swap-bytes']:
+            return struct.pack('<I12s', *struct.unpack('>I12s', p))
+        return p
 
 
 conf.l2types.register(DLT_CAN_SOCKETCAN, CAN)
