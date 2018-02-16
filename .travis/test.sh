be764074bd073bacebfdd53974a0a8ddc5ad976d
Guillaume Valadon <guillaume@valadon.net>
2018-02-16 17:42:47 +0100

    Switch Linux Travis test to tox

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,7 +1,24 @@
-# Test Python3 Scapy on osx
-if [ "$TRAVIS_OS_NAME" = "osx" ] && [ ! -z "$TOXENV" ]
+# Launch tox
+if [ ! -z "$TOXENV" ]
 then
-  tox -- -K tcpdump
+  if [ "$TRAVIS_OS_NAME" = "linux" ]
+  then
+    # Linux 
+    UT_FLAGS=" -K tshark" # TODO: also test as root ?
+    if [ "$TRAVIS_SUDO" != "true" ]
+    then
+      # Linux non root
+      UT_FLAGS+=" -K manufdb"
+    fi
+  elif [ "$TRAVIS_OS_NAME" = "osx" ]
+  then
+    UT_FLAGS=" -K tcpdump"
+  fi
+
+  # Dump Environment (so that we can check PATH, UT_FLAGS, etc.)
+  set
+
+  tox -- $UT_FLAGS
   exit $?
 fi
 
