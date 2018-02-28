f688c2ac8c6f15c22c50d89c8ac652cece8fee93
Pierre Lalet <pierre@droids-corp.org>
2018-02-28 17:00:04 +0100

    https://github.com/secdev/scapy/pull/1186/files/d0a2544ae13ba353d25e86be2cfc13cceda36a10

--- scapy/layers/ntp.py
+++ scapy/layers/ntp.py
@@ -20,7 +20,7 @@ LEIntField, FixedPointField, IPField, StrField, StrFixedLenField,\
 StrFixedLenEnumField, XStrFixedLenField, PacketField, PacketLenField,\
 PacketListField, FieldListField, ConditionalField, PadField)
 from scapy.layers.inet6 import IP6Field
-from scapy.layers.inet import UDP, TCP, Ether
+from scapy.layers.inet import UDP
 from scapy.utils import lhex
 from scapy.compat import *
 from scapy.config import conf
@@ -214,7 +214,7 @@ class NTP(Packet):
         if cls == "NTP":
             if isinstance(self, NTP):
                 return True
-        elif issubclass(globals()[cls], NTP):
+        elif not isinstance(cls, str) and issubclass(cls, NTP):
             if isinstance(self, cls):
                 return True
         return super(NTP, self).haslayer(cls)
