736b83f284a032adfc1a6aebc25ab23ceefe5d85
Nils Weiss <nils@we155.de>
2018-03-04 16:21:44 +0100

    cleaned pull request

--- scapy/layers/can.py
+++ scapy/layers/can.py
@@ -1,6 +1,6 @@
 # This file is part of Scapy
 # See http://www.secdev.org/projects/scapy for more informations
-# Copyright (C) Philippe Biondi <phil@secdev.org>
+# Copyright (C) Philippe Biondi <phil@secdev.org>, Nils Weiss <nils@we155.de>
 # This program is published under a GPLv2 license
 
 
@@ -14,8 +14,7 @@ import scapy.modules.six as six
 from scapy.compat import *
 from scapy.config import conf
 from scapy.data import DLT_CAN_SOCKETCAN
-from scapy.fields import BitField, FieldLenField, FlagsField, StrLenField, \
-    ThreeBytesField, XBitField
+from scapy.fields import PadField, FieldLenField, FlagsField, StrLenField, XBitField
 from scapy.packet import Packet, bind_layers, RawVal
 from scapy.layers.l2 import CookedLinux
 
@@ -24,6 +23,8 @@ from scapy.layers.l2 import CookedLinux
 #   set to True when working with PF_CAN sockets
 conf.contribs['CAN'] = {'swap-bytes': False}
 
+CAN_FRAME_SIZE = 16
+CAN_INV_FILTER = 0x20000000
 
 class CAN(Packet):
     """A minimal implementation of the CANopen protocol, based on
@@ -32,12 +33,18 @@ class CAN(Packet):
     """
     fields_desc = [
         FlagsField('flags', 0, 3, ['error', 'remote_transmission_request', 'extended']),
-        XBitField('identifier', 0, 29),
-        FieldLenField('length', None, length_of='data', fmt='B'),
-        ThreeBytesField('reserved', 0),
-        StrLenField('data', '', length_from=lambda pkt: pkt.length),
-        StrLenField('padding', '', length_from=lambda pkt: 8 - pkt.length),
+        XBitField("identifier", 0, 29),
+        PadField(FieldLenField("length", None, length_of="data", fmt="B"), 4),
+        PadField(StrLenField("data", "", length_from=lambda pkt: min(pkt.length, 8)), 8)
     ]
+
+    @property
+    def id(self):
+        return self.identifier
+
+    @property
+    def dlc(self):
+        return self.length
 
     def pre_dissect(self, s):
         """ Implements the swap-bytes functionality when dissecting """
