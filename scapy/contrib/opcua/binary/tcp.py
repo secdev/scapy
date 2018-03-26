0efef827b432a342801c266bfdc38ad9e0b12916
Mark <mark.giraud@t-online.de>
2018-03-26 17:04:28 +0200

    Add documentation

--- scapy/contrib/opcua/binary/tcp.py
+++ scapy/contrib/opcua/binary/tcp.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 This module implements all networking structures required for OPC UA TCP communication.
 For SecureConversation messages refer to secureConversation.py
@@ -10,12 +11,20 @@ from scapy.fields import ConditionalField
 
 
 class UaTcpMessageHeader(UaTypePacket):
+    """
+    This class represents the TcpMessageHeader of UA TCP messages in the binary encoding according to the specification.
+    See part 6 v1.03 Table 34.
+    """
     fields_desc = [UaBytesField("MessageType", None, 3),
                    UaByteField("Reserved", b'F'),
                    UaUInt32Field("MessageSize", None)]
 
 
 class UaTcpHelloMessage(UaTypePacket):
+    """
+    This class represents a TcpHelloMessage in the OPC UA binary encoding according to the specification.
+    See part 6 v1.03 Table 35.
+    """
     fields_desc = [UaUInt32Field("ProtocolVersion", 0),
                    UaUInt32Field("ReceiveBufferSize", 1 << 12),
                    UaUInt32Field("SendBufferSize", 1 << 12),
@@ -24,7 +33,11 @@ class UaTcpHelloMessage(UaTypePacket):
                    UaPacketField("EndpointUrl", UaString(), UaString)]
 
     def post_build(self, pkt, pay):
-        
+        """
+        Replaces all fields except for the EndpointUrl with values specified in the ConnectionContext.
+        If a field was modified manually it is not changed automatically.
+        If no ConnectionContext is set, the fields remain untouched.
+        """
         if self.connectionContext is not None:
             # Just build completely new packet, since we replace a lot
             newPkt = UaTcpHelloMessage()
@@ -62,6 +75,10 @@ class UaTcpHelloMessage(UaTypePacket):
 
 
 class UaTcpAcknowledgeMessage(UaTypePacket):
+    """
+    This class represents a TcpAcknowledgeMessage in the OPC UA binary encoding according to the specification.
+    See part 6 v1.03 Table 36.
+    """
     fields_desc = [UaUInt32Field("ProtocolVersion", 0),
                    UaUInt32Field("ReceiveBufferSize", 0),
                    UaUInt32Field("SendBufferSize", 0),
@@ -70,25 +87,47 @@ class UaTcpAcknowledgeMessage(UaTypePacket):
 
 
 class UaTcpErrorMessage(UaTypePacket):
+    """
+    This class represents a TcpErrorMessage in the OPC UA binary encoding according to the specification.
+    See part 6 v1.03 Table 37.
+    """
     fields_desc = [UaUInt32Field("Error", 0),
                    UaPacketField("Reason", UaString(), UaString)]
 
 
-def isHEL(p):
+def _is_hel(p):
+    """
+    Determines whether the supplied packet contains a TcpHelloMessage
+    
+    :param p: the packet bytes starting at the UA TCP layer.
+    :return: True if the packet contains a TcpHelloMessage and False otherwise.
+    """
     messageType = p.TcpMessageHeader.getfieldval("MessageType")
     if messageType is None:
         return isinstance(p.Message, UaTcpHelloMessage)
     return p.TcpMessageHeader.MessageType == b'HEL' or isinstance(p.Message, UaTcpHelloMessage)
 
 
-def isACK(p):
+def _is_ack(p):
+    """
+    Determines whether the supplied packet contains a TcpAcknowledgeMessage
+    
+    :param p: the packet bytes starting at the UA TCP layer.
+    :return: True if the packet contains a TcpAcknowledgeMessage and False otherwise.
+    """
     messageType = p.TcpMessageHeader.getfieldval("MessageType")
     if messageType is None:
         return isinstance(p.Message, UaTcpAcknowledgeMessage)
     return p.TcpMessageHeader.MessageType == b'ACK'
 
 
-def isERR(p):
+def _is_err(p):
+    """
+    Determines whether the supplied packet contains a TcpErrorMessage
+    
+    :param p: the packet bytes starting at the UA TCP layer.
+    :return: True if the packet contains a TcpErrorMessage and False otherwise.
+    """
     messageType = p.TcpMessageHeader.getfieldval("MessageType")
     if messageType is None:
         return isinstance(p.Message, UaTcpErrorMessage)
@@ -96,25 +135,45 @@ def isERR(p):
 
 
 class UaTcp(UaTypePacket):
+    """
+    This class models a packet on the UA TCP layer.
+    
+    It is the entry point to any OPC UA binary communication.
+    This means that every OPC UA packet encoded with the binary encoding can be decoded by binding the normal
+    tcp layer to this packet class.
+    
+    A TCP message in OPC UA consists of the TcpMessageHeader (see :class:`UaTcpMessageHeader`)
+    and one of the three possible Messages (see :class:`UaTcpHelloMessage`, :class:`UaTcpAcknowledgeMessage` and
+    :class:`UaTcpErrorMessage`).
+    
+    One of the three messages can be set in the Message attribute.
+    The default is TcpHelloMessage.
+    """
     __slots__ = []
     fields_desc = [UaPacketField("TcpMessageHeader", UaTcpMessageHeader(), UaTcpMessageHeader),
                    ConditionalField(UaPacketField("Message",
                                                   UaTcpErrorMessage(),
                                                   UaTcpErrorMessage),
-                                    isERR),
+                                    _is_err),
                    ConditionalField(UaPacketField("Message",
                                                   UaTcpAcknowledgeMessage(),
                                                   UaTcpAcknowledgeMessage),
-                                    isACK),
+                                    _is_ack),
                    ConditionalField(UaPacketField("Message",
                                                   UaTcpHelloMessage(),
                                                   UaTcpHelloMessage),
-                                    isHEL)
+                                    _is_hel)
                    ]
     
     message_types = [b'HEL', b'ACK', b'ERR']
     
     def post_build(self, pkt, pay):
+        """
+        Automatically calculates the MessageType depending on the contained message if it was not manually set.
+        The message size of the packet is also calculated automatically if not set manually.
+        
+        See overridden method for more.
+        """
         messageTypeField, messageType = self.TcpMessageHeader.getfield_and_val("MessageType")
         messageSizeField, messageSize = self.TcpMessageHeader.getfield_and_val("MessageSize")
         
@@ -143,7 +202,9 @@ class UaTcp(UaTypePacket):
     @classmethod
     def dispatch_hook(cls, _pkt=None, *args, **kargs):
         """
-        Determines whether we are dealing with UA TCP messages or with SecureConversation messages
+        Determines whether we are dealing with UA TCP messages or with SecureConversation messages.
+        
+        :return the correct class with which the packet will be decoded
         """
         if _pkt is None:
             return cls
