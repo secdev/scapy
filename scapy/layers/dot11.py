b45f3c3e4f855d3178edfed0d61a46d1f4c5ec67
Pierre Lorinquer <pierre.lorinquer@ssi.gouv.fr>
2018-05-24 14:09:00 +0200

    Tests rewritten in guess_payload_class().

--- scapy/layers/dot11.py
+++ scapy/layers/dot11.py
@@ -206,11 +206,8 @@ class RadioTap(Packet):
                    StrLenField('notdecoded', "", length_from=lambda pkt: pkt.len - pkt._tmp_dissect_pos)]  # noqa: E501
 
     def guess_payload_class(self, payload):
-        if self.present and self.present.Flags:
-            if self.Flags.FCS:
-                return Dot11FCS
-            else:
-                return Dot11
+        if self.present and self.present.Flags and self.Flags.FCS:
+            return Dot11FCS
         return Dot11
 
     def post_build(self, p, pay):
