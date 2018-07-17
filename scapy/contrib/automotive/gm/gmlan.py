ce40106cc86302d6dcccde0773f72910ccfce416
Gabriel Potter <gabriel@potter.fr>
2018-07-17 15:02:04 +0200

    automative: informations->information (#1526)
    informations->information

--- scapy/contrib/automotive/gm/gmlan.py
+++ scapy/contrib/automotive/gm/gmlan.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # This file is part of Scapy
-# See http://www.secdev.org/projects/scapy for more informations
+# See http://www.secdev.org/projects/scapy for more information
 # Copyright (C) Nils Weiss <nils@we155.de>
 # Copyright (C) Enrico Pozzobon <enrico.pozzobon@gmail.com>
 # This program is published under a GPLv2 license
