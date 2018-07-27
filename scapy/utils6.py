7aa545fb90e087214e96701c03139416c7c47727
MichaelK760 <40545094+MichaelK760@users.noreply.github.com>
2018-07-26 21:58:53 -0700

    commenting in6_ifaceidtomac function behavior
    I did my best to comment the function behavior as requested on the contribution roadmap

--- scapy/utils6.py
+++ scapy/utils6.py
@@ -225,16 +225,21 @@ def in6_ifaceidtomac(ifaceid):  # TODO: finish commenting function behavior
     is returned on error.
     """
     try:
+        # Set ifaceid to a binary form
         ifaceid = inet_pton(socket.AF_INET6, "::" + ifaceid)[8:16]
     except:
         return None
-    if ifaceid[3:5] != b'\xff\xfe':
-        return None
+    if ifaceid[3:5] != b'\xff\xfe': # Check for burned-in MAC address
+        return None 
+    
+    # Unpacking and converting first byte of faceid to MAC address equivalent
     first = struct.unpack("B", ifaceid[:1])[0]
-    ulbit = 2 * [1, '-', 0][first & 0x02]
-    first = struct.pack("B", ((first & 0xFD) | ulbit))
+    ulbit = 2 * [1, '-', 0][first & 0x02] 
+    first = struct.pack("B", ((first & 0xFD) | ulbit)) 
+    # Split into two vars to remove the \xff\xfe bytes
     oui = first + ifaceid[1:3]
     end = ifaceid[5:]
+    # Convert and reconstruct into a MAC Address
     l = ["%.02x" % orb(x) for x in list(oui + end)]
     return ":".join(l)
 
