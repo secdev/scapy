f8631ce1c48b29b2a3da867c13c7983d64111bdc
MichaelK760 <40545094+MichaelK760@users.noreply.github.com>
2018-07-27 09:38:50 -0700

    Linting

--- scapy/utils6.py
+++ scapy/utils6.py
@@ -229,13 +229,13 @@ def in6_ifaceidtomac(ifaceid):
         ifaceid = inet_pton(socket.AF_INET6, "::" + ifaceid)[8:16]
     except:
         return None
-    if ifaceid[3:5] != b'\xff\xfe': # Check for burned-in MAC address
-        return None 
-    
+    if ifaceid[3:5] != b'\xff\xfe':  # Check for burned-in MAC address
+        return None
+
     # Unpacking and converting first byte of faceid to MAC address equivalent
     first = struct.unpack("B", ifaceid[:1])[0]
-    ulbit = 2 * [1, '-', 0][first & 0x02] 
-    first = struct.pack("B", ((first & 0xFD) | ulbit)) 
+    ulbit = 2 * [1, '-', 0][first & 0x02]
+    first = struct.pack("B", ((first & 0xFD) | ulbit))
     # Split into two vars to remove the \xff\xfe bytes
     oui = first + ifaceid[1:3]
     end = ifaceid[5:]
