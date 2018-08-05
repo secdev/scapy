f5da790ef2e57cb20feae49dc2495f05ffe0ff30
MichaelK760 <40545094+MichaelK760@users.noreply.github.com>
2018-08-05 10:35:06 -0700

    commenting in6_ifaceidtomac function behavior (#1539)
    * commenting in6_ifaceidtomac function behavior
    
    I did my best to comment the function behavior as requested on the contribution roadmap

--- scapy/utils6.py
+++ scapy/utils6.py
@@ -218,23 +218,28 @@ def in6_mactoifaceid(mac, ulbit=None):
     return eui64.upper()
 
 
-def in6_ifaceidtomac(ifaceid):  # TODO: finish commenting function behavior
+def in6_ifaceidtomac(ifaceid):
     """
     Extract the mac address from provided iface ID. Iface ID is provided
     in printable format ("XXXX:XXFF:FEXX:XXXX", eventually compressed). None
     is returned on error.
     """
     try:
+        # Set ifaceid to a binary form
         ifaceid = inet_pton(socket.AF_INET6, "::" + ifaceid)[8:16]
     except:
         return None
-    if ifaceid[3:5] != b'\xff\xfe':
+    if ifaceid[3:5] != b'\xff\xfe':  # Check for burned-in MAC address
         return None
+
+    # Unpacking and converting first byte of faceid to MAC address equivalent
     first = struct.unpack("B", ifaceid[:1])[0]
     ulbit = 2 * [1, '-', 0][first & 0x02]
     first = struct.pack("B", ((first & 0xFD) | ulbit))
+    # Split into two vars to remove the \xff\xfe bytes
     oui = first + ifaceid[1:3]
     end = ifaceid[5:]
+    # Convert and reconstruct into a MAC Address
     l = ["%.02x" % orb(x) for x in list(oui + end)]
     return ":".join(l)
 
