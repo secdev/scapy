f2a137c0388494318662b19f7082b492c7c22fca
Harald Albrecht <hidden@hidden.org>
2018-08-27 20:42:19 +0200

    adds summary output regression tests; improves summary printing of lists of DNS server addresses and search lists

--- scapy/layers/inet6.py
+++ scapy/layers/inet6.py
@@ -1946,7 +1946,7 @@ class ICMPv6NDOptRDNSS(_ICMPv6NDGuessPayload, Packet):  # RFC 5006
                                 length_from=lambda pkt: 8 * (pkt.len - 1))]
 
     def mysummary(self):
-        return self.sprintf("%name% %dns%")
+        return self.sprintf("%name% " + ", ".join(self.dns))
 
 
 class ICMPv6NDOptEFA(_ICMPv6NDGuessPayload, Packet):  # RFC 5175 (prev. 5075)
@@ -2023,7 +2023,7 @@ class ICMPv6NDOptDNSSL(_ICMPv6NDGuessPayload, Packet):  # RFC 6106
                    ]
 
     def mysummary(self):
-        return self.sprintf("%name% %searchlist%")
+        return self.sprintf("%name% " + ", ".join(self.searchlist))
 
 # End of ICMPv6 Neighbor Discovery Options.
 
