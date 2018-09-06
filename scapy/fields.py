d2c9d48960903b8204af43470e49a06fff1007dc
Pierre LALET <pierre.lalet@cea.fr>
2018-09-06 17:10:20 +0200

    Fix FlagsField volatile values

--- scapy/fields.py
+++ scapy/fields.py
@@ -1890,6 +1890,8 @@ used in *2i() and i2*() methods.
         return self._fixup_val(super(FlagsField, self).m2i(pkt, x))
 
     def i2h(self, pkt, x):
+        if isinstance(x, VolatileValue):
+            return super(FlagsField, self).i2h(pkt, x)
         return self._fixup_val(super(FlagsField, self).i2h(pkt, x))
 
     def i2repr(self, pkt, x):
