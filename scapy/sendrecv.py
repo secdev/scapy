6751963e0dcd16af03d4f153c3765124c1dbdc4f
Yunior Luis <yl.rojo@zaleos.net>
2018-09-08 18:15:00 +0200

    PEP8 compliant.

--- scapy/sendrecv.py
+++ scapy/sendrecv.py
@@ -926,7 +926,8 @@ def sniff(count=0, store=True, offline=None, prn=None, lfilter=None,
                 try:
                     p = s.recv()
                 except socket.error as ex:
-                    log_runtime.warning("Socket %s failed with '%s' and thus will be ignored" % (s, ex))
+                    log_runtime.warning("Socket %s failed with '%s' and thus"
+                                        " will be ignored" % (s, ex))
                     del sniff_sockets[s]
                     continue
                 except read_allowed_exceptions:
