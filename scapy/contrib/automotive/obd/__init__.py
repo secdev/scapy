22294141cedd0248bc468a2b1800699c26908b28
Nils Weiss <nils@we155.de>
2018-12-11 10:58:26 +0100

    update license headers

--- scapy/contrib/automotive/obd/__init__.py
+++ scapy/contrib/automotive/obd/__init__.py
@@ -1,6 +1,7 @@
 # This file is part of Scapy
 # See http://www.secdev.org/projects/scapy for more information
 # Copyright (C) Andreas Korb <andreas.d.korb@gmail.com>
+# Copyright (C) Nils Weiss <nils@we155.de>
 # This program is published under a GPLv2 license
 
 """
