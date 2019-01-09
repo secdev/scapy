secdev/scapy

commit 435918e3d8e95b7374fadd8c6d962ee9b5032fcb

Make ARP() great again
This uses MultipleTypeField() for {hw,p}{dst,src} fields, to achieve
two goals:

  - Make ARP() usable with other {hw,p}types than Ether and IP.

  - Keep ARP() as simple as possible.

Notes:

  - raw(ARP()) returns the same result as before.

  - ARP(pytpe="IPv4") works and raw(ARP(pytpe="IPv4")) == raw(ARP())

  - ARP(ptype="IPv6") now works, plen is then set to 16 and psrc &
    pdst fields are IPv6 addresses:

      ARP(raw(ARP(ptype="IPv6"))).pdst == "::"

scapy/layers/l2.py
@@ -25,7 +25,7 @@ from scapy.plist import SndRcvList
 from scapy.fields import *
 from scapy.sendrecv import srp, srp1, srpflood
 from scapy.arch import get_if_hwaddr
-from scapy.utils import inet_ntoa, inet_aton
+from scapy.utils import inet_ntoa, inet_aton, valid_mac, valid_net, valid_net6
 from scapy.error import warning
 if conf.route is None:
     # unused import, only to initialize conf.route
@@ -140,10 +140,7 @@ class SourceMACField(MACField):
 
 class ARPSourceMACField(SourceMACField):
     def __init__(self, name):
-        super(ARPSourceMACField, self).__init__(
-            name,
-            getif=lambda pkt: pkt.route()[0],
-        )
+        super(ARPSourceMACField, self).__init__(name)
 
 
 # Layers
@@ -301,15 +298,81 @@ class STP(Packet):
 
 class ARP(Packet):
     name = "ARP"
-    fields_desc = [XShortField("hwtype", 0x0001),
-                   XShortEnumField("ptype", 0x0800, ETHER_TYPES),
-                   ByteField("hwlen", 6),
-                   ByteField("plen", 4),
-                   ShortEnumField("op", 1, {"who-has": 1, "is-at": 2, "RARP-req": 3, "RARP-rep": 4, "Dyn-RARP-req": 5, "Dyn-RAR-rep": 6, "Dyn-RARP-err": 7, "InARP-req": 8, "InARP-rep": 9}),
-                   ARPSourceMACField("hwsrc"),
-                   SourceIPField("psrc", "pdst"),
-                   MACField("hwdst", ETHER_ANY),
-                   IPField("pdst", "0.0.0.0")]
+    fields_desc = [
+        XShortField("hwtype", 0x0001),
+        XShortEnumField("ptype", 0x0800, ETHER_TYPES),
+        FieldLenField("hwlen", None, fmt="B", length_of="hwsrc"),
+        FieldLenField("plen", None, fmt="B", length_of="psrc"),
+        ShortEnumField("op", 1, {
+            "who-has": 1,
+            "is-at": 2,
+            "RARP-req": 3,
+            "RARP-rep": 4,
+            "Dyn-RARP-req": 5,
+            "Dyn-RAR-rep": 6,
+            "Dyn-RARP-err": 7,
+            "InARP-req": 8,
+            "InARP-rep": 9
+        }),
+        MultipleTypeField(
+            [
+                (ARPSourceMACField("hwsrc"),
+                 (lambda pkt: pkt.hwtype == 1 and pkt.hwlen == 6,
+                  lambda pkt, val: pkt.hwtype == 1 and (
+                      pkt.hwlen == 6 or (pkt.hwlen is None and
+                                         (val is None or len(val) == 6 or
+                                          valid_mac(val)))
+                  ))),
+            ],
+            StrFixedLenField("hwsrc", None, length_from=lambda pkt: pkt.hwlen),
+        ),
+        MultipleTypeField(
+            [
+                (SourceIPField("psrc", "pdst"),
+                 (lambda pkt: pkt.ptype == 0x0800 and pkt.plen == 4,
+                  lambda pkt, val: pkt.ptype == 0x0800 and (
+                      pkt.plen == 4 or (pkt.plen is None and
+                                        (val is None or valid_net(val)))
+                  ))),
+                (SourceIP6Field("psrc", "pdst"),
+                 (lambda pkt: pkt.ptype == 0x86dd and pkt.plen == 16,
+                  lambda pkt, val: pkt.ptype == 0x86dd and (
+                      pkt.plen == 16 or (pkt.plen is None and
+                                         (val is None or valid_net6(val)))
+                  ))),
+            ],
+            StrFixedLenField("psrc", None, length_from=lambda pkt: pkt.plen),
+        ),
+        MultipleTypeField(
+            [
+                (MACField("hwdst", ETHER_ANY),
+                 (lambda pkt: pkt.hwtype == 1 and pkt.hwlen == 6,
+                  lambda pkt, val: pkt.hwtype == 1 and (
+                      pkt.hwlen == 6 or (pkt.hwlen is None and
+                                         (val is None or len(val) == 6 or
+                                          valid_mac(val)))
+                  ))),
+            ],
+            StrFixedLenField("hwdst", None, length_from=lambda pkt: pkt.hwlen),
+        ),
+        MultipleTypeField(
+            [
+                (IPField("pdst", "0.0.0.0"),
+                 (lambda pkt: pkt.ptype == 0x0800 and pkt.plen == 4,
+                  lambda pkt, val: pkt.ptype == 0x0800 and (
+                      pkt.plen == 4 or (pkt.plen is None and
+                                        (val is None or valid_net(val)))
+                  ))),
+                (IP6Field("pdst", "::"),
+                 (lambda pkt: pkt.ptype == 0x86dd and pkt.plen == 16,
+                  lambda pkt, val: pkt.ptype == 0x86dd and (
+                      pkt.plen == 16 or (pkt.plen is None and
+                                         (val is None or valid_net6(val)))
+                  ))),
+            ],
+            StrFixedLenField("pdst", None, length_from=lambda pkt: pkt.plen),
+        ),
+    ]
     who_has = 1
     is_at = 2
 

commit d139af40b72664b8c73aebbe37cca99b7fdc764b

Fix (Ether() / ARP()).show()
Bug introduced in 435918e3d8e95b7374fadd8c6d962ee9b5032fcb

Test added.

scapy/layers/l2.py
@@ -71,7 +71,7 @@ def getmacbyip(ip, chainCC=0):
     """Return MAC address corresponding to a given IP address"""
     if isinstance(ip, Net):
         ip = next(iter(ip))
-    ip = inet_ntoa(inet_aton(ip))
+    ip = inet_ntoa(inet_aton(ip or "0.0.0.0"))
     tmp = [orb(e) for e in inet_aton(ip)]
     if (tmp[0] & 0xf0) == 0xe0:  # mcast @
         return "01:00:5e:%.2x:%.2x:%.2x" % (tmp[1] & 0x7f, tmp[2], tmp[3])
@@ -85,13 +85,16 @@ def getmacbyip(ip, chainCC=0):
     if mac:
         return mac
 
-    res = srp1(Ether(dst=ETHER_BROADCAST) / ARP(op="who-has", pdst=ip),
-               type=ETH_P_ARP,
-               iface=iff,
-               timeout=2,
-               verbose=0,
-               chainCC=chainCC,
-               nofilter=1)
+    try:
+        res = srp1(Ether(dst=ETHER_BROADCAST) / ARP(op="who-has", pdst=ip),
+                   type=ETH_P_ARP,
+                   iface=iff,
+                   timeout=2,
+                   verbose=0,
+                   chainCC=chainCC,
+                   nofilter=1)
+    except Exception:
+        return None
     if res is not None:
         mac = res.payload.hwsrc
         conf.netcache.arp_cache[ip] = mac

test/regression.uts
@@ -10697,6 +10697,9 @@ test_IPID_count()
 ############
 + ARP
 
+= Simple Ether() / ARP() show
+(Ether() / ARP()).show()
+
 = ARP for IPv4
 
 p = raw(ARP())