83de6dc264cd5a995ce7970309f91d12f2bce671
qingxp9 <qingxp9@gmail.com>
2019-02-27 21:23:39 +0800

    Added Dot11ProbeResp.network_stats and parse country code

--- scapy/layers/dot11.py
+++ scapy/layers/dot11.py
@@ -421,6 +421,8 @@ class Dot11Beacon(Packet):
                 summary["ssid"] = plain_str(p.info)
             elif p.ID == 3:
                 summary["channel"] = ord(p.info)
+            elif p.ID == 7:
+                summary["country"] = plain_str(p.info[:2])
             elif isinstance(p, Dot11EltRates):
                 summary["rates"] = p.rates
             elif isinstance(p, Dot11EltRSN):
@@ -725,6 +727,37 @@ class Dot11ProbeResp(Packet):
     fields_desc = [LELongField("timestamp", 0),
                    LEShortField("beacon_interval", 0x0064),
                    FlagsField("cap", 0, 16, capability_list)]
+
+    def network_stats(self):
+        """Return a dictionary containing a summary of the Dot11
+        elements fields
+        """
+        summary = {}
+        crypto = set()
+        p = self.payload
+        while isinstance(p, Dot11Elt):
+            if p.ID == 0:
+                summary["ssid"] = plain_str(p.info)
+            elif p.ID == 3:
+                summary["channel"] = ord(p.info)
+            elif p.ID == 7:
+                summary["country"] = plain_str(p.info[:2])
+            elif isinstance(p, Dot11EltRates):
+                summary["rates"] = p.rates
+            elif isinstance(p, Dot11EltRSN):
+                crypto.add("WPA2")
+            elif p.ID == 221:
+                if isinstance(p, Dot11EltMicrosoftWPA) or \
+                        p.info.startswith(b'\x00P\xf2\x01\x01\x00'):
+                    crypto.add("WPA")
+            p = p.payload
+        if not crypto:
+            if self.cap.privacy:
+                crypto.add("WEP")
+            else:
+                crypto.add("OPN")
+        summary["crypto"] = crypto
+        return summary
 
 
 class Dot11Auth(Packet):
