ab3e6f8d5865ac221b3d2cc233e68badc84be785
hissssst <csqcsqcsq@yandex.ru>
2019-03-27 17:53:17 +0300

    Written p0f_correl. And Quirks_p0f rethinked

--- scapy/modules/p0f.py
+++ scapy/modules/p0f.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from scapy.data import KnowledgeBase, MTU
+from scapy.data import KnowledgeBase
 from scapy.config import conf
 from scapy.layers.inet import IP, TCP, TCPOptions
 from scapy.error import warning, Scapy_Exception
@@ -13,7 +13,7 @@ conf.p0f_base = '/etc/p0f/p0f.fp'
 
 def lparse(line, n, default='', splitchar=':'):
     """Function for nice parcing of 'a:b:c:d:e' lines"""
-    a = line.split(splitchar)
+    a = line.split(splitchar)[:n]
     yield from a
     yield from [default] * (n - len(a))
 
@@ -32,7 +32,7 @@ class p0fDatabase(KnowledgeBase):
     def lazy_init(self):
 
         #FIXME I have found out that KnowledgeBase class is used
-        # for p0f and nmap modules which are outdated
+        # only for p0f and nmap modules which are outdated
         # I think it would be nice to rewrite some of
         # KnowledgeBase class code
 
@@ -88,6 +88,30 @@ class p0fDatabase(KnowledgeBase):
                 elif param == 'classes':
                     currdict[currlabel] = value
 
+    def p0f_correl(self, module, moduledir, pkt2p0f_out, pquirks):
+        
+        # prepare values
+        ver, ttl, olen, mss, wsize, wscale, olayout, pclass = pkt2p0f_out
+        sigdict = self.base[module][moduledir]
+
+        for sig, label in sigdict.items():
+            hits = 0
+
+            # 's' stands for 'signature_'
+            sver, sttl, solen, smss, swsize_sscale, solayout, squirks, spclass = lparse(sig, 8)
+            swsize, sscale = swsize_sscale.split(':')
+            # compares main values
+            hits += (sver == '*') or (int(sver) == ver)
+            hits += (int(sttl[:-1]) >= ttl) if sttl[-1] == '-' else int(sttl) == ttl
+            hits += int(solen) == olen
+            hits += (smss == '*') or (int(smss) == mss)
+            hits += (swsize == '*') or (eval(swsize) >= wsize)
+            hits += (sscale == '*') or (int(sscale) == wscale)
+
+            # compares quirks
+            quirks = quirks.split(',')
+            correl = sum(quirks_correl(pquircks, quirks)) / len(quirks)
+
 
 p0fdb = p0fDatabase(conf.p0f_base)
 
@@ -106,45 +130,50 @@ def preprocessPacket4p0f(pkt):
     
     return pkt
 
-class Quirks_p0f:
-    """Nice namespace for p0f quirks"""
-    df      = "don't fragment flag"
-    idp     = "df set but IPID non-zero"
-    idm     = "df not set but IPID zero"
-    ecn     = "explicit confestion notification support"
-    zerop   = "'must be zero' field not zero"
-    flow    = "non-zero IPv6 flow ID"
+# Nice namespace for p0f quirks
+Quirks_p0f = {
+    'df'     : 0,  # don't fragment flag
+    'id+'    : 1,  # df set but IPID non-zero
+    'id-'    : 2,  # df not set but IPID zero
+    'ecn'    : 3,  # explicit confestion notification support
+    'zero+'  : 4,  # 'must be zero' field not zero
+    'flow'   : 5,  # non-zero IPv6 flow ID
 
-    seq     = "sequence number is zero"
-    ackp    = "ACK number is non-zero but ACK flag is not set"
-    ackm    = "ACK number is zero but ACK flag is set"
-    uptrp   = "URG pointer is non-zero but URG flag not set"
-    urgfp   = "URG flag used"
-    pushfpp = "PSUH flag used"
+    'seq-'   : 6,  # sequence number is zero
+    'ack+'   : 7,  # ACK number is non-zero but ACK flag is not set
+    'ack-'   : 8,  # ACK number is zero but ACK flag is set
+    'uptr+'  : 9,  # URG pointer is non-zero but URG flag not set
+    'urgf+'  : 10, # URG flag used
+    'pushf+' : 11, # PUSH flag used
 
-    ts1m    = "own timestamp specified as zero"
-    ts2p    = "non-zero peer timestamp on initial SYN"
-    optp    = "trailing non-zero data in options segment"
-    exws    = "excessive window scaling factor ( > 14)"
-    bad     = "malformed tcp options"
+    'ts1-'   : 12, # own timestamp specified as zero
+    'ts2+'   : 13, # non-zero peer timestamp on initial SYN
+    'opt+'   : 14, # trailing non-zero data in options segment
+    'exws'   : 15, # excessive window scaling factor ( > 14)
+    'bad'    : 16} # malformed tcp options
+
+def quirks_correl(qint, quirks):
+    for quirk in quirks:
+        yield bool((1 << Quirks_p0f[quirk]) & quirks)
 
 def packet2quirks(pkt):
     """requires preprocessed packet"""
+    """not done yet"""
 
-    quirks = []
+    quirks = 0
 
     #df check
     if pkt.flags == 2:
-        quirks.append(Quirks_p0f.df)
+        quirks += 1 << Quirks_p0f['df']
 
         if pkt.id != 0:
-            quirks.append(Quirks_p0f.idp)
+            quirks += 1 << Quirks_p0f['id+']
 
     elif pkt.id == 0:
-        quirks.append(Quirks_p0f.idm)
+        quirks += 1 << Quirks_p0f['id-']
 
     if pkt.flags.ECN:
-        quircks.append(Quirks_p0f.ecn)
+        quircks += 1 << Quirks_p0f['ecn']
 
 
 def packet2p0f(pkt):
@@ -153,8 +182,12 @@ def packet2p0f(pkt):
     #IP  layer - pkt
     #TCP layer - pkt.payload
 
+    #Independent IP
+    ver = pkt.version
+
     #Independent TCP
     wsize = pkt.payload.window
+    pclass = bool(pkt.payload.payload)
 
     #IPv-dependent
     try:
@@ -176,12 +209,12 @@ def packet2p0f(pkt):
         if option[0] == 'WScale':
             wscale = option[1]
 
-    olayout = set(map(lambda x: x[0], pkt.payload.options))
+    olayout = set(map(lambda x: x[0].lower(), pkt.payload.options))
 
-    return wsize, ttl, olen, mss, wscale, olayout
+    return (ver, ttl, olen, mss, wsize, wscale, olayout, pclass)
 
 if __name__ == '__main__':
-    """ This one is for testing"""
+    # This one is for testing
 
     pdb = p0fDatabase(conf.p0f_base)
     base = pdb.get_base()
