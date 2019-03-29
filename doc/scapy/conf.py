95e2f257bf3a70876207147e922be6259c3ccd51
Gabriel <gabriel@potter.fr>
2019-03-29 12:03:45 +0100

    Revert "Render SVGs in the PDF doc"

--- doc/scapy/conf.py
+++ doc/scapy/conf.py
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.imgconverter']  # Render SVG in PDF
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
