66fd5f90b80ae2099dcebac2561142255baa3b97
hissssst <csqcsqcsq@yandex.ru>
2019-04-01 22:40:27 +0300

    A bit of everything

--- scapy/modules/p0f.py
+++ scapy/modules/p0f.py
@@ -10,6 +10,7 @@ from scapy.modules import six
 from scapy.modules.six.moves import map, range
 
 conf.p0f_base = '/etc/p0f/p0f.fp'
+mtu = 123231231
 
 def lparse(line, n, default='', splitchar=':'):
     """Function for nice parcing of 'a:b:c:d:e' lines"""
@@ -24,9 +25,11 @@ class p0fDatabase(KnowledgeBase):
     nested dictionary structure
     self.base = {
         'module' (str): {
-            'moduledir' (str): {'sig' (str): 'label' (str)}
+            'moduledir' (str): {'sig' (str): labelnum (int)}
             }
         }
+
+    self.labels = ['label' (str), ...]
     """
 
     def lazy_init(self):
@@ -44,7 +47,9 @@ class p0fDatabase(KnowledgeBase):
 
         #try:
         self.base = {}
+        self.labels = []
         self.parse_file(f)
+        self.labels = tuple(self.labels)
         #except Exception:
         #    warning("Can't parse p0f database (new p0f version ?)")
         #    self.base = None
@@ -80,7 +85,9 @@ class p0fDatabase(KnowledgeBase):
                 value = value.strip()
 
                 if param =='label':
-                    currlabel = value
+                    l = len(self.labels)
+                    self.labels.append(value)
+                    currlabel = l
     
                 elif param == 'sig':
                     currdict[value] = currlabel
@@ -88,30 +95,44 @@ class p0fDatabase(KnowledgeBase):
                 elif param == 'classes':
                     currdict[currlabel] = value
 
-    def p0f_correl(self, module, moduledir, pkt2p0f_out, pquirks):
+    def p0f_tcp_correl(self, moduledir, pkt2p0f_out, olayout, quirks):
+        """corellates the tcp-packet with p0f database"""
         
         # prepare values
-        ver, ttl, olen, mss, wsize, wscale, olayout, pclass = pkt2p0f_out
-        sigdict = self.base[module][moduledir]
+        ver, ttl, olen, mss, wsize, wscale, pclass = pkt2p0f_out
+        sigdict = self.base['tcp'][moduledir]
 
-        for sig, label in sigdict.items():
+        for sig, numlabel in sigdict.items():
+            label = self.labels[numlabel]
             hits = 0
 
             # 's' stands for 'signature_'
             sver, sttl, solen, smss, swsize_sscale, solayout, squirks, spclass = lparse(sig, 8)
-            swsize, sscale = swsize_sscale.split(':')
+            swsize, sscale = swsize_sscale.split(',')
+
             # compares main values
             hits += (sver == '*') or (int(sver) == ver)
             hits += (int(sttl[:-1]) >= ttl) if sttl[-1] == '-' else int(sttl) == ttl
             hits += int(solen) == olen
             hits += (smss == '*') or (int(smss) == mss)
-            hits += (swsize == '*') or (eval(swsize) >= wsize)
+            hits += (swsize == '*') or (type(eval(swsize)) == str) or (eval(swsize) >= wsize)
             hits += (sscale == '*') or (int(sscale) == wscale)
+            hits += (spclass == '*') or (spclass == pclass == 0) or (spclass == '+' and pclass)
 
             # compares quirks
-            quirks = quirks.split(',')
-            correl = sum(quirks_correl(pquircks, quirks)) / len(quirks)
+            squirks = squirks.split(',')
+            if squirks[0]:
+                qhits = sum(quirks_correl(quirks, squirks))
+            else:
+                qhits = 0
+            # compares layaouts
+            # TODO
 
+            q_correl = qhits / len(squirks)
+            h_correl = hits / 7
+            l_correl = 1
+
+            yield q_correl, h_correl, l_correl, label
 
 p0fdb = p0fDatabase(conf.p0f_base)
 
@@ -152,13 +173,20 @@ Quirks_p0f = {
     'exws'   : 15, # excessive window scaling factor ( > 14)
     'bad'    : 16} # malformed tcp options
 
+Layouts_p0f = {
+        
+        
+}
+
 def quirks_correl(qint, quirks):
     for quirk in quirks:
-        yield bool((1 << Quirks_p0f[quirk]) & quirks)
+        yield bool((1 << Quirks_p0f[quirk]) & qint)
 
 def packet2quirks(pkt):
-    """requires preprocessed packet"""
-    """not done yet"""
+    """
+        requires preprocessed packet
+        not done yet
+    """
 
     quirks = 0
 
@@ -174,6 +202,11 @@ def packet2quirks(pkt):
 
     if pkt.flags.ECN:
         quircks += 1 << Quirks_p0f['ecn']
+
+
+def packet2olayout(pkt):
+    """requires preprocessed packet"""
+    pass
 
 
 def packet2p0f(pkt):
@@ -209,13 +242,16 @@ def packet2p0f(pkt):
         if option[0] == 'WScale':
             wscale = option[1]
 
-    olayout = set(map(lambda x: x[0].lower(), pkt.payload.options))
-
-    return (ver, ttl, olen, mss, wsize, wscale, olayout, pclass)
+    return (ver, ttl, olen, mss, wsize, wscale, pclass)
 
 if __name__ == '__main__':
     # This one is for testing
 
     pdb = p0fDatabase(conf.p0f_base)
     base = pdb.get_base()
-
+    from time import time
+    to = time()
+    packet = IP(version=4, ttl=64)/TCP(options=[], window=128)
+    gen = pdb.p0f_tcp_correl('request', packet2p0f(packet), [], 0)
+    l = list(map(lambda x: (sum(x[:2]) / 3, x[3]), gen))
+    print(time() - to)
