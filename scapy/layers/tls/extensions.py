a37c9d42d692090267f20010a28df56517dd87f5
Justice Parham <justking100@gmail.com>
2019-06-18 08:34:53 -0500

    Updated formatting to comply with scapy

--- scapy/layers/tls/extensions.py
+++ scapy/layers/tls/extensions.py
@@ -153,15 +153,18 @@ class ServerName(Packet):
 class EncryptedServerName(Packet):
     name = "Encrypted HostName"
     fields_desc = [ShortEnumField("type", 0xffce, _tls_ext),
-                   ShortField("len", None),EnumField("cipher", None, _tls_cipher_suites),
+                   ShortField("len", None),
+                   EnumField("cipher", None, _tls_cipher_suites),
                    ShortEnumField("key_exchange_group", None, _tls_named_groups),
-                   FieldLenField("key_exchange_len", None, length_of="key_exchange",fmt="H"),
+                   FieldLenField("key_exchange_len", None,
+                               length_of="key_exchange",fmt="H"),
                    XStrLenField("key_exchange", "",
                                length_from=lambda pkt: pkt.key_exchange_len),
                    FieldLenField("record_digest_len", None, length_of="record_digest"),
                    XStrLenField("record_digest", "",
                                length_from=lambda pkt: pkt.record_digest_len),
-                   FieldLenField("encrypted_sni_len", None, length_of="encrypted_sni",fmt="H"),
+                   FieldLenField("encrypted_sni_len", None,
+                               length_of="encrypted_sni",fmt="H"),
                    XStrLenField("encrypted_sni", "",
                                length_from=lambda pkt: pkt.encrypted_sni_len)]
 	
@@ -199,15 +202,18 @@ class TLS_Ext_ServerName(TLS_Ext_PrettyPacketList):                 # RFC 4366
 class TLS_Ext_EncryptedServerName(TLS_Ext_PrettyPacketList):
     name = "TLS Extension - Encrypted Server Name"
     fields_desc = [ShortEnumField("type", 0xffce, _tls_ext),
-                   ShortField("len", None),EnumField("cipher", None, _tls_cipher_suites),
+                   ShortField("len", None),
+                   EnumField("cipher", None, _tls_cipher_suites),
                    ShortEnumField("key_exchange_group", None, _tls_named_groups),
-                   FieldLenField("key_exchange_len", None, length_of="key_exchange",fmt="H"),
+                   FieldLenField("key_exchange_len", None,
+                               length_of="key_exchange",fmt="H"),
                    XStrLenField("key_exchange", "",
                                length_from=lambda pkt: pkt.key_exchange_len),
                    FieldLenField("record_digest_len", None, length_of="record_digest"),
                    XStrLenField("record_digest", "",
                                length_from=lambda pkt: pkt.record_digest_len),
-                   FieldLenField("encrypted_sni_len", None, length_of="encrypted_sni",fmt="H"),
+                   FieldLenField("encrypted_sni_len", None,
+                               length_of="encrypted_sni",fmt="H"),
                    XStrLenField("encrypted_sni", "",
                                length_from=lambda pkt: pkt.encrypted_sni_len)]
 
