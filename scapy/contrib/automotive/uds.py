e8767f80db592198a3dbaaca481359ab321d10cc
superuserx <schroeder.alxndr@web.de>
2019-09-16 15:27:53 +0200

    final version uds

--- scapy/contrib/automotive/uds.py
+++ scapy/contrib/automotive/uds.py
@@ -9,6 +9,7 @@
 # scapy.contrib.status = loads
 
 import struct
+from itertools import product
 from scapy.fields import ByteEnumField, StrField, ConditionalField, \
     BitEnumField, BitField, XByteField, FieldListField, \
     XShortField, X3BytesField, XIntField, ByteField, \
@@ -1341,25 +1342,22 @@ class UDS_TesterPresentSender(PeriodicSenderThread):
         PeriodicSenderThread.__init__(self, sock, pkt, interval)
 
 
-def UDS_SessionEnumerator(sock, wait_after_reset=0.5):
-    """ Enumerates session ID's 0x0 - 0xff
+def UDS_SessionEnumerator(sock, session_range=range(0x100), reset_wait=1.5):
+    """ Enumerates session ID's in given range
         and returns list of UDS()/UDS_DSC() packets
         with valid session types
 
     Args:
         sock: socket where packets are sent
+        session_range: range for session ID's
+        reset_wait: wait time in sec after every packet
     """
-    pkts = UDS()/UDS_DSC(diagnosticSessionType=range(0xff))
-    results = []
-    for p in pkts:
-        a = sock.sr1(p, timeout=0.5, verbose=False)
-        if a != None:
-            if a.service == 0x50 or \
-                    a.negativeResponseCode not in [0x10, 0x11, 0x12]:
-                results.append(p)
-        sock.send(UDS() / UDS_ER())
-        time.sleep(wait_after_reset)
-    return results
+    pkts = (req for tup in product(UDS()/UDS_DSC(diagnosticSessionType=session_range),
+                                   UDS()/UDS_ER(resetType='hardReset')) for req in tup)
+    results, _ = sock.sr(pkts, timeout=len(session_range) * reset_wait * 2 + 1,
+                         verbose=False, inter=reset_wait)
+    return [req for req, res in results if req != None and req.service != 0x11 and
+            (res.service == 0x50 or res.negativeResponseCode not in [0x10, 0x11, 0x12])]
 
 
 def UDS_ServiceEnumerator(sock, session="DefaultSession", filter_responses=True):
@@ -1384,6 +1382,11 @@ def getTableEntry(tup):
 
     Args:
         tup: tuple with session and UDS response package
+
+    Example:
+        make_lined_table([('DefaultSession', UDS()/UDS_SAPR(),
+                           'ExtendedDiagnosticSession', UDS()/UDS_IOCBI())], 
+                           getTableEntry)
     """
     session, pkt = tup
     if pkt.service == 0x7f:
