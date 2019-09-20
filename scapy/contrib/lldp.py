e3d139ec04ff4c810aea0bdc166cb003350e62bd
Przemek Jacak <przemek.jacak@gmail.com>
2019-09-20 10:35:15 +0100

    As per IEEE 802.1AB-2016 section 8.2 LLDPDU format:
    If the End Of LLDPDU TLV is present, it shall be the last TLV in the LLDPDU.
    
    and 9.1.2.1 Normal LLDPDUs:
    
    constructs an LLDPDU as defined in 9.1, containing the following:
    a) The mandatory TLVs as specified in 8.2:
       1) Chassis ID TLV (see 8.5.2).
       2) Port ID TLV (see 8.5.3).
       3) Time To Live TLV, with the TTL value set equal to txTTL (see 8.5.4).
    b) Additional optional TLVs, from the basic management set or from one or more organizationally
    specific sets, as allowed by LLDPDU length restrictions and as selected in the LLDP local system
    MIB by network management (see Table 8-1).
    c) Optionally, an End Of LLDPDU TLV.
    
    in both cases End Of LLDPDU TLV is optional

--- scapy/contrib/lldp.py
+++ scapy/contrib/lldp.py
@@ -72,13 +72,6 @@ class LLDPInvalidFrameStructure(Scapy_Exception):
     pass
 
 
-class LLDPInvalidLastLayerException(Scapy_Exception):
-    """
-    in strict mode, last layer in frame must be of type LLDPDUEndOfLLDPDU
-    """
-    pass
-
-
 class LLDPMissingLowerLayer(Scapy_Exception):
     """
     first layer below first LLDPDU must be Ethernet or Dot1q
@@ -153,14 +146,6 @@ class LLDPDU(Packet):
 
     def post_build(self, pkt, pay):
 
-        last_layer = not pay
-        if last_layer and conf.contribs['LLDP'].strict_mode() and \
-                type(self).__name__ != LLDPDUEndOfLLDPDU.__name__:
-            raise LLDPInvalidLastLayerException('Last layer must be instance '
-                                                'of LLDPDUEndOfLLDPDU - '
-                                                'got {}'.
-                                                format(type(self).__name__))
-
         under_layer = self.underlayer
 
         if under_layer is None:
@@ -198,10 +183,9 @@ class LLDPDU(Packet):
         standard_frame_structure = [LLDPDUChassisID.__name__,
                                     LLDPDUPortID.__name__,
                                     LLDPDUTimeToLive.__name__,
-                                    '<...>',
-                                    LLDPDUEndOfLLDPDU.__name__]
+                                    '<...>' ]
 
-        if len(structure_description) < 4:
+        if len(structure_description) < 3:
             raise LLDPInvalidFrameStructure(
                 'Invalid frame structure.\ngot: {}\nexpected: '
                 '{}'.format(' '.join(structure_description),
@@ -217,12 +201,6 @@ class LLDPDU(Packet):
                     '{}'.format(' '.join(structure_description),
                                 ' '.join(standard_frame_structure)))
 
-        if structure_description[-1] != standard_frame_structure[-1]:
-            raise LLDPInvalidFrameStructure(
-                'Invalid frame structure.\ngot: {}\nexpected: '
-                '{}'.format(' '.join(structure_description),
-                            ' '.join(standard_frame_structure)))
-
     @staticmethod
     def _tlv_multiplicities_check(tlv_type_count):
         """
@@ -232,7 +210,7 @@ class LLDPDU(Packet):
 
         # * : 0..n, 1 : one and only one.
         standard_multiplicities = {
-            LLDPDUEndOfLLDPDU.__name__: 1,
+            LLDPDUEndOfLLDPDU.__name__: '*',
             LLDPDUChassisID.__name__: 1,
             LLDPDUPortID.__name__: 1,
             LLDPDUTimeToLive.__name__: 1,
