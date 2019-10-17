9c573157de36d649c8a0feba01212a77bd35187a
superuserx <schroeder.alxndr@web.de>
2019-10-17 13:28:17 +0200

    ecu/uds fix

--- scapy/contrib/automotive/uds.py
+++ scapy/contrib/automotive/uds.py
@@ -102,7 +102,7 @@ class UDS(ISOTP):
         if self.service == (other.service + 0x40):
             if isinstance(self.payload, NoPayload) or \
                     isinstance(other.payload, NoPayload):
-                return len(self) == len(other)
+                return len(self) <= len(other)
             else:
                 return self.payload.answers(other.payload)
         return False
