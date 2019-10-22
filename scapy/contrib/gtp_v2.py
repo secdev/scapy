9e63797e6a0a4b4d3e04cb3757b6f460cfe8b1aa
Kirill Spitsyn <ksp@google.com>
2019-10-22 10:31:18 -0700

    Add all possible cause values from the GTPv2 spec. (#2294)

--- scapy/contrib/gtp_v2.py
+++ scapy/contrib/gtp_v2.py
@@ -211,10 +211,6 @@ IEType = {1: "IMSI",
              161: "Max MBR/APN-AMBR (MMBR)"
           }
 
-CauseValues = {
-    16: "Request Accepted",
-}
-
 
 class GTPHeader(Packet):
     # 3GPP TS 29.060 V9.1.0 (2009-12)
@@ -456,13 +452,112 @@ class IE_IMSI(gtp.IE_Base):
                                      length_from=lambda x: x.length)]
 
 
+# 3GPP TS 29.274 v16.1.0 table 8.4-1
+CAUSE_VALUES = {
+    # 0: Reserved. Shall not be sent and if received the Cause shall be treated
+    #    as an invalid IE.
+    # 1: Reserved.
+    2: "Local Detach",
+    3: "Complete Detach",
+    4: "RAT changed from 3GPP to Non-3GPP",
+    5: "ISR deactivation",
+    6: "Error Indication received from RNC/eNodeB/S4-SGSN/MME",
+    7: "IMSI Detach Only",
+    8: "Reactivation Requested",
+    9: "PDN reconnection to this APN disallowed",
+    10: "Access changed from Non-3GPP to 3GPP",
+    11: "PDN connection inactivity timer expires",
+    12: "PGW not responding",
+    13: "Network Failure",
+    14: "QoS parameter mismatch",
+    15: "EPS to 5GS Mobility",
+    16: "Request accepted",
+    17: "Request accepted partially",
+    18: "New PDN type due to network preference",
+    19: "New PDN type due to single address bearer only",
+    # 20-63: Spare. This value range shall be used by Cause values in an
+    #        acceptance response/triggered message.
+    64: "Context Not Found",
+    65: "Invalid Message Format",
+    66: "Version not supported by next peer",
+    67: "Invalid length",
+    68: "Service not supported",
+    69: "Mandatory IE incorrect",
+    70: "Mandatory IE missing",
+    # 71: Shall not be used. See NOTE 2 and NOTE 3.
+    72: "System failure",
+    73: "No resources available",
+    74: "Semantic error in the TFT operation",
+    75: "Syntactic error in the TFT operation",
+    76: "Semantic errors in packet filter(s)",
+    77: "Syntactic errors in packet filter(s)",
+    78: "Missing or unknown APN",
+    # 79: Shall not be used. See NOTE 2 and NOTE 3.
+    80: "GRE key not found",
+    81: "Relocation failure",
+    82: "Denied in RAT",
+    83: "Preferred PDN type not supported",
+    84: "All dynamic addresses are occupied",
+    85: "UE context without TFT already activated",
+    86: "Protocol type not supported",
+    87: "UE not responding",
+    88: "UE refuses",
+    89: "Service denied",
+    90: "Unable to page UE",
+    91: "No memory available",
+    92: "User authentication failed",
+    93: "APN access denied - no subscription",
+    94: "Request rejected (reason not specified)",
+    95: "P-TMSI Signature mismatch",
+    96: "IMSI/IMEI not known",
+    97: "Semantic error in the TAD operation",
+    98: "Syntactic error in the TAD operation",
+    # 99: Shall not be used. See NOTE 2 and NOTE 3.
+    100: "Remote peer not responding",
+    101: "Collision with network initiated request",
+    102: "Unable to page UE due to Suspension",
+    103: "Conditional IE missing",
+    104: "APN Restriction type Incompatible with currently active PDN "
+         "connection",
+    105: "Invalid overall length of the triggered response message and a "
+         "piggybacked initial message",
+    106: "Data forwarding not supported",
+    107: "Invalid reply from remote peer",
+    108: "Fallback to GTPv1",
+    109: "Invalid peer",
+    110: "Temporarily rejected due to handover/TAU/RAU procedure in progress",
+    111: "Modifications not limited to S1-U bearers",
+    112: "Request rejected for a PMIPv6 reason",
+    113: "APN Congestion",
+    114: "Bearer handling not supported",
+    115: "UE already re-attached",
+    116: "Multiple PDN connections for a given APN not allowed",
+    117: "Target access restricted for the subscriber",
+    # 118: Shall not be used. See NOTE 2 and NOTE 3.
+    119: "MME/SGSN refuses due to VPLMN Policy",
+    120: "GTP-C Entity Congestion",
+    121: "Late Overlapping Request",
+    122: "Timed out Request",
+    123: "UE is temporarily not reachable due to power saving",
+    124: "Relocation failure due to NAS message redirection",
+    125: "UE not authorised by OCS or external AAA Server",
+    126: "Multiple accesses to a PDN connection not allowed",
+    127: "Request rejected due to UE capability",
+    128: "S1-U Path Failure",
+    129: "5GC not allowed",
+    # 130-239: Spare. For future use in a triggered/response message.
+    #          See NOTE 4.
+    # 240-255: Spare. For future use in an initial/request message. See NOTE 5.
+}
+
+
 class IE_Cause(gtp.IE_Base):
     name = "IE Cause"
     fields_desc = [ByteEnumField("ietype", 2, IEType),
                    ShortField("length", None),
                    BitField("CR_flag", 0, 4),
                    BitField("instance", 0, 4),
-                   ByteEnumField("Cause", 1, CauseValues),
+                   ByteEnumField("Cause", 1, CAUSE_VALUES),
                    BitField("SPARE", 0, 5),
                    BitField("PCE", 0, 1),
                    BitField("BCE", 0, 1),
