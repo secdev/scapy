0ebf40b4d1f64de96c24564916f83bc9c7870883
gpotter2 <gabriel@potter.fr>
2020-01-17 00:09:58 +0000

    Use rfc() in autodoc

--- scapy/fields.py
+++ scapy/fields.py
@@ -1740,6 +1740,7 @@ class BitEnumField(BitField, _EnumField):
         _EnumField.__init__(self, name, default, enum)
         self.rev = size < 0
         self.size = abs(size)
+        self.sz = self.size / 8.
 
     def any2i(self, pkt, x):
         return _EnumField.any2i(self, pkt, x)
@@ -1852,6 +1853,7 @@ class BitMultiEnumField(BitField, _MultiEnumField):
         _MultiEnumField.__init__(self, name, default, enum, depends_on)
         self.rev = size < 0
         self.size = abs(size)
+        self.sz = self.size / 8.
 
     def any2i(self, pkt, x):
         return _MultiEnumField.any2i(self, pkt, x)
