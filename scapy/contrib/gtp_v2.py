3438a425f51cb4d4de504d4020837b059fc43f09
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2020-05-18 11:35:47 -0400

    Revert ULI fields changes

--- scapy/contrib/gtp_v2.py
+++ scapy/contrib/gtp_v2.py
@@ -345,8 +345,8 @@ class ULI_CGI(ULI_Field):
     fields_desc = [
         gtp.TBCDByteField("MCC", "", 2),
         gtp.TBCDByteField("MNC", "", 1),
-        BitField("LAC", 0, 16),
-        BitField("CI", 0, 16),
+        ShortField("LAC", 0),
+        ShortField("CI", 0),
     ]
 
 
@@ -355,8 +355,8 @@ class ULI_SAI(ULI_Field):
     fields_desc = [
         gtp.TBCDByteField("MCC", "", 2),
         gtp.TBCDByteField("MNC", "", 1),
-        BitField("LAC", 0, 16),
-        BitField("SAC", 0, 16),
+        ShortField("LAC", 0),
+        ShortField("SAC", 0),
     ]
 
 
@@ -367,8 +367,8 @@ class ULI_RAI(ULI_Field):
         # MNC: if the third digit of MCC is 0xf, then the length of
         # MNC is 1 byte
         gtp.TBCDByteField("MNC", "", 1),
-        BitField("LAC", 0, 16),
-        BitField("RAC", 0, 16),
+        ShortField("LAC", 0),
+        ShortField("RAC", 0),
     ]
 
 
@@ -377,7 +377,7 @@ class ULI_TAI(ULI_Field):
     fields_desc = [
         gtp.TBCDByteField("MCC", "", 2),
         gtp.TBCDByteField("MNC", "", 1),
-        BitField("TAC", 0, 16),
+        ShortField("TAC", 0),
     ]
 
 
@@ -396,7 +396,7 @@ class ULI_LAI(ULI_Field):
     fields_desc = [
         gtp.TBCDByteField("MCC", "", 2),
         gtp.TBCDByteField("MNC", "", 1),
-        BitField("LAC", 0, 16),
+        ShortField("LAC", 0),
     ]
 
 
