680c7032cd0ac3ea1194656dace8951dc02131bf
Guillaume Valadon <guillaume.valadon@netatmo.com>
2020-07-16 13:35:56 +0200

    Update to FreeBSD 12.1

--- doc/vagrant_ci/provision_freebsd.sh
+++ doc/vagrant_ci/provision_freebsd.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 # This file is part of Scapy
 # See http://www.secdev.org/projects/scapy for more information
 # Copyright (C) Philippe Biondi <phil@secdev.org>
 # This program is published under a GPLv2 license
 
-sudo pkg install --yes git python2 python3 py27-virtualenv bash
+pkg install --yes git python2 python3 py27-virtualenv py27-sqlite3 py37-sqlite3 bash
+su - vagrant
 bash
 git clone https://github.com/secdev/scapy
 cd scapy
@@ -13,3 +14,4 @@ export PATH=/usr/local/bin/:$PATH
 virtualenv-2.7 -p python2.7 venv
 source venv/bin/activate
 pip install tox
+sudo chown -R vagrant:vagrant /home/vagrant/scapy
