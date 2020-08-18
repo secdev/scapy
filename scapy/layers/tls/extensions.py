4ef538352fa716bd669cfdbd7a3450f6fc2937b6
gpotter2 <gabriel@potter.fr>
2020-08-18 02:03:06 +0200

    Apply suggestion

--- scapy/layers/tls/extensions.py
+++ scapy/layers/tls/extensions.py
@@ -779,7 +779,7 @@ class _ExtensionsField(StrLenField):
         return len(self.i2m(pkt, i))
 
     def getfield(self, pkt, s):
-        tmp_len = self.length_from(pkt)
+        tmp_len = self.length_from(pkt) or 0
         if tmp_len <= 0:
             return s, []
         return s[tmp_len:], self.m2i(pkt, s[:tmp_len])
