fba21efcddb7fbb94c24befa18edf82c6c791e69
Guillaume Valadon <guillaume@valadon.net>
2020-10-14 11:33:54 +0200

    Python 3.9 support (#2851)
    * Unit tests linting for Python 3.9
    
    * Python 3.9 support

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Security",
         "Topic :: System :: Networking",
         "Topic :: System :: Networking :: Monitoring",
