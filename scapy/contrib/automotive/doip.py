796c7f8ea87f6bb36e3788a79ce9614f1b3355a1
Nils Weiss <nils@we155.de>
2020-11-25 10:28:56 +0100

    typing

--- scapy/contrib/automotive/doip.py
+++ scapy/contrib/automotive/doip.py
@@ -10,6 +10,7 @@
 
 import struct
 import socket
+import time
 
 from scapy.fields import ByteEnumField, ConditionalField, \
     XByteField, XShortField, XIntField, XShortEnumField, XByteEnumField, \
@@ -18,6 +19,9 @@ from scapy.packet import Packet, bind_layers
 from scapy.supersocket import StreamSocket
 from scapy.layers.inet import TCP, UDP
 from scapy.contrib.automotive.uds import UDS
+from scapy.error import Scapy_Exception
+from scapy.data import MTU
+from scapy.compat import Union, cast
 
 
 class DoIP(Packet):
@@ -48,15 +52,15 @@ class DoIP(Packet):
             0: "Incorrect pattern format", 1: "Unknown payload type",
             2: "Message too large", 3: "Out of memory",
             4: "Invalid payload length"
-        }), lambda p: p.payload_type in [0x0]),
+        }), lambda p: cast(DoIP, p).payload_type in [0x0]),
         ConditionalField(StrFixedLenField("vin", b"", 17),
-                         lambda p: p.payload_type in [3, 4]),
+                         lambda p: cast(DoIP, p).payload_type in [3, 4]),
         ConditionalField(XShortField("logical_address", 0),
-                         lambda p: p.payload_type in [4]),
+                         lambda p: cast(DoIP, p).payload_type in [4]),
         ConditionalField(StrFixedLenField("eid", b"", 6),
-                         lambda p: p.payload_type in [2, 4]),
+                         lambda p: cast(DoIP, p).payload_type in [2, 4]),
         ConditionalField(StrFixedLenField("gid", b"", 6),
-                         lambda p: p.payload_type in [4]),
+                         lambda p: cast(DoIP, p).payload_type in [4]),
         ConditionalField(XByteEnumField("further_action", 0, {
             0x00: "No further action required",
             0x01: "Reserved by ISO 13400", 0x02: "Reserved by ISO 13400",
@@ -68,7 +72,7 @@ class DoIP(Packet):
             0x0d: "Reserved by ISO 13400", 0x0e: "Reserved by ISO 13400",
             0x0f: "Reserved by ISO 13400",
             0x10: "Routing activation required to initiate central security",
-        }), lambda p: p.payload_type in [4]),
+        }), lambda p: cast(DoIP, p).payload_type in [4]),
         ConditionalField(XByteEnumField("vin_gid_status", 0, {
             0x00: "VIN and/or GID are synchronized",
             0x01: "Reserved by ISO 13400", 0x02: "Reserved by ISO 13400",
@@ -80,16 +84,16 @@ class DoIP(Packet):
             0x0d: "Reserved by ISO 13400", 0x0e: "Reserved by ISO 13400",
             0x0f: "Reserved by ISO 13400",
             0x10: "Incomplete: VIN and GID are NOT synchronized"
-        }), lambda p: p.payload_type in [4]),
+        }), lambda p: cast(DoIP, p).payload_type in [4]),
         ConditionalField(XShortField("source_address", 0),
-                         lambda p: p.payload_type in [5, 8, 0x8001, 0x8002, 0x8003]),  # noqa: E501
+                         lambda p: cast(DoIP, p).payload_type in [5, 8, 0x8001, 0x8002, 0x8003]),  # noqa: E501
         ConditionalField(XByteEnumField("activation_type", 0, {
             0: "Default", 1: "WWH-OBD", 0xe0: "Central security"
-        }), lambda p: p.payload_type in [5]),
+        }), lambda p: cast(DoIP, p).payload_type in [5]),
         ConditionalField(XShortField("logical_address_tester", 0),
-                         lambda p: p.payload_type in [6]),
+                         lambda p: cast(DoIP, p).payload_type in [6]),
         ConditionalField(XShortField("logical_address_doip_entity", 0),
-                         lambda p: p.payload_type in [6]),
+                         lambda p: cast(DoIP, p).payload_type in [6]),
         ConditionalField(XByteEnumField("routing_activation_response", 0, {
             0x00: "Routing activation denied due to unknown source address.",
             0x01: "Routing activation denied because all concurrently supported TCP_DATA sockets are registered and active.",  # noqa: E501
@@ -105,41 +109,42 @@ class DoIP(Packet):
             0x0f: "Reserved by ISO 13400.",
             0x10: "Routing successfully activated.",
             0x11: "Routing will be activated; confirmation required."
-        }), lambda p: p.payload_type in [6]),
+        }), lambda p: cast(DoIP, p).payload_type in [6]),
         ConditionalField(XIntField("reserved_iso", 0),
-                         lambda p: p.payload_type in [5, 6]),
+                         lambda p: cast(DoIP, p).payload_type in [5, 6]),
         ConditionalField(XIntField("reserved_oem", 0),
-                         lambda p: p.payload_type in [5, 6]),
+                         lambda p: cast(DoIP, p).payload_type in [5, 6]),
         ConditionalField(XByteEnumField("diagnostic_power_mode", 0, {
             0: "not ready", 1: "ready", 2: "not supported"
-        }), lambda p: p.payload_type in [0x4004]),
+        }), lambda p: cast(DoIP, p).payload_type in [0x4004]),
         ConditionalField(ByteEnumField("node_type", 0, {
             0: "DoIP gateway", 1: "DoIP node"
-        }), lambda p: p.payload_type in [0x4002]),
+        }), lambda p: cast(DoIP, p).payload_type in [0x4002]),
         ConditionalField(XByteField("max_open_sockets", 0),
-                         lambda p: p.payload_type in [0x4002]),
+                         lambda p: cast(DoIP, p).payload_type in [0x4002]),
         ConditionalField(XByteField("cur_open_sockets", 0),
-                         lambda p: p.payload_type in [0x4002]),
+                         lambda p: cast(DoIP, p).payload_type in [0x4002]),
         ConditionalField(IntField("max_data_size", 0),
-                         lambda p: p.payload_type in [0x4002]),
+                         lambda p: cast(DoIP, p).payload_type in [0x4002]),
         ConditionalField(XShortField("target_address", 0),
-                         lambda p: p.payload_type in [0x8001, 0x8002, 0x8003]),
+                         lambda p: cast(DoIP, p).payload_type in [0x8001, 0x8002, 0x8003]),  # noqa: E501
         ConditionalField(XByteEnumField("ack_code", 0, {0: "ACK"}),
-                         lambda p: p.payload_type in [0x8002]),
+                         lambda p: cast(DoIP, p).payload_type in [0x8002]),
         ConditionalField(ByteEnumField("nack_code", 0, {
             0x00: "Reserved by ISO 13400", 0x01: "Reserved by ISO 13400",
             0x02: "Invalid source address", 0x03: "Unknown target address",
             0x04: "Diagnostic message too large", 0x05: "Out of memory",
             0x06: "Target unreachable", 0x07: "Unknown network",
             0x08: "Transport protocol error"
-        }), lambda p: p.payload_type in [0x8003]),
+        }), lambda p: cast(DoIP, p).payload_type in [0x8003]),
     ]
 
     def answers(self, other):
+        # type: (Packet) -> int
         """DEV: true if self is an answer from other"""
         if other.__class__ == self.__class__:
             if self.payload_type == 0:
-                return True
+                return 1
 
             matches = [(4, 1), (4, 2), (4, 3), (6, 5), (8, 7),
                        (0x4002, 0x4001), (0x4004, 0x4003),
@@ -147,16 +152,18 @@ class DoIP(Packet):
             if (self.payload_type, other.payload_type) in matches:
                 if self.payload_type == 0x8001:
                     return self.payload.answers(other.payload)
-                return True
-        return False
+                return 1
+        return 0
 
     def hashret(self):
+        # type: () -> bytes
         if self.payload_type in [0x8001, 0x8002, 0x8003]:
             return bytes(self)[:2] + struct.pack(
                 "H", self.target_address ^ self.source_address)
         return bytes(self)[:2]
 
     def post_build(self, pkt, pay):
+        # type: (bytes, bytes) -> bytes
         """
         This will set the Field 'payload_length' to the correct value.
         """
@@ -168,10 +175,13 @@ class DoIP(Packet):
 
 class DoIPSocket(StreamSocket):
     def __init__(self, ip='127.0.0.1', port=13400, activate_routing=True,
-                 source_address=0xe80, activation_type=0):
+                 source_address=0xe80, target_address=0x4010,
+                 activation_type=0):
+        # type: (str, int, bool, int, int, int) -> None
         self.ip = ip
         self.port = port
         self.source_address = source_address
+        self.target_address = target_address
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -188,6 +198,26 @@ class DoIPSocket(StreamSocket):
                 print("Routing activated!")
             else:
                 print("Routing activation failure!")
+
+
+class UDS_DoIPSocket(DoIPSocket):
+    def send(self, x):
+        # type: (Union[Packet, bytes]) -> int
+        if not isinstance(x, UDS):
+            raise Scapy_Exception("Please provide a UDS packet")
+        try:
+            x.sent_time = time.time()
+        except AttributeError:
+            pass
+
+        return super(UDS_DoIPSocket, self).send(
+            DoIP(payload_type=0x8001, source_address=self.source_address,
+                 target_address=self.target_address) / x)
+
+    def recv(self, x=MTU):
+        # type: (int) -> Packet
+        pkt = super(UDS_DoIPSocket, self).recv(x)
+        return pkt[1]
 
 
 bind_layers(UDP, DoIP, sport=13400)
