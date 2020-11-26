53337388074ddefeb3ac8c0785c5c9e454df7d2c
Nils Weiss <nils@we155.de>
2020-11-26 10:14:49 +0100

    remove casts

--- scapy/contrib/automotive/doip.py
+++ scapy/contrib/automotive/doip.py
@@ -21,7 +21,7 @@ from scapy.layers.inet import TCP, UDP
 from scapy.contrib.automotive.uds import UDS
 from scapy.error import Scapy_Exception
 from scapy.data import MTU
-from scapy.compat import Union, cast
+from scapy.compat import Union
 
 
 class DoIP(Packet):
@@ -52,15 +52,15 @@ class DoIP(Packet):
             0: "Incorrect pattern format", 1: "Unknown payload type",
             2: "Message too large", 3: "Out of memory",
             4: "Invalid payload length"
-        }), lambda p: cast(DoIP, p).payload_type in [0x0]),
+        }), lambda p: p.payload_type in [0x0]),
         ConditionalField(StrFixedLenField("vin", b"", 17),
-                         lambda p: cast(DoIP, p).payload_type in [3, 4]),
+                         lambda p: p.payload_type in [3, 4]),
         ConditionalField(XShortField("logical_address", 0),
-                         lambda p: cast(DoIP, p).payload_type in [4]),
+                         lambda p: p.payload_type in [4]),
         ConditionalField(StrFixedLenField("eid", b"", 6),
-                         lambda p: cast(DoIP, p).payload_type in [2, 4]),
+                         lambda p: p.payload_type in [2, 4]),
         ConditionalField(StrFixedLenField("gid", b"", 6),
-                         lambda p: cast(DoIP, p).payload_type in [4]),
+                         lambda p: p.payload_type in [4]),
         ConditionalField(XByteEnumField("further_action", 0, {
             0x00: "No further action required",
             0x01: "Reserved by ISO 13400", 0x02: "Reserved by ISO 13400",
@@ -72,7 +72,7 @@ class DoIP(Packet):
             0x0d: "Reserved by ISO 13400", 0x0e: "Reserved by ISO 13400",
             0x0f: "Reserved by ISO 13400",
             0x10: "Routing activation required to initiate central security",
-        }), lambda p: cast(DoIP, p).payload_type in [4]),
+        }), lambda p: p.payload_type in [4]),
         ConditionalField(XByteEnumField("vin_gid_status", 0, {
             0x00: "VIN and/or GID are synchronized",
             0x01: "Reserved by ISO 13400", 0x02: "Reserved by ISO 13400",
@@ -84,16 +84,16 @@ class DoIP(Packet):
             0x0d: "Reserved by ISO 13400", 0x0e: "Reserved by ISO 13400",
             0x0f: "Reserved by ISO 13400",
             0x10: "Incomplete: VIN and GID are NOT synchronized"
-        }), lambda p: cast(DoIP, p).payload_type in [4]),
+        }), lambda p: p.payload_type in [4]),
         ConditionalField(XShortField("source_address", 0),
-                         lambda p: cast(DoIP, p).payload_type in [5, 8, 0x8001, 0x8002, 0x8003]),  # noqa: E501
+                         lambda p: p.payload_type in [5, 8, 0x8001, 0x8002, 0x8003]),  # noqa: E501
         ConditionalField(XByteEnumField("activation_type", 0, {
             0: "Default", 1: "WWH-OBD", 0xe0: "Central security"
-        }), lambda p: cast(DoIP, p).payload_type in [5]),
+        }), lambda p: p.payload_type in [5]),
         ConditionalField(XShortField("logical_address_tester", 0),
-                         lambda p: cast(DoIP, p).payload_type in [6]),
+                         lambda p: p.payload_type in [6]),
         ConditionalField(XShortField("logical_address_doip_entity", 0),
-                         lambda p: cast(DoIP, p).payload_type in [6]),
+                         lambda p: p.payload_type in [6]),
         ConditionalField(XByteEnumField("routing_activation_response", 0, {
             0x00: "Routing activation denied due to unknown source address.",
             0x01: "Routing activation denied because all concurrently supported TCP_DATA sockets are registered and active.",  # noqa: E501
@@ -109,34 +109,34 @@ class DoIP(Packet):
             0x0f: "Reserved by ISO 13400.",
             0x10: "Routing successfully activated.",
             0x11: "Routing will be activated; confirmation required."
-        }), lambda p: cast(DoIP, p).payload_type in [6]),
+        }), lambda p: p.payload_type in [6]),
         ConditionalField(XIntField("reserved_iso", 0),
-                         lambda p: cast(DoIP, p).payload_type in [5, 6]),
+                         lambda p: p.payload_type in [5, 6]),
         ConditionalField(XIntField("reserved_oem", 0),
-                         lambda p: cast(DoIP, p).payload_type in [5, 6]),
+                         lambda p: p.payload_type in [5, 6]),
         ConditionalField(XByteEnumField("diagnostic_power_mode", 0, {
             0: "not ready", 1: "ready", 2: "not supported"
-        }), lambda p: cast(DoIP, p).payload_type in [0x4004]),
+        }), lambda p: p.payload_type in [0x4004]),
         ConditionalField(ByteEnumField("node_type", 0, {
             0: "DoIP gateway", 1: "DoIP node"
-        }), lambda p: cast(DoIP, p).payload_type in [0x4002]),
+        }), lambda p: p.payload_type in [0x4002]),
         ConditionalField(XByteField("max_open_sockets", 0),
-                         lambda p: cast(DoIP, p).payload_type in [0x4002]),
+                         lambda p: p.payload_type in [0x4002]),
         ConditionalField(XByteField("cur_open_sockets", 0),
-                         lambda p: cast(DoIP, p).payload_type in [0x4002]),
+                         lambda p: p.payload_type in [0x4002]),
         ConditionalField(IntField("max_data_size", 0),
-                         lambda p: cast(DoIP, p).payload_type in [0x4002]),
+                         lambda p: p.payload_type in [0x4002]),
         ConditionalField(XShortField("target_address", 0),
-                         lambda p: cast(DoIP, p).payload_type in [0x8001, 0x8002, 0x8003]),  # noqa: E501
+                         lambda p: p.payload_type in [0x8001, 0x8002, 0x8003]),  # noqa: E501
         ConditionalField(XByteEnumField("ack_code", 0, {0: "ACK"}),
-                         lambda p: cast(DoIP, p).payload_type in [0x8002]),
+                         lambda p: p.payload_type in [0x8002]),
         ConditionalField(ByteEnumField("nack_code", 0, {
             0x00: "Reserved by ISO 13400", 0x01: "Reserved by ISO 13400",
             0x02: "Invalid source address", 0x03: "Unknown target address",
             0x04: "Diagnostic message too large", 0x05: "Out of memory",
             0x06: "Target unreachable", 0x07: "Unknown network",
             0x08: "Transport protocol error"
-        }), lambda p: cast(DoIP, p).payload_type in [0x8003]),
+        }), lambda p: p.payload_type in [0x8003]),
     ]
 
     def answers(self, other):
