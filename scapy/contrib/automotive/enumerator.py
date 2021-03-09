99558be12d495f872bba8d06481aacd3d627e5d6
Nils Weiss <nils@we155.de>
2021-03-09 09:32:58 +0100

    Split #3054: Add independant Graph file, including typing and unit tests (#3128)

--- scapy/contrib/automotive/enumerator.py
+++ scapy/contrib/automotive/enumerator.py
@@ -12,72 +12,7 @@ from scapy.error import Scapy_Exception, log_interactive, warning
 from scapy.utils import make_lined_table, SingleConversationSocket
 import scapy.modules.six as six
 from scapy.contrib.automotive.ecu import EcuState
-
-
-class Graph:
-    def __init__(self):
-        """
-        self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-        self.weights has all the weights between two nodes,
-        with the two nodes as a tuple as the key
-        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
-        """
-        self.edges = defaultdict(list)
-        self.weights = {}
-
-    def add_edge(self, from_node, to_node, weight=1):
-        # Note: assumes edges are bi-directional
-        self.edges[from_node].append(to_node)
-        self.edges[to_node].append(from_node)
-        self.weights[(from_node, to_node)] = weight
-        self.weights[(to_node, from_node)] = weight
-
-    @property
-    def nodes(self):
-        return self.edges.keys()
-
-    @staticmethod
-    def dijsktra(graph, initial, end):
-        # shortest paths is a dict of nodes
-        # whose value is a tuple of (previous node, weight)
-        shortest_paths = {initial: (None, 0)}
-        current_node = initial
-        visited = set()
-
-        while current_node != end:
-            visited.add(current_node)
-            destinations = graph.edges[current_node]
-            weight_to_current_node = shortest_paths[current_node][1]
-
-            for next_node in destinations:
-                weight = \
-                    graph.weights[(current_node, next_node)] + \
-                    weight_to_current_node
-                if next_node not in shortest_paths:
-                    shortest_paths[next_node] = (current_node, weight)
-                else:
-                    current_shortest_weight = shortest_paths[next_node][1]
-                    if current_shortest_weight > weight:
-                        shortest_paths[next_node] = (current_node, weight)
-
-            next_destinations = {node: shortest_paths[node] for node in
-                                 shortest_paths if node not in visited}
-            if not next_destinations:
-                return None
-            # next node is the destination with the lowest weight
-            current_node = min(next_destinations,
-                               key=lambda k: next_destinations[k][1])
-
-        # Work back through destinations in shortest path
-        path = []
-        while current_node is not None:
-            path.append(current_node)
-            next_node = shortest_paths[current_node][0]
-            current_node = next_node
-        # Reverse path
-        path.reverse()
-        return path
+from scapy.contrib.automotive.graph import Graph
 
 
 class Enumerator(object):
@@ -293,7 +228,7 @@ class Scanner(object):
             self.enumerators = [e(self.socket) for e in self.default_enumerator_clss]  # noqa: E501
         self.enumerator_classes = [e.__class__ for e in self.enumerators]
         self.state_graph = Graph()
-        self.state_graph.add_edge(EcuState(), EcuState())
+        self.state_graph.add_edge((EcuState(), EcuState()))
         self.configuration = \
             {"dynamic_timeout": kwargs.pop("dynamic_timeout", False),
              "enumerator_classes": self.enumerator_classes,
@@ -325,7 +260,7 @@ class Scanner(object):
                 "delay_state_change": self.configuration["delay_state_change"]}
 
     def get_state_paths(self):
-        paths = [Graph.dijsktra(self.state_graph, EcuState(), s)
+        paths = [Graph.dijkstra(self.state_graph, EcuState(), s)
                  for s in self.state_graph.nodes if s != EcuState()]
         return sorted([p for p in paths if p is not None] + [[EcuState()]],
                       key=lambda x: x[-1])
