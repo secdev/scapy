a19ebc9e247d32ac7e7e39ed3c28fb5783d76e4f
Guillaume Valadon <guillaume@valadon.net>
2021-03-28 14:46:43 +0200

    NetBSD provisioning updated

--- doc/vagrant_ci/provision_netbsd.sh
+++ doc/vagrant_ci/provision_netbsd.sh
@@ -5,15 +5,15 @@
 # Copyright (C) Philippe Biondi <phil@secdev.org>
 # This program is published under a GPLv2 license
 
-RELEASE="9.0_2020Q1"
+RELEASE="9.0_2020Q4"
 
 sudo -s
 unset PROMPT_COMMAND
 export PATH="/sbin:/usr/pkg/sbin:/usr/pkg/bin:$PATH"
 export PKG_PATH="http://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/${RELEASE}/All/"
 pkg_delete curl
-pkg_add git python27 python38 py27-virtualenv py27-sqlite3 py38-expat
-git -c http.sslVerify=false clone https://github.com/secdev/scapy
+pkg_add git python27 python38 py27-virtualenv py27-sqlite3 py38-sqlite3 py38-expat rust mozilla-rootcerts-openssl
+git clone https://github.com/secdev/scapy
 cd scapy
 virtualenv-2.7 venv
 . venv/bin/activate
