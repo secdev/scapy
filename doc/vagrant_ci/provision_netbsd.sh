006944125e0905530bd960d2b1ddead4bf0ab77f
Guillaume Valadon <guillaume@valadon.net>
2021-04-18 20:33:06 +0200

    NetBSD provisioning fixed

--- doc/vagrant_ci/provision_netbsd.sh
+++ doc/vagrant_ci/provision_netbsd.sh
@@ -12,10 +12,10 @@ unset PROMPT_COMMAND
 export PATH="/sbin:/usr/pkg/sbin:/usr/pkg/bin:$PATH"
 export PKG_PATH="http://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/${RELEASE}/All/"
 pkg_delete curl
-pkg_add git python27 python38 py27-virtualenv py27-sqlite3 py38-sqlite3 py38-expat rust mozilla-rootcerts-openssl
+pkg_add git python27 python38 py38-virtualenv py27-sqlite3 py38-sqlite3 py38-expat rust mozilla-rootcerts-openssl
 git clone https://github.com/secdev/scapy
 cd scapy
-virtualenv-2.7 venv
+virtualenv-3.8 venv
 . venv/bin/activate
 pip install tox
 chown -R vagrant:vagrant ../scapy/
