40cf09373065b8f15db88535424b6c226fd62b1d
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2021-12-21 08:31:11 +0100

    Simplify license
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

--- scapy/contrib/tcpros.py
+++ scapy/contrib/tcpros.py
@@ -1,21 +1,7 @@
 # This file is part of Scapy
-# Scapy is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# any later version.
-#
-# Scapy is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Scapy. If not, see <http://www.gnu.org/licenses/>.
-
-# Copyright (C) Alias Robotics
-#
-#   Author:
-#     - Víctor Mayoral Vilches <v.mayoralv@gmail.com>
+# See http://www.secdev.org/projects/scapy for more information
+# Copyright (C) Víctor Mayoral-Vilches <v.mayoralv@gmail.com>
+# This program is published under a GPLv2 license
 
 """
 TCPROS transport layer for ROS Melodic Morenia 1.14.5
