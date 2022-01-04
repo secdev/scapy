ad6409bfe02cb920a33c61455a6ee5b174017149
Adam Lee <adam-lee@users.noreply.github.com>
2022-01-03 18:44:37 -0800

    Fix a typo in the documentation
    This may be my dyslexic moment, but I believe it should read "mbps", not "mpbs".

--- scapy/sendrecv.py
+++ scapy/sendrecv.py
@@ -489,7 +489,7 @@ def sendpfast(x,  # type: _PacketIterable
     """Send packets at layer 2 using tcpreplay for performance
 
     :param pps:  packets per second
-    :param mpbs: MBits per second
+    :param mbps: MBits per second
     :param realtime: use packet's timestamp, bending time with real-time value
     :param loop: number of times to process the packet list
     :param file_cache: cache packets in RAM instead of reading from
