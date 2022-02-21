1b2cdb1ec658bf956700898cbb04edce8f40bce9
Nils Weiss <nils@we155.de>
2022-02-21 12:19:31 +0100

    UDS: add standalone 'get_security_access' function (UDS_SA enumerators)
    * Add standalone 'get_security_access' function for UDS_SA enumerators
    
    * minor enhancment to fix IndexError

--- scapy/contrib/automotive/uds_scan.py
+++ scapy/contrib/automotive/uds_scan.py
@@ -650,18 +650,20 @@ class UDS_SA_XOR_Enumerator(UDS_SAEnumerator, StateGenerator):
         if key_pkt is None:
             return False
 
-        last_seed_req = self._results[-1].req
-        last_state = self._results[-1].state
-
         try:
             res = sock.sr1(key_pkt, timeout=5, verbose=False)
             if sock.closed:
                 log_interactive.critical("[-] Socket closed during scan.")
                 raise Scapy_Exception("Socket closed during scan")
         except (OSError, ValueError, Scapy_Exception) as e:
-            if not self._populate_retry(last_state, last_seed_req):
-                log_interactive.critical(
-                    "[-] Exception during retry. This is bad")
+            try:
+                last_seed_req = self._results[-1].req
+                last_state = self._results[-1].state
+                if not self._populate_retry(last_state, last_seed_req):
+                    log_interactive.critical(
+                        "[-] Exception during retry. This is bad")
+            except IndexError:
+                log_interactive.warning("[-] Couldn't populate retry.")
             raise e
 
         return self.evaluate_security_access_response(
