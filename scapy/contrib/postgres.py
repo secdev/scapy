79787608fe0d86ed1058acdc14760a55b807d558
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-04-21 18:47:23 +1000

    Add more enumerations

--- scapy/contrib/postgres.py
+++ scapy/contrib/postgres.py
@@ -17,18 +17,72 @@
 # scapy.contrib.status = loads
 
 
-
-from typing import Dict
 from scapy.packet import Packet
-from scapy.fields import ByteField, CharEnumField, IntField, FieldLenField, PadField, StrField, StrLenField, XNBytesField
+from scapy.fields import ByteField, CharEnumField, IntEnumField, FieldLenField, PacketListField, StrLenField, XNBytesField
 
 
-BASE_PACKET_TAGS = {
-    b'E': 'Error',
-    b'R': 'Authentication',
-    b'S': 'Parameter Status',
+FRONTEND_MSG_TYPE = {
+    b'B' : 'Bind',
+    b'C': 'Close',
+    b'd': 'CopyData',
+    b'c': 'CopyDone',
+    b'f': 'CopyFail',
+    b'D': 'Describe',
+    b'E': 'Execute',
+    b'H': 'Flush',
+    b'F': 'FunctionCall',
+    b'P': 'Parse',
+    b'p': 'PasswordMessage',
     b'Q': 'Query',
+    b'S': 'Sync',
+    b'X': 'Terminate'
 }
+
+BACKEND_MSG_TYPE = {
+    b'R': 'Authentication',
+    b'K': 'BackendKeyData',
+    b'2': 'BindComplete',
+    b'3': 'CloseComplete',
+    b'C': 'CommandComplete',
+    b'd': 'CopyData', # backend and frontend message
+    b'c': 'CopyDone', # backend and frontend message
+    b'G': 'CopyInResponse',
+    b'H': 'CopyOutResponse',
+    b'W': 'CopyBothResponse',
+    b'D': 'DataRow',
+    b'I': 'EmptyQueryResponse',
+    b'E': 'ErrorResponse',
+    b'V': 'FunctionCallResponse',
+    b'v': 'NegotiateProtocolVersion',
+    b'n': 'NoData',
+    b'N': 'NoticeResponse',
+    b'A': 'NotificationResponse',
+    b't': 'ParameterDescription',
+    b'S': 'ParameterStatus',
+    b'1': 'ParseComplete',
+    b's': 'PortalSuspended',
+    b'Z': 'ReadyForQuery',
+    b'T': 'RowDescription'
+}
+
+TAG_TO_PACKET_CLS = dict() # Lazy-defined
+
+AUTH_CODES = {
+    0: 'AuthenticationOk',
+    1: 'AuthenticationKerberosV4',
+    2: 'AuthenticationKerberosV5',
+    3: 'AuthenticationCleartextPassword',
+    4: 'AuthenticationCryptPassword',
+    5: 'AuthenticationMD5Password',
+    6: 'AuthenticationSCMCredential',
+    7: 'AuthenticationGSS',
+    8: 'AuthenticationGSSContinue',
+    9: 'AuthenticationSSPI',
+    10: 'AuthenticationSASL',
+    11: 'AuthenticationSASLContinue',
+    12: 'AuthenticationSASLFinal'
+}
+
 
 class Startup(Packet):
     name = 'Startup Request Packet'
@@ -40,10 +94,47 @@ class Startup(Packet):
         ByteField("padding", 0x00)
     ]
 
+def determine_pg_field(pkt, lst, cur, remain):
+    if pkt.tag in TAG_TO_PACKET_CLS:
+        return TAG_TO_PACKET_CLS[pkt.tag]
+    else:
+        return Packet
+
 class BasePacket(Packet):
     name = 'Regular packet'
     fields_desc = [
-        CharEnumField("tag", b'R', BASE_PACKET_TAGS),
-        FieldLenField("len", None, count_of="options", fmt="I"),
-        StrLenField("payload", "\0", length_from = lambda pkt: pkt.len)
+        CharEnumField("tag", b'R', FRONTEND_MSG_TYPE),
+        FieldLenField("len", None, fmt="I"),
+        PacketListField("payload", [], next_cls_cb=determine_pg_field)
     ]
+
+class AuthenticationRequest(Packet):
+    name = 'Authentication Request'
+    fields_desc = [
+        CharEnumField("tag", b'R', BACKEND_MSG_TYPE),
+        FieldLenField("len", None, length_of="optional", fmt="I", adjust=lambda pkt, x: x + 8),
+        IntEnumField("method", default=0, enum=AUTH_CODES),
+        StrLenField("optional", None, length_from = lambda pkt: pkt.len - 8)
+    ]
+
+class ParameterStatus(Packet):
+    name = 'Parameter Status'
+    fields_desc = [
+        CharEnumField("tag", b'S', BACKEND_MSG_TYPE),
+        FieldLenField("len", None, length_of="keyvalue", fmt="I", adjust=lambda pkt, x: x + 8),
+        StrLenField("keyvalue", "", length_from = lambda pkt: pkt.len - 8),
+    ]
+
+class ErrorResponse(Packet):
+    name = 'Error Response'
+    fields_desc = [
+        ByteField("tag", b'E'),
+        FieldLenField("len", None, length_of="optional", fmt="I", adjust=lambda pkt, x: x + 4),
+        IntEnumField("method", default=0, enum=AUTH_CODES),
+        StrLenField("optional", "\0", length_from = lambda pkt: pkt.len - 4)
+    ]
+
+TAG_TO_PACKET_CLS = {
+    b'R': AuthenticationRequest,
+    b'S': ParameterStatus,
+}
--- test_pg.py
+++ test_pg.py
@@ -1,24 +1,38 @@
-from scapy.contrib.postgres import BasePacket, Startup
+from scapy.compat import raw
+from scapy.contrib.postgres import AuthenticationRequest, BasePacket, Startup, ErrorResponse
 from scapy.layers.inet import IP, TCP
 from scapy.layers.l2 import Ether
-from scapy.sendrecv import sendp, srp1
-from scapy.utils import hexdump
-
+from scapy.sendrecv import srp1, sendp
+import random
 
 o = {
-    'user': 'anthonyshaw',
-    'database': 'anthonyshaw',
-    'application_name': 'psql',
-    'client_encoding': 'UTF8'
+    b'user': b'postgres',
+    # b'database': b'postgres\xfe',
+    b'application_name': b'psql',
+    b'client_encoding': b'UTF8'
 }
 
 result = bytes()
 for k, v in o.items():
-    result += k.encode(encoding='ascii') + b'\0' + v.encode(encoding='ascii') + b'\0'
+    result += k + b'\0' + v + b'\0'
 
+sport = random.randint(1024,65535)
+dest = Ether()/IP(dst='192.168.86.237')
 
-packet = Ether()/IP(dst='192.168.86.227')/TCP(dport=5432, flags='S')/Startup(options=result)
-hexdump(packet)
+syn = TCP(sport=sport, dport=5432, flags='S', seq=0)
+synack = srp1(dest/syn)
 
-srp1(packet)
+my_ack = synack.seq + 1
+# ACK
+ack = TCP(sport=sport, dport=5432, flags='A', seq=1, ack=my_ack)
+sendp(dest/ack)
 
+startup = TCP(sport=sport, dport=5432, flags='PA', seq=1, ack=my_ack)/Startup(options=result)
+
+r = srp1(dest/startup)
+
+auth_response = BasePacket(r.payload.payload.payload.load)
+
+auth_response.show()
+
+sendp(dest/TCP(sport=sport, dport=5432, flags='F'))
