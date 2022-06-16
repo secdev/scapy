6616370a414d04d796b2890ae8002ce86bd623ba
gpotter2 <gabriel@potter.fr>
2022-06-16 23:56:38 +0200

    Reorder version detection
    First check if we are in a git archive, THEN try to call git THEN use
    VERSION

--- scapy/main.py
+++ scapy/main.py
@@ -691,7 +691,7 @@ def interact(mydict=None, argv=None, mybanner=None, loglevel=logging.INFO):
                 cfg.TerminalInteractiveShell.confirm_exit = False
                 cfg.TerminalInteractiveShell.separate_in = u''
             if int(IPython.__version__[0]) >= 6:
-                cfg.TerminalInteractiveShell.term_title_format = ("Scapy v%s" %
+                cfg.TerminalInteractiveShell.term_title_format = ("Scapy %s" %
                                                                   conf.version)
                 # As of IPython 6-7, the jedi completion module is a dumpster
                 # of fire that should be scrapped never to be seen again.
