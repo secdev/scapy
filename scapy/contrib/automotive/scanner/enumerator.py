0b030308134905fd9eb5f9064484e48f5312b227
Nils Weiss <nils@we155.de>
2022-06-23 18:37:20 +0200

    Add count parameter to enumerators in automotive scanners (#3658)

--- scapy/contrib/automotive/scanner/enumerator.py
+++ scapy/contrib/automotive/scanner/enumerator.py
@@ -69,6 +69,7 @@ class ServiceEnumerator(AutomotiveTestCase):
     _supported_kwargs_doc = AutomotiveTestCase._supported_kwargs_doc + """
         :param timeout: Timeout until a response will arrive after a request
         :type timeout: integer or float
+        :param integer count: Number of request to be sent in one execution
         :param int execution_time: Time in seconds until the execution of
                                    this enumerator is stopped.
         :param state_allow_list: List of EcuState objects or EcuState object
@@ -233,6 +234,7 @@ class ServiceEnumerator(AutomotiveTestCase):
         # type: (_SocketUnion, EcuState, Any) -> None
         self.check_kwargs(kwargs)
         timeout = kwargs.pop('timeout', 1)
+        count = kwargs.pop('count', None)
         execution_time = kwargs.pop("execution_time", 1200)
 
         state_block_list = kwargs.get('state_block_list', list())
@@ -267,6 +269,14 @@ class ServiceEnumerator(AutomotiveTestCase):
                 log_interactive.debug("[i] Stop test_case execution because "
                                       "of response evaluation")
                 return
+
+            if count is not None:
+                if count <= 0:
+                    log_interactive.debug(
+                        "[i] Finished execution count of enumerator")
+                    return
+                else:
+                    count -= 1
 
             if (start_time + execution_time) < time.time():
                 log_interactive.debug(
