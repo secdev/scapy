de9dc66b2cc5c7587f31234aa7f97b1c7f523627
gpotter2 <gabriel@potter.fr>
2022-06-27 11:30:42 +0200

    Apply guedou suggestions

--- scapy/contrib/aoe.py
+++ scapy/contrib/aoe.py
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0-only
 # This file is part of Scapy
 # See https://scapy.net/ for more information
 # Copyright (C) 2018 antoine.torre <torreantoine1@gmail.com>
