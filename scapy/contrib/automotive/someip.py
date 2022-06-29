91602689a9928d3943605502ea7a86abbdc8e3b1
gpotter2 <gabriel@potter.fr>
2022-06-30 00:39:44 +0200

    Relicense someim under GPL2

--- scapy/contrib/automotive/someip.py
+++ scapy/contrib/automotive/someip.py
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: GPL-2.0-only OR MIT
+# SPDX-License-Identifier: GPL-2.0-only
 # This file is part of Scapy
 # See https://scapy.net/ for more information
 # Copyright (C) Sebastian Baar <sebastian.baar@gmx.de>
