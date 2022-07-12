2c92b0350ab5df8ea0adc164fb4441c979bec568
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-07-12 16:25:15 +1000

    Add Python 3.10 classifier

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Security",
         "Topic :: System :: Networking",
         "Topic :: System :: Networking :: Monitoring",
