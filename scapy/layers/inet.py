15ecb437cac2087bfeac55849146c94bb835ea90
Noam Rathaus <rathaus@gmail.com>
2022-11-07 11:47:27 +0200

    Improved patch

--- scapy/layers/inet.py
+++ scapy/layers/inet.py
@@ -7,8 +7,6 @@
 IPv4 (Internet Protocol v4).
 """
 
-from __future__ import absolute_import
-from __future__ import print_function
 import time
 import struct
 import re
@@ -402,15 +400,10 @@ class RandTCPOptions(VolatileValue):
                 rand_vals.append((oname, b''))
             else:
                 # Process the fmt arguments 1 by 1
-                structs = fmt[1:] if fmt[0] == "!" else fmt
+                structs = re.findall(r"!?([bBhHiIlLqQfdpP]|\d+[spx])", fmt)
                 rval = []
 
-                struct_list = list(structs) # split it char by char
-                if 's' in structs:
-                    # We need to split it with the number before the 's'
-                    struct_list = [structs]
-
-                for stru in struct_list:
+                for stru in structs:
                     stru = "!" + stru
                     if "s" in stru or "p" in stru:  # str / chr
                         v = bytes(RandBin(struct.calcsize(stru)))
