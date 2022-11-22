7d7530fa802307ed94f28624defa632e6b61befc
Noam Rathaus <noamr@ssd-disclosure.com>
2022-11-22 14:47:50 +0200

    Revert "Fix edge case"
    This reverts commit 051ba5faeb13c2e06d4dc66200bace002fa55468.

--- scapy/packet.py
+++ scapy/packet.py
@@ -864,7 +864,9 @@ class Packet(six.with_metaclass(Packet_metaclass,  # type: ignore
             if not field['done']:
                 field_fuzzed = self.locate_field(self, field['name'])
                 if "state_pos" not in dir(field_fuzzed):
-                    next_field['done'] = True # Mark it as done
+                    # Make sure next_field exists, as it might be the first element
+                    if next_field is not None:
+                        next_field['done'] = True # Mark it as done
                     continue
         
                 # If there are more than 128 combinations, do jumps
