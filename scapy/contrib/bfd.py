6faf853d366b5636378886013b5a10af62a2762b
ldaniel <ludovic.daniel@ssi.gouv.fr>
2023-02-17 11:53:50 +0100

    code cleaning

--- scapy/contrib/bfd.py
+++ scapy/contrib/bfd.py
@@ -1,4 +1,3 @@
-
 # SPDX-License-Identifier: GPL-2.0-only
 # This file is part of Scapy
 # See https://scapy.net/ for more information
@@ -11,17 +10,31 @@ BFD - Bidirectional Forwarding Detection - RFC 5880, 5881, 7130, 7881
 # scapy.contrib.description = BFD
 # scapy.contrib.status = loads
 
-debug_dissector = True
 
 from scapy.packet import Packet, bind_layers, bind_bottom_up
-from scapy.fields import BitField, BitEnumField, ByteEnumField, XNBytesField, XByteField, MultipleTypeField, IntField, FieldLenField, FlagsField, ByteField, PacketField, ConditionalField, StrFixedLenField
+from scapy.fields import (
+    BitField,
+    BitEnumField,
+    ByteEnumField,
+    XNBytesField,
+    XByteField,
+    MultipleTypeField,
+    IntField,
+    FieldLenField,
+    FlagsField,
+    ByteField,
+    PacketField,
+    ConditionalField,
+    StrFixedLenField,
+)
 from scapy.layers.inet import UDP
 
-_sta_names = {0: "AdminDown",
-              1: "Down",
-              2: "Init",
-              3: "Up",
-              }
+_sta_names = {
+    0: "AdminDown",
+    1: "Down",
+    2: "Init",
+    3: "Up",
+}
 
 
 # https://www.iana.org/assignments/bfd-parameters/bfd-parameters.xhtml
@@ -59,7 +72,7 @@ class OptionalAuth(Packet):
             None,
             fmt="B",
             length_of="auth_key",
-            adjust=lambda pkt, x: int(x + 3) if pkt.auth_type <= 1 else int(x + 8)
+            adjust=lambda pkt, x: int(x + 3) if pkt.auth_type <= 1 else int(x + 8),
         ),
         ByteField("auth_keyid", 1),
         ConditionalField(
@@ -67,25 +80,31 @@ class OptionalAuth(Packet):
             lambda pkt: pkt.auth_type > 1,
         ),
         ConditionalField(
-            IntField("sequence_number",0),
+            IntField("sequence_number", 0),
             lambda pkt: pkt.auth_type > 1,
         ),
         MultipleTypeField(
             [
                 (
-                    StrFixedLenField("auth_key", "", length_from=lambda pkt: pkt.auth_len),
+                    StrFixedLenField(
+                        "auth_key", "", length_from=lambda pkt: pkt.auth_len
+                    ),
                     lambda pkt: pkt.auth_type == 0,
                 ),
                 (
-                    XNBytesField("auth_key", 0x5f4dcc3b5aa765d61d8327deb882cf99, 16),
+                    XNBytesField("auth_key", 0x5F4DCC3B5AA765D61D8327DEB882CF99, 16),
                     lambda pkt: pkt.auth_type == 2 or pkt.auth_type == 3,
                 ),
                 (
-                    XNBytesField("auth_key", 0x5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8, 20),
+                    XNBytesField(
+                        "auth_key", 0x5BAA61E4C9B93F3F0682250B6CF8331B7EE68FD8, 20
+                    ),
                     lambda pkt: pkt.auth_type == 4 or pkt.auth_type == 5,
                 ),
             ],
-            StrFixedLenField("auth_key", "password", length_from=lambda pkt: pkt.auth_len),
+            StrFixedLenField(
+                "auth_key", "password", length_from=lambda pkt: pkt.auth_len
+            ),
         ),
     ]
 
@@ -103,7 +122,7 @@ class BFD(Packet):
             None,
             fmt="B",
             length_of="optional_auth",
-            adjust=lambda pkt, x: x + 24 # Add original size of BFD packet without auth
+            adjust=lambda pkt, x: x + 24,
         ),
         BitField("my_discriminator", 0x11111111, 32),
         BitField("your_discriminator", 0x22222222, 32),
@@ -112,8 +131,8 @@ class BFD(Packet):
         BitField("echo_rx_interval", 1000000000, 32),
         ConditionalField(
             PacketField("optional_auth", None, OptionalAuth),
-            lambda pkt: pkt.flags.names[2] == 'A',
-        )
+            lambda pkt: pkt.flags.names[2] == "A",
+        ),
     ]
 
     def mysummary(self):
@@ -124,10 +143,12 @@ class BFD(Packet):
         )
 
 
-for _bfd_port in [3784,          # single-hop BFD
-                  4784,          # multi-hop BFD
-                  6784,          # BFD for LAG a.k.a micro-BFD
-                  7784]:         # seamless BFD
+for _bfd_port in [
+    3784,  # single-hop BFD
+    4784,  # multi-hop BFD
+    6784,  # BFD for LAG a.k.a micro-BFD
+    7784,
+]:  # seamless BFD
     bind_bottom_up(UDP, BFD, dport=_bfd_port)
     bind_bottom_up(UDP, BFD, sport=_bfd_port)
     bind_layers(UDP, BFD, dport=_bfd_port, sport=_bfd_port)
