0f49a3db82d04d3e911a5d05b82f93b50a96d5d7
nibl <nicolas.bloyet@ekinops.com>
2023-03-06 11:31:11 +0100

    Flake8 conformation

--- scapy/layers/netflow.py
+++ scapy/layers/netflow.py
@@ -1272,8 +1272,10 @@ class NetflowHeaderV9(Packet):
                 return 0
 
         if self.count is None:
+            # https://www.rfc-editor.org/rfc/rfc3954#section-5.1
             count = sum(
-                sum(count_by_layer(self.getlayer(layer_cls, nth)) for nth in range(1, n + 1))
+                sum(count_by_layer(self.getlayer(layer_cls, nth))
+                    for nth in range(1, n + 1))
                 for layer_cls, n in Counter(self.layers()).items()
             )
             pkt = struct.pack("!H", count) + pkt[2:]
