6b339781f0006d937e17086d13b0ca85f1f204f3
Noam Rathaus <rathaus@gmail.com>
2023-03-13 19:03:14 +0200

    Less verbose

--- scapy/packet.py
+++ scapy/packet.py
@@ -2795,7 +2795,7 @@ def fuzz(p,  # type: _P
                     # Store the default value of the field
                     rnd.default = f.default
                     if rnd is not None:
-                        print(f"Adding: {f.name} with {f.default=}")
+                        # print(f"Adding: {f.name} with {f.default=}")
                         new_default_fields[f.name] = rnd
         # Process packets with MultipleTypeFields
         if multiple_type_fields:
