3bbe8c02c94baf109b83fe662a23e839f3a20cba
Deniz Bingol <dbingol@technica-engineering.net>
2023-03-23 11:31:24 +0100

    Corrected the syntax by tox.

--- scapy/contrib/pdu.py
+++ scapy/contrib/pdu.py
@@ -9,23 +9,26 @@
 from scapy.all import Packet, IntField, FieldLenField, StrLenField, bind_layers, PacketListField
 from scapy.layers.inet import UDP
 
+
 class PDU(Packet):
     """Protocol Data Unit Packet"""
     name = "PDU Packet"
     fields_desc = [
-         IntField("pkg_id", 0),
-         FieldLenField("data_length", None, "data", fmt="I"),
-         StrLenField("data", b'', length_from=lambda pkt: pkt.data_length)
-         ]
-    
+        IntField("pkg_id", 0),
+        FieldLenField("data_length", None, "data", fmt="I"),
+        StrLenField("data", b'', length_from=lambda pkt: pkt.data_length)
+        ]
+
     def extract_padding(self, s):
         return "", s
+
 
 class nPDU(Packet):
     """Container for n-PDU packets"""
     name = "nPDU Container"
     fields_desc = [
         PacketListField("pkgs", None, PDU)
-    ]
+        ]
+
 
 bind_layers(UDP, nPDU, dport=6661, sport=5551)
