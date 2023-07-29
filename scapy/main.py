d29cd0c46390e6de92f93817a47beafc818708f6
gpotter2 <10530980+gpotter2@users.noreply.github.com>
2023-07-29 11:26:41 +0200

    Add another common setting

--- scapy/main.py
+++ scapy/main.py
@@ -134,7 +134,7 @@ def _validate_local(k):
 # Default scapy prestart.py config file
 
 DEFAULT_PRESTART = """
-# Scapy pre-start config file
+# Scapy CLI 'pre-start' config file
 # see https://scapy.readthedocs.io/en/latest/api/scapy.config.html#scapy.config.Conf
 # for all available options
 
@@ -143,6 +143,9 @@ conf.interactive_shell = "auto"
 
 # color theme (DefaultTheme, BrightTheme, ColorOnBlackTheme, BlackAndWhite, ...)
 conf.color_theme = DefaultTheme()
+
+# disable INFO: tags related to dependencies missing
+# log_loading.setLevel(logging.WARNING)
 
 # force-use libpcap
 # conf.use_pcap = True
