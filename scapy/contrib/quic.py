b81221dc808f3dffd91d4dd660d3ba5388587b82
Rohan Paranjpe <48198937+rohanvp@users.noreply.github.com>
2023-12-11 01:16:14 -0500

    Update quic.py

--- scapy/contrib/quic.py
+++ scapy/contrib/quic.py
@@ -17,70 +17,67 @@ class QUIC(Packet):
 
     # def long_header_packet():
     #     # long header packet header
-    #     fields_desc = [ BitField("Header Form", size=1,default=1), # default=1 because in long header packet, this field is set to 1.
-    #                     BitField("Fixed Bit", size=1),
-    #                     BitField("Long Packet Type", size=2),
-    #                     BitField("Type-Specific Bits",size=4),  
-    #                     BitField("Version", size=32),
-    #                     BitField("Destination Connection ID Length",size=8),
-    #                     BitExtendedField("Destination Connection ID",None,1), # extension_bit=1 . more bytes following this field
-    #                     BitField("Source Connection ID Length",size=8),
-    #                     BitExtendedField("Source Connection ID",1) # extension_bit=1 . no bytes following this field.
+    #     fields_desc = [ BitField("headerform", size=1,default=1), # default=1 because in long header packet, this field is set to 1.
+    #                     BitField("fixedbit", size=1),
+    #                     BitField("longpackettype", size=2),
+    #                     BitField("typespecificbits",size=4),  
+    #                     BitField("version", size=32),
+    #                     BitField("dconnectionidlen",size=8),
+    #                     BitExtendedField("dconnectionid",None,1), # extension_bit=1 . more bytes following this field
+    #                     BitField("sconnectionidlen",size=8),
+    #                     BitExtendedField("sconnectionid",1) # extension_bit=1 . no bytes following this field.
     #                     #payload attached from here.                        
     #                 ]
         
     #     def optional_field(field):
 
-    #         if field=="Reserved Bits":
-    #             fields_desc.append(BitField("Reserved Bits", size=2))
+    #         if field=="reservedbits":
+    #             fields_desc.append(BitField("reservedbits", size=2))
             
-    #         elif field=="Packet Number Length":
-    #             fields_desc.append(BitField("Packet Number Length", size=2))
+    #         elif field=="packetnumberlength":
+    #             fields_desc.append(BitField("packetnumberlength", size=2))
             
-    #         # length of Packet Number and Payload in bytes
+    #         # length of packetnum and Payload in bytes
     #         elif field=="Length":
-    #             fields_desc.append(BitExtendedField("Length"))
+    #             fields_desc.append(BitExtendedField("length"))
 
-    #         elif field=="Packet Number":
-    #             fields_desc.append(BitExtendedField("Packet Number"))
-    
+    #         elif field=="packetnum":
+    #             fields_desc.append(BitExtendedField("packetnum"))
+
 
 
 # long header packets
 class VersionNegotiationPacket(Packet):
 
     # version negotiation packet header
-    fields_desc = [ BitField("Header Form", size=1,default=1), # default=1 because in long header packet, this field is set to 1.
-                    BitField("Unused", size=7),
-                    BitField("Version", size=32,default=0),
-                    BitField("Destination Connection ID Length",size=8),
-                    BitExtendedField("Destination Connection ID",None,1), # extension_bit=1 . more bytes following this field
-                    
-                    BoundStrLenField(name="Destination Connection ID"),
-                    
-                    BitField("Source Connection ID Length"),
-                    BitExtendedField("Source Connection ID",1), # extension_bit=1 . no bytes following this field.
-                    BitField("Supported Version",size=32),
+    fields_desc = [ BitField("headerform", size=1,default=1), # default=1 because in long header packet, this field is set to 1.
+                    BitField("unused", size=7),
+                    BitField("version", size=32,default=0),
+                    BitField("dconnectionidlen",size=8),
+                    BitExtendedField("dconnectionid",None,1), # extension_bit=1 . more bytes following this field
+                    BitField("sconnectionidlen"),
+                    BitExtendedField("sconnectionid",1), # extension_bit=1 . no bytes following this field.
+                    BitField("supportedversion",size=32),
                     #payload attached from here.                        
                 ]
 
 class InitialPacket(Packet):     
     
     # initial packet header
-    fields_desc = [ BitField("Header Form", size=1,default=1), # default=1 because in long header packet, this field is set to 1.
-                    BitField("Fixed Bit", size=1,default=1),
-                    BitField("Long Packet Type", size=2,default=0),
-                    BitField("Reserved Bits", size=2),
-                    BitField("Packet Number Length", size=2),
-                    BitField("Version", size=32),
-                    BitField("Destination Connection ID Length",size=8),
-                    BitExtendedField("Destination Connection ID",None,1), 
-                    BitField("Source Connection ID Length",size=8),
-                    BitExtendedField("Source Connection ID",1),
-                    BitExtendedField("Token Length",default=0),
-                    BitExtendedField("Token"),
-                    BitExtendedField("Length"),
-                    BitExtendedField("Packet Number"),
+    fields_desc = [ BitField("headerform", size=1,default=1), # default=1 because in long header packet, this field is set to 1.
+                    BitField("fixedbit", size=1,default=1),
+                    BitField("longpackettype", size=2,default=0),
+                    BitField("reservedbits", size=2),
+                    BitField("packetnumberlength", size=2),
+                    BitField("version", size=32),
+                    BitField("dconnectionidlen",size=8),
+                    BitExtendedField("dconnectionid",None,1), 
+                    BitField("sconnectionidlen",size=8),
+                    BitExtendedField("sconnectionid",1),
+                    BitExtendedField("tokenlength",default=0),
+                    BitExtendedField("token"),
+                    BitExtendedField("length"),
+                    BitExtendedField("packetnum"),
                     #payload attached from here.                        
                 ]
 
@@ -88,65 +85,65 @@ class InitialPacket(Packet):
 class _0RTTPacket(Packet):
     
     # 0-RTT Packet Header
-    fields_desc = [ BitField("Header Form", size=1,default=1),
-                    BitField("Fixed Bit", size=1,default=1),
-                    BitField("Long Packet Type", size=2,default=1),
-                    BitField("Reserved Bits", size=2),
-                    BitField("Packet Number Length", size=2),
-                    BitField("Version", size=32),
-                    BitField("Destination Connection ID Length",size=8),
-                    BitExtendedField("Destination Connection ID",None,1), 
-                    BitField("Source Connection ID Length",size=8),
-                    BitExtendedField("Source Connection ID",1),
-                    BitExtendedField("Length"),
-                    BitExtendedField("Packet Number"),
+    fields_desc = [ BitField("headerform", size=1,default=1),
+                    BitField("fixedbit", size=1,default=1),
+                    BitField("longpackettype", size=2,default=1),
+                    BitField("reservedbits", size=2),
+                    BitField("packetnumberlength", size=2),
+                    BitField("version", size=32),
+                    BitField("dconnectionidlen",size=8),
+                    BitExtendedField("dconnectionid",None,1), 
+                    BitField("sconnectionidlen",size=8),
+                    BitExtendedField("sconnectionid",1),
+                    BitExtendedField("length"),
+                    BitExtendedField("packetnum"),
                     #payload attached from here.                        
                 ]
 
 class HandshakePacket(Packet):
 
     # Handshake Packet Header
-    fields_desc = [ BitField("Header Form", size=1,default=1),
-                    BitField("Fixed Bit", size=1,default=1),
-                    BitField("Long Packet Type", size=2,default=2),
-                    BitField("Reserved Bits", size=2),
-                    BitField("Packet Number Length", size=2),
-                    BitField("Version", size=32),
-                    BitField("Destination Connection ID Length",size=8),
-                    BitExtendedField("Destination Connection ID",None,1), 
-                    BitField("Source Connection ID Length",size=8),
-                    BitExtendedField("Source Connection ID",1),
-                    BitExtendedField("Length"),
-                    BitExtendedField("Packet Number"),
+    fields_desc = [ BitField("headerform", size=1,default=1),
+                    BitField("fixedbit", size=1,default=1),
+                    BitField("longpackettype", size=2,default=2),
+                    BitField("reservedbits", size=2),
+                    BitField("packetnumberlength", size=2),
+                    BitField("version", size=32),
+                    BitField("dconnectionidlen",size=8),
+                    BitExtendedField("dconnectionid",None,1), 
+                    BitField("sconnectionidlen",size=8),
+                    BitExtendedField("sconnectionid",1),
+                    BitExtendedField("length"),
+                    BitExtendedField("packetnum"),
                     #payload attached from here.                        
                 ]
         
 
 class RetryPacket():
         
-    fields_desc = [ BitField("Header Form", size=1,default=1),
-                    BitField("Fixed Bit", size=1,default=1),
-                    BitField("Long Packet Type", size=2,default=3),
-                    BitField("Reserved Bits", size=2),
+    fields_desc = [ BitField("headerform", size=1,default=1),
+                    BitField("fixedbit", size=1,default=1),
+                    BitField("longpackettype", size=2,default=3),
+                    BitField("reservedbits", size=2),
                     BitField("Unused", size=4),
                     BitField("Version", size=32),
-                    BitField("Destination Connection ID Length",size=8),
-                    BitExtendedField("Destination Connection ID",None,1), 
-                    BitField("Source Connection ID Length",size=8),
-                    BitExtendedField("Retry Token"),
-                    BitField("Source Connection ID Length",size=128)
+                    BitField("dconnectionidlen",size=8),
+                    BitExtendedField("dconnectionid",None,1), 
+                    BitField("sconnectionidlen",size=8),
+                    BitExtendedField("retrytoken"),
+                    BitField("sconnectionidlen",size=128)
                     #payload attached from here.                        
                 ]
 
 # short header packet
 class _1RTTPacket():
-    fields_desc = [ BitField("Header Form", size=1,default=0),
-                    BitField("Fixed Bit", size=1,default=1),
-                    BitField("Spin Bit",size=1),
-                    BitField("Reserved Bit",size=2),
-                    BitField("Key Phase",size=1),
-                    BitField("Packet Number Length",size=2),
-                    BitExtendedField("Destination Connection ID",None,1),
-                    BitExtendedField("Packet Number",None,1),
+    fields_desc = [ BitField("headerform", size=1,default=0),
+                    BitField("fixedbit", size=1,default=1),
+                    BitField("spinbit",size=1),
+                    BitField("reservedbit",size=2),
+                    BitField("keyphase",size=1),
+                    BitField("packetnumberlength",size=2),
+                    BitExtendedField("dconnectionid",None,1),
+                    BitExtendedField("packetnum",None,1),
                     #payload attached from here.                        
                 ]
