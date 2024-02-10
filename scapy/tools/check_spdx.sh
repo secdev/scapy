908982e0055914ee747eebc7684a027382e22c53
gpotter2 <10530980+gpotter2@users.noreply.github.com>
2024-02-10 19:12:53 +0100

    Update check_spdx.sh to return 1 on failure

--- scapy/tools/check_spdx.sh
+++ scapy/tools/check_spdx.sh
@@ -9,11 +9,23 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$SCRIPT_DIR/../..
 
+# http://mywiki.wooledge.org/BashFAQ/024
+# This documents an absolutely WTF behavior of bash.
+set +m
+shopt -s lastpipe
+
 function check_path() {
     cd $ROOT_DIR
+    RCODE=0
     for ext in "${@:2}"; do
-        find $1 -name "*.$ext" -exec bash -c '[[ -z $(grep "SPDX" {}) ]] && echo "{}"' \;
+        find $1 -name "*.$ext" | while read f; do
+            if [[ -z $(grep "SPDX" $f) ]]; then
+                echo "$f"
+                RCODE=1
+            fi
+        done
     done
+    return $RCODE
 }
 
-check_path scapy py
+check_path scapy py || exit $?
