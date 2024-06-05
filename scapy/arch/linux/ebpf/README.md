bfae9bfec80c4ed31c8396b3f7adc1e15adc2f09
Guillaume Valadon <guillaune@valadon.net>
2024-06-05 08:10:31 +0200

    Interactive mode

--- scapy/arch/linux/ebpf/README.md
+++ scapy/arch/linux/ebpf/README.md
@@ -2,9 +2,36 @@
 
 This directory contains the Scapy eBPF playground that aims to get process information for each sent and received packets. This is still under developpment, and needs testing.
 
+## Testing
+
+
+A simple demo will sniff 10 IPv4 packets (excluding SSH), and display their summaries along with process information:
+```shell
+python3 -m scapy.arch.linux.ebpf
+Scapy + eBPF = <3
+
+1115177 b'curl' Ether / IP / TCP 10.211.55.4:51194 > 104.21.5.178:https S
+1115177 b'curl' Ether / IP / TCP 104.21.5.178:https > 10.211.55.4:51194 SA
+1115177 b'curl' Ether / IP / TCP 10.211.55.4:51194 > 104.21.5.178:https A
+1115177 b'curl' Ether / IP / TCP 10.211.55.4:51194 > 104.21.5.178:https PA / Raw
+1115177 b'curl' Ether / IP / TCP 104.21.5.178:https > 10.211.55.4:51194 A
+1115177 b'curl' Ether / IP / TCP 104.21.5.178:https > 10.211.55.4:51194 PA / Raw
+1115177 b'curl' Ether / IP / TCP 104.21.5.178:https > 10.211.55.4:51194 PA / Raw
+1115177 b'curl' Ether / IP / TCP 10.211.55.4:51194 > 104.21.5.178:https A
+1115177 b'curl' Ether / IP / TCP 10.211.55.4:51194 > 104.21.5.178:https A
+1115177 b'curl' Ether / IP / TCP 104.21.5.178:https > 10.211.55.4:51194 PA / Raw
+```
+
+An interactive mode is also available tp experiment with potential extended usages:
+```shell
+# python3 -m scapy.arch.linux.ebpf -i -H
+Welcome to Scapy (2.6.0rc1.dev28)
+Scapy + eBPF = <3
+>>> l = sniff(process_information=True)
+```
+
 ## TODO
 
-* [ ] Interactive mode via `__main__.py`
 * [ ] Improve Program_security_sk_classify_flow
   * [ ] ICMPv4 support
   * [ ] IPv6 & ICMPv6 support
--- scapy/arch/linux/ebpf/__main__.py
+++ scapy/arch/linux/ebpf/__main__.py
@@ -7,13 +7,64 @@
 Scapy eBPF native support - programs that retrieve process information
 """
 
-from .demo import sniff
+import argparse
+import sys
+
+from scapy.all import *
+
+from .process_information import KprobeProcessInformation
+
+
+@conf.commands.register
+def sniff(*args, **kwargs):
+    # type: (*Any, **Any) -> PacketList
+
+    kprobe = None
+    if "process_information" in kwargs:
+        del kwargs["process_information"]
+
+        if LINUX:
+            try:
+                kprobe = KprobeProcessInformation()
+                kprobe.start()
+
+                user_prn = lambda p: None
+                if "prn" in kwargs:
+                    user_prn = kwargs["prn"]
+
+                def _prn(packet):
+                    kprobe.lookup(packet)
+                    user_prn(packet)
+
+                kwargs["prn"] = _prn
+            except Scapy_Exception:
+                warning("Could not instanciate KprobeProcessInformation() !")
+        else:
+            warning("'process_information' is only support on Linux!")
+
+    sniffer = AsyncSniffer()
+    sniffer._run(*args, **kwargs)
+    if kprobe:
+        kprobe.stop()
+        kprobe.join()
+    return cast(PacketList, sniffer.results)
+
 
 if __name__ == "__main__":
-    print("Scapy + eBPF = <3\n")
+    banner = "Scapy + eBPF = <3"
 
-    packets = sniff(count=10, filter="ip and not port 22",
-                    process_information=True)
+    parser = argparse.ArgumentParser(description=banner)
+    parser.add_argument("-i", "--interactive", action="store_true", default=False)
+    args, left = parser.parse_known_args()
+    sys.argv = sys.argv[:1] + left
 
-    for p in packets:
-        print(p.comment, p.summary())
+    if args.interactive:
+        interact(mydict=globals(), mybanner=banner)
+    else:
+        print(banner + "\n")
+
+        packets = sniff(count=10, filter="ip and not port 22",
+                        process_information=True)
+
+        for p in packets:
+            print(p.comment, p.summary())
