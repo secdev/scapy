47d32ad4abc9930ae29bc43b4933f2ef7e237782
Wes <5124946+wesinator@users.noreply.github.com>
2024-06-07 22:44:04 -0400

    DNS - change rr 0 to IANA standard, ALL -> ANY

--- scapy/layers/dns.py
+++ scapy/layers/dns.py
@@ -72,7 +72,7 @@ from typing import (
 
 # https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-4
 dnstypes = {
-    0: "ANY",
+    0: "RESERVED",
     1: "A", 2: "NS", 3: "MD", 4: "MF", 5: "CNAME", 6: "SOA", 7: "MB", 8: "MG",
     9: "MR", 10: "NULL", 11: "WKS", 12: "PTR", 13: "HINFO", 14: "MINFO",
     15: "MX", 16: "TXT", 17: "RP", 18: "AFSDB", 19: "X25", 20: "ISDN",
@@ -91,7 +91,7 @@ dnstypes = {
 }
 
 
-dnsqtypes = {251: "IXFR", 252: "AXFR", 253: "MAILB", 254: "MAILA", 255: "ALL"}
+dnsqtypes = {251: "IXFR", 252: "AXFR", 253: "MAILB", 254: "MAILA", 255: "ANY"}
 dnsqtypes.update(dnstypes)
 dnsclasses = {1: 'IN', 2: 'CS', 3: 'CH', 4: 'HS', 255: 'ANY'}
 
