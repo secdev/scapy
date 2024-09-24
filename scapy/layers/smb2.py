df6289b12a426eef66bce26dc0c4ece0bc5835cd
gpotter2 <10530980+gpotter2@users.noreply.github.com>
2024-09-25 01:45:37 +0200

    Greatly improve SMB2 ACL support

--- scapy/layers/smb2.py
+++ scapy/layers/smb2.py
@@ -11,10 +11,11 @@ SMB (Server Message Block), also known as CIFS - version 2
     `SMB <https://scapy.readthedocs.io/en/latest/layers/smb.html>`_
 """
 
-import os
 import collections
 import functools
 import hashlib
+import os
+import re
 import struct
 
 from scapy.automaton import select_objects
@@ -758,7 +759,7 @@ class FileStreamInformation(Packet):
 
 class WINNT_SID_IDENTIFIER_AUTHORITY(Packet):
     fields_desc = [
-        StrFixedLenField("Value", b"", length=6),
+        StrFixedLenField("Value", b"\x00\x00\x00\x00\x00\x01", length=6),
     ]
 
     def default_payload_class(self, payload):
@@ -779,7 +780,7 @@ class WINNT_SID(Packet):
         ),
         FieldListField(
             "SubAuthority",
-            [],
+            [0],
             LEIntField("", 0),
             count_from=lambda pkt: pkt.SubAuthorityCount,
         ),
@@ -787,6 +788,22 @@ class WINNT_SID(Packet):
 
     def default_payload_class(self, payload):
         return conf.padding_layer
+
+    _SID_REG = re.compile(r"^S-(\d)-(\d+)((?:-\d+)*)$")
+
+    @staticmethod
+    def fromstr(x):
+        m = WINNT_SID._SID_REG.match(x)
+        if not m:
+            raise ValueError("Invalid SID format !")
+        rev, authority, subauthority = m.groups()
+        return WINNT_SID(
+            Revision=int(rev),
+            IdentifierAuthority=WINNT_SID_IDENTIFIER_AUTHORITY(
+                Value=struct.pack(">Q", int(authority))[2:]
+            ),
+            SubAuthority=[int(x) for x in subauthority[1:].split("-")],
+        )
 
     def summary(self):
         return "S-%s-%s%s" % (
@@ -796,6 +813,83 @@ class WINNT_SID(Packet):
             if self.SubAuthority
             else "",
         )
+
+
+# https://learn.microsoft.com/en-us/windows-server/identity/ad-ds/manage/understand-security-identifiers
+
+WELL_KNOWN_SIDS = {
+    # Universal well-known SID
+    "S-1-0-0": "Null SID",
+    "S-1-1-0": "Everyone",
+    "S-1-2-0": "Local",
+    "S-1-2-1": "Console Logon",
+    "S-1-3-0": "Creator Owner ID",
+    "S-1-3-1": "Creator Group ID",
+    "S-1-3-2": "Owner Server",
+    "S-1-3-3": "Group Server",
+    "S-1-3-4": "Owner Rights",
+    "S-1-4": "Non-unique Authority",
+    "S-1-5": "NT Authority",
+    "S-1-5-80-0": "All Services",
+    # NT well-known SIDs
+    "S-1-5-1": "Dialup",
+    "S-1-5-113": "Local account",
+    "S-1-5-114": "Local account and member of Administrators group",
+    "S-1-5-2": "Network",
+    "S-1-5-3": "Batch",
+    "S-1-5-4": "Interactive",
+    "S-1-5-6": "Service",
+    "S-1-5-7": "Anonymous Logon",
+    "S-1-5-8": "Proxy",
+    "S-1-5-9": "Enterprise Domain Controllers",
+    "S-1-5-10": "Self",
+    "S-1-5-11": "Authenticated Users",
+    "S-1-5-12": "Restricted Code",
+    "S-1-5-13": "Terminal Server User",
+    "S-1-5-14": "Remote Interactive Logon",
+    "S-1-5-15": "This Organization",
+    "S-1-5-17": "IUSR",
+    "S-1-5-18": "System (or LocalSystem)",
+    "S-1-5-19": "NT Authority (LocalService)",
+    "S-1-5-20": "Network Service",
+    "S-1-5-32-544": "Administrators",
+    "S-1-5-32-545": "Users",
+    "S-1-5-32-546": "Guests",
+    "S-1-5-32-547": "Power Users",
+    "S-1-5-32-548": "Account Operators",
+    "S-1-5-32-549": "Server Operators",
+    "S-1-5-32-550": "Print Operators",
+    "S-1-5-32-551": "Backup Operators",
+    "S-1-5-32-552": "Replicators",
+    "S-1-5-32-554": r"Builtin\Pre-Windows 2000 Compatible Access",
+    "S-1-5-32-555": r"Builtin\Remote Desktop Users",
+    "S-1-5-32-556": r"Builtin\Network Configuration Operators",
+    "S-1-5-32-557": r"Builtin\Incoming Forest Trust Builders",
+    "S-1-5-32-558": r"Builtin\Performance Monitor Users",
+    "S-1-5-32-559": r"Builtin\Performance Log Users",
+    "S-1-5-32-560": r"Builtin\Windows Authorization Access Group",
+    "S-1-5-32-561": r"Builtin\Terminal Server License Servers",
+    "S-1-5-32-562": r"Builtin\Distributed COM Users",
+    "S-1-5-32-568": r"Builtin\IIS_IUSRS",
+    "S-1-5-32-569": r"Builtin\Cryptographic Operators",
+    "S-1-5-32-573": r"Builtin\Event Log Readers",
+    "S-1-5-32-574": r"Builtin\Certificate Service DCOM Access",
+    "S-1-5-32-575": r"Builtin\RDS Remote Access Servers",
+    "S-1-5-32-576": r"Builtin\RDS Endpoint Servers",
+    "S-1-5-32-577": r"Builtin\RDS Management Servers",
+    "S-1-5-32-578": r"Builtin\Hyper-V Administrators",
+    "S-1-5-32-579": r"Builtin\Access Control Assistance Operators",
+    "S-1-5-32-580": r"Builtin\Remote Management Users",
+    "S-1-5-32-581": r"Builtin\Default Account",
+    "S-1-5-32-582": r"Builtin\Storage Replica Admins",
+    "S-1-5-32-583": r"Builtin\Device Owners",
+    "S-1-5-64-10": "NTLM Authentication",
+    "S-1-5-64-14": "SChannel Authentication",
+    "S-1-5-64-21": "Digest Authentication",
+    "S-1-5-80": "NT Service",
+    "S-1-5-80-0": "All Services",
+    "S-1-5-83-0": r"NT VIRTUAL MACHINE\Virtual Machines",
+}
 
 
 # [MS-DTYP] sect 2.4.3
@@ -816,6 +910,17 @@ _WINNT_ACCESS_MASK = {
 
 
 # [MS-DTYP] sect 2.4.4.1
+
+
+WINNT_ACE_FLAGS = {
+    0x01: "OBJECT_INHERIT",
+    0x02: "CONTAINER_INHERIT",
+    0x04: "NO_PROPAGATE_INHERIT",
+    0x08: "INHERIT_ONLY",
+    0x10: "INHERITED_ACE",
+    0x40: "SUCCESSFUL_ACCESS",
+    0x80: "FAILED_ACCESS",
+}
 
 
 class WINNT_ACE_HEADER(Packet):
@@ -850,15 +955,7 @@ class WINNT_ACE_HEADER(Packet):
             "AceFlags",
             0,
             8,
-            {
-                0x01: "OBJECT_INHERIT",
-                0x02: "CONTAINER_INHERIT",
-                0x04: "NO_PROPAGATE_INHERIT",
-                0x08: "INHERIT_ONLY",
-                0x10: "INHERITED_ACE",
-                0x40: "SUCCESSFUL_ACCESS",
-                0x80: "FAILED_ACCESS",
-            },
+            WINNT_ACE_FLAGS,
         ),
         LenField("AceSize", None, fmt="<H", adjust=lambda x: x + 4),
     ]
@@ -867,22 +964,23 @@ class WINNT_ACE_HEADER(Packet):
         return p[: self.AceSize - 4], p[self.AceSize - 4 :]
 
     # fmt: off
-    def toSDDL(self):
+    def extractData(self, accessMask=None):
         """
-        Return SDDL
+        Return the ACE data as usable data.
+
+        :param accessMask: context-specific flags for the ACE Mask.
         """
-        sid = self.payload.Sid.summary()
+        sid_string = self.payload.Sid.summary()
+        mask = self.payload.Mask
+        if accessMask is not None:
+            mask = FlagValue(mask, FlagsField("", 0, 32, accessMask).names)
         ace_flag_string = str(
-            FlagValue(
-                self.AceFlags,
-                ["OI", "CI", "NP", "IO", "ID", "SA", "FA"]
-            )
+            FlagValue(self.AceFlags, ["OI", "CI", "NP", "IO", "ID", "SA", "FA"])
         )
-        ace_rights = ""  # TODO
-        object_guid = ""  # TODO
-        inherit_object_guid = ""  # TODO
+        object_guid = getattr(self.payload, "ObjectType", "")
+        inherit_object_guid = getattr(self.payload, "InheritedObjectType", "")
         # ApplicationData -> conditional expression
-        condexpr = ""
+        cond_expr = None
         if hasattr(self.payload, "ApplicationData"):
             # Parse tokens
             res = []
@@ -925,7 +1023,23 @@ class WINNT_ACE_HEADER(Packet):
                     raise ValueError("Unhandled token type %s" % ct.TokenType)
             if len(res) != 1:
                 raise ValueError("Incomplete SDDL !")
-            condexpr = ";(%s)" % res[0]
+            cond_expr = "(%s)" % res[0]
+        return {
+            "ace-flags-string": ace_flag_string,
+            "sid-string": sid_string,
+            "mask": mask,
+            "object-guid": object_guid,
+            "inherited-object-guid": inherit_object_guid,
+            "cond-expr": cond_expr,
+        }
+    # fmt: on
+
+    def toSDDL(self, accessMask=None):
+        """
+        Return SDDL
+        """
+        data = self.extractData(accessMask=accessMask)
+        ace_rights = ""  # TODO
         if self.AceType in [0x9, 0xA, 0xB, 0xD]:  # Conditional ACE
             conditional_ace_type = {
                 0x09: "XA",
@@ -934,14 +1048,19 @@ class WINNT_ACE_HEADER(Packet):
                 0x0D: "ZA",
             }[self.AceType]
             return "D:(%s)" % (
-                ";".join([
-                    conditional_ace_type,
-                    ace_flag_string,
-                    ace_rights,
-                    object_guid,
-                    inherit_object_guid,
-                    sid
-                ]) + condexpr
+                ";".join(
+                    x
+                    for x in [
+                        conditional_ace_type,
+                        data["ace-flags-string"],
+                        ace_rights,
+                        str(data["object-guid"]),
+                        str(data["inherited-object-guid"]),
+                        data["sid-string"],
+                        data["cond-expr"],
+                    ]
+                    if x is not None
+                )
             )
         else:
             ace_type = {
@@ -955,18 +1074,20 @@ class WINNT_ACE_HEADER(Packet):
                 0x13: "SP",
             }[self.AceType]
             return "(%s)" % (
-                ";".join([
-                    ace_type,
-                    ace_flag_string,
-                    ace_rights,
-                    object_guid,
-                    inherit_object_guid,
-                    sid
-                ]) + condexpr
+                ";".join(
+                    x
+                    for x in [
+                        ace_type,
+                        data["ace-flags-string"],
+                        ace_rights,
+                        str(data["object-guid"]),
+                        str(data["inherited-object-guid"]),
+                        data["sid-string"],
+                        data["cond-expr"],
+                    ]
+                    if x is not None
+                )
             )
-
-
-# fmt: on
 
 
 # [MS-DTYP] sect 2.4.4.2
@@ -982,6 +1103,36 @@ class WINNT_ACCESS_ALLOWED_ACE(Packet):
 bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_ALLOWED_ACE, AceType=0x00)
 
 
+# [MS-DTYP] sect 2.4.4.3
+
+
+class WINNT_ACCESS_ALLOWED_OBJECT_ACE(Packet):
+    fields_desc = [
+        FlagsField("Mask", 0, -32, _WINNT_ACCESS_MASK),
+        FlagsField(
+            "Flags",
+            0,
+            -32,
+            {
+                0x00000001: "OBJECT_TYPE_PRESENT",
+                0x00000002: "INHERITED_OBJECT_TYPE_PRESENT",
+            },
+        ),
+        ConditionalField(
+            UUIDField("ObjectType", None, uuid_fmt=UUIDField.FORMAT_LE),
+            lambda pkt: pkt.Flags.OBJECT_TYPE_PRESENT,
+        ),
+        ConditionalField(
+            UUIDField("InheritedObjectType", None, uuid_fmt=UUIDField.FORMAT_LE),
+            lambda pkt: pkt.Flags.INHERITED_OBJECT_TYPE_PRESENT,
+        ),
+        PacketField("Sid", WINNT_SID(), WINNT_SID),
+    ]
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_ALLOWED_OBJECT_ACE, AceType=0x05)
+
+
 # [MS-DTYP] sect 2.4.4.4
 
 
@@ -990,6 +1141,16 @@ class WINNT_ACCESS_DENIED_ACE(Packet):
 
 
 bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_DENIED_ACE, AceType=0x01)
+
+
+# [MS-DTYP] sect 2.4.4.5
+
+
+class WINNT_ACCESS_DENIED_OBJECT_ACE(Packet):
+    fields_desc = WINNT_ACCESS_ALLOWED_OBJECT_ACE.fields_desc
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_DENIED_OBJECT_ACE, AceType=0x06)
 
 
 # [MS-DTYP] sect 2.4.4.17.4+
@@ -1054,7 +1215,7 @@ WINNT_APPLICATION_DATA_LITERAL_TOKEN.fields_desc = [
         lambda pkt: pkt.TokenType in [
             0x10,  # Unicode string
             0x18,  # Octet string
-            0xf8, 0xf8, 0xfa, 0xfb,  # Attribute tokens
+            0xf8, 0xf9, 0xfa, 0xfb,  # Attribute tokens
             0x50,  # Composite
         ]
     ),
@@ -1074,7 +1235,7 @@ WINNT_APPLICATION_DATA_LITERAL_TOKEN.fields_desc = [
                     StrLenFieldUtf16("value", b"", length_from=lambda pkt: pkt.length),
                     lambda pkt: pkt.TokenType in [
                         0x10,  # Unicode string
-                        0xf8, 0xf8, 0xfa, 0xfb,  # Attribute tokens
+                        0xf8, 0xf9, 0xfa, 0xfb,  # Attribute tokens
                     ]
                 ),
                 (
@@ -1091,7 +1252,7 @@ WINNT_APPLICATION_DATA_LITERAL_TOKEN.fields_desc = [
             StrFixedLenField("value", b"", length=0),
         ),
         lambda pkt: pkt.TokenType in [
-            0x01, 0x02, 0x03, 0x04, 0x10, 0x18, 0xf8, 0xf8, 0xfa, 0xfb, 0x50
+            0x01, 0x02, 0x03, 0x04, 0x10, 0x18, 0xf8, 0xf9, 0xfa, 0xfb, 0x50
         ]
     ),
     ConditionalField(
@@ -1154,7 +1315,7 @@ class WINNT_ACCESS_ALLOWED_CALLBACK_ACE(Packet):
 bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_ALLOWED_CALLBACK_ACE, AceType=0x09)
 
 
-# [MS-DTYP] sect 2.4.4.6
+# [MS-DTYP] sect 2.4.4.7
 
 
 class WINNT_ACCESS_DENIED_CALLBACK_ACE(Packet):
@@ -1164,15 +1325,163 @@ class WINNT_ACCESS_DENIED_CALLBACK_ACE(Packet):
 bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_DENIED_CALLBACK_ACE, AceType=0x0A)
 
 
+# [MS-DTYP] sect 2.4.4.8
+
+
+class WINNT_ACCESS_ALLOWED_CALLBACK_OBJECT_ACE(Packet):
+    fields_desc = WINNT_ACCESS_ALLOWED_OBJECT_ACE.fields_desc + [
+        PacketField(
+            "ApplicationData", WINNT_APPLICATION_DATA(), WINNT_APPLICATION_DATA
+        ),
+    ]
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_ALLOWED_CALLBACK_OBJECT_ACE, AceType=0x0B)
+
+
+# [MS-DTYP] sect 2.4.4.9
+
+
+class WINNT_ACCESS_DENIED_CALLBACK_OBJECT_ACE(Packet):
+    fields_desc = WINNT_ACCESS_DENIED_OBJECT_ACE.fields_desc + [
+        PacketField(
+            "ApplicationData", WINNT_APPLICATION_DATA(), WINNT_APPLICATION_DATA
+        ),
+    ]
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_ACCESS_DENIED_CALLBACK_OBJECT_ACE, AceType=0x0C)
+
+
 # [MS-DTYP] sect 2.4.4.10
 
 
-class WINNT_AUDIT_ACE(Packet):
+class WINNT_SYSTEM_AUDIT_ACE(Packet):
     fields_desc = WINNT_ACCESS_ALLOWED_ACE.fields_desc
 
 
-bind_layers(WINNT_ACE_HEADER, WINNT_AUDIT_ACE, AceType=0x02)
+bind_layers(WINNT_ACE_HEADER, WINNT_SYSTEM_AUDIT_ACE, AceType=0x02)
 
+
+# [MS-DTYP] sect 2.4.4.11
+
+
+class WINNT_SYSTEM_AUDIT_OBJECT_ACE(Packet):
+    # doc is wrong.
+    fields_desc = WINNT_ACCESS_ALLOWED_OBJECT_ACE.fields_desc
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_SYSTEM_AUDIT_OBJECT_ACE, AceType=0x07)
+
+
+# [MS-DTYP] sect 2.4.4.12
+
+
+class WINNT_SYSTEM_AUDIT_CALLBACK_ACE(Packet):
+    fields_desc = WINNT_SYSTEM_AUDIT_ACE.fields_desc + [
+        PacketField(
+            "ApplicationData", WINNT_APPLICATION_DATA(), WINNT_APPLICATION_DATA
+        ),
+    ]
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_SYSTEM_AUDIT_CALLBACK_ACE, AceType=0x0D)
+
+
+# [MS-DTYP] sect 2.4.4.13
+
+
+class WINNT_SYSTEM_MANDATORY_LABEL_ACE(Packet):
+    fields_desc = WINNT_SYSTEM_AUDIT_ACE.fields_desc
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_SYSTEM_MANDATORY_LABEL_ACE, AceType=0x11)
+
+
+# [MS-DTYP] sect 2.4.4.14
+
+
+class WINNT_SYSTEM_AUDIT_CALLBACK_OBJECT_ACE(Packet):
+    fields_desc = WINNT_SYSTEM_AUDIT_OBJECT_ACE.fields_desc
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_SYSTEM_AUDIT_CALLBACK_OBJECT_ACE, AceType=0x0F)
+
+# [MS-DTYP] sect 2.4.10.1
+
+
+class CLAIM_SECURITY_ATTRIBUTE_RELATIVE_V1(_NTLMPayloadPacket):
+    _NTLM_PAYLOAD_FIELD_NAME = "Data"
+    fields_desc = [
+        LEIntField("NameOffset", 0),
+        LEShortEnumField(
+            "ValueType",
+            0,
+            {
+                0x0001: "CLAIM_SECURITY_ATTRIBUTE_TYPE_INT64",
+                0x0002: "CLAIM_SECURITY_ATTRIBUTE_TYPE_UINT64",
+                0x0003: "CLAIM_SECURITY_ATTRIBUTE_TYPE_STRING",
+                0x0005: "CLAIM_SECURITY_ATTRIBUTE_TYPE_SID",
+                0x0006: "CLAIM_SECURITY_ATTRIBUTE_TYPE_BOOLEAN",
+                0x0010: "CLAIM_SECURITY_ATTRIBUTE_TYPE_OCTET_STRING",
+            },
+        ),
+        LEShortField("Reserved", 0),
+        FlagsField(
+            "Flags",
+            0,
+            -32,
+            {
+                0x0001: "CLAIM_SECURITY_ATTRIBUTE_NON_INHERITABLE",
+                0x0002: "CLAIM_SECURITY_ATTRIBUTE_VALUE_CASE_SENSITIVE",
+                0x0004: "CLAIM_SECURITY_ATTRIBUTE_USE_FOR_DENY_ONLY",
+                0x0008: "CLAIM_SECURITY_ATTRIBUTE_DISABLED_BY_DEFAULT",
+                0x0010: "CLAIM_SECURITY_ATTRIBUTE_DISABLED",
+                0x0020: "CLAIM_SECURITY_ATTRIBUTE_MANDATORY",
+            },
+        ),
+        LEIntField("ValueCount", 0),
+        FieldListField(
+            "ValueOffsets", [], LEIntField("", 0), count_from=lambda pkt: pkt.ValueCount
+        ),
+        _NTLMPayloadField(
+            "Data",
+            lambda pkt: 16 + pkt.ValueCount * 4,
+            [
+                ConditionalField(
+                    StrFieldUtf16("Name", b""),
+                    lambda pkt: pkt.NameOffset,
+                ),
+                # TODO: Values
+            ],
+            offset_name="Offset",
+        ),
+    ]
+
+
+# [MS-DTYP] sect 2.4.4.15
+
+
+class WINNT_SYSTEM_RESOURCE_ATTRIBUTE_ACE(Packet):
+    fields_desc = WINNT_ACCESS_ALLOWED_ACE.fields_desc + [
+        PacketField(
+            "AttributeData",
+            CLAIM_SECURITY_ATTRIBUTE_RELATIVE_V1(),
+            CLAIM_SECURITY_ATTRIBUTE_RELATIVE_V1,
+        )
+    ]
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_SYSTEM_RESOURCE_ATTRIBUTE_ACE, AceType=0x12)
+
+# [MS-DTYP] sect 2.4.4.16
+
+
+class WINNT_SYSTEM_SCOPED_POLICY_ID_ACE(Packet):
+    fields_desc = WINNT_ACCESS_ALLOWED_ACE.fields_desc
+
+
+bind_layers(WINNT_ACE_HEADER, WINNT_SYSTEM_SCOPED_POLICY_ID_ACE, AceType=0x13)
 
 # [MS-DTYP] sect 2.4.5
 
@@ -1212,28 +1521,28 @@ class SECURITY_DESCRIPTOR(_NTLMPayloadPacket):
             0x00,
             -16,
             [
-                "OwnerDefaulted",
-                "GroupDefaulted",
-                "DACLPresent",
-                "DACLDefaulted",
-                "SACLPresent",
-                "SACLDefaulted",
-                "DACLTrusted",
-                "ServerSecurity",
-                "DACLComputer",
-                "SACLComputer",
-                "DACLAutoInheriter",
-                "SACLAutoInherited",
-                "DACLProtected",
-                "SACLProtected",
-                "RMControlValid",
-                "SelfRelative",
+                "OWNER_DEFAULTED",
+                "GROUP_DEFAULTED",
+                "DACL_PRESENT",
+                "DACL_DEFAULTED",
+                "SACL_PRESENT",
+                "SACL_DEFAULTED",
+                "DACL_TRUSTED",
+                "SERVER_SECURITY",
+                "DACL_COMPUTED",
+                "SACL_COMPUTED",
+                "DACL_AUTO_INHERITED",
+                "SACL_AUTO_INHERITED",
+                "DACL_PROTECTED",
+                "SACL_PROTECTED",
+                "RM_CONTROL_VALID",
+                "SELF_RELATIVE",
             ],
         ),
         LEIntField("OwnerSidOffset", 0),
         LEIntField("GroupSidOffset", 0),
-        LEIntField("SaclOffset", 0),
-        LEIntField("DaclOffset", 0),
+        LEIntField("SACLOffset", 0),
+        LEIntField("DACLOffset", 0),
         _NTLMPayloadField(
             "Data",
             OFFSET,
@@ -1247,12 +1556,12 @@ class SECURITY_DESCRIPTOR(_NTLMPayloadPacket):
                     lambda pkt: pkt.GroupSidOffset,
                 ),
                 ConditionalField(
-                    PacketField("Sacl", WINNT_ACL(), WINNT_ACL),
-                    lambda pkt: pkt.Control.SACLPresent,
+                    PacketField("SACL", WINNT_ACL(), WINNT_ACL),
+                    lambda pkt: pkt.Control.SACL_PRESENT,
                 ),
                 ConditionalField(
-                    PacketField("Dacl", WINNT_ACL(), WINNT_ACL),
-                    lambda pkt: pkt.Control.DACLPresent,
+                    PacketField("DACL", WINNT_ACL(), WINNT_ACL),
+                    lambda pkt: pkt.Control.DACL_PRESENT,
                 ),
             ],
             offset_name="Offset",
