a4c6e262c923efaf3f5b53a1fdb70064021e27c4
Antonio Vazquez <antoniovazquezblanco@gmail.com>
2025-02-03 02:06:05 +0100

    bluetooth: Add new LE Meta subtype descriptions (#4622)

--- scapy/layers/bluetooth.py
+++ scapy/layers/bluetooth.py
@@ -2377,10 +2377,20 @@ class HCI_Event_LE_Meta(Packet):
     """
     name = "HCI_LE_Meta"
     fields_desc = [ByteEnumField("event", 0, {
-                   1: "connection_complete",
-                   2: "advertising_report",
-                   3: "connection_update_complete",
-                   5: "long_term_key_request",
+                   0x01: "connection_complete",
+                   0x02: "advertising_report",
+                   0x03: "connection_update_complete",
+                   0x04: "read_remote_features_page_0_complete",
+                   0x05: "long_term_key_request",
+                   0x06: "remote_connection_parameter_request",
+                   0x07: "data_length_change",
+                   0x08: "read_local_p256_public_key_complete",
+                   0x09: "generate_dhkey_complete",
+                   0x0a: "enhanced_connection_complete_v1",
+                   0x0b: "directed_advertising_report",
+                   0x0c: "phy_update_complete",
+                   0x0d: "extended_advertising_report",
+                   0x29: "enhanced_connection_complete_v2"
                    }), ]
 
     def answers(self, other):
@@ -2622,10 +2632,10 @@ bind_layers(HCI_Event_Command_Complete, HCI_Cmd_Complete_Read_Local_Extended_Fea
 bind_layers(HCI_Event_Command_Complete, HCI_Cmd_Complete_Read_BD_Addr, opcode=0x1009)  # noqa: E501
 bind_layers(HCI_Event_Command_Complete, HCI_Cmd_Complete_LE_Read_White_List_Size, opcode=0x200f)  # noqa: E501
 
-bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Connection_Complete, event=1)
-bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Advertising_Reports, event=2)
-bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Connection_Update_Complete, event=3)
-bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Long_Term_Key_Request, event=5)
+bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Connection_Complete, event=0x01)
+bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Advertising_Reports, event=0x02)
+bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Connection_Update_Complete, event=0x03)
+bind_layers(HCI_Event_LE_Meta, HCI_LE_Meta_Long_Term_Key_Request, event=0x05)
 
 bind_layers(EIR_Hdr, EIR_Flags, type=0x01)
 bind_layers(EIR_Hdr, EIR_IncompleteList16BitServiceUUIDs, type=0x02)
