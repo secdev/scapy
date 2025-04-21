4a449c97ed382da5f3317c64af609510b875619b
gpotter2 <10530980+gpotter2@users.noreply.github.com>
2025-04-21 22:00:27 +0200

    Try to fix LDAP test

--- .config/ci/openldap/install.sh
+++ .config/ci/openldap/install.sh
@@ -15,18 +15,30 @@ sudo debconf-set-selections <<< 'slapd slapd/domain string scapy.net'
 sudo apt-get -qy install slapd
 
 # Enable LDAPs
+echo "Enabling HTTPS on slapd..."
 sudo sed -i '/^SLAPD_SERVICES/ c\SLAPD_SERVICES="ldap:/// ldapi:/// ldaps://"' /etc/default/slapd
-sudo service slapd restart
+sudo systemctl restart slapd
+
+# Calculate the paths we're going to need.
+CUR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+PKIPATH=$(realpath "$CUR/../../../test/scapy/layers/tls/pki")
+OLDAPPATH=$(mktemp -d -t scapy_openldap_XXXX)
+
+# Copy certificates to temp path
+cp ${PKIPATH}/ca_cert.pem ${OLDAPPATH}
+cp ${PKIPATH}/srv_cert.pem ${OLDAPPATH}
+cp ${PKIPATH}/srv_key.pem ${OLDAPPATH}
+chmod a+rx -R ${OLDAPPATH}
 
 # Copy config template and replace variables.
-CUR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-openldap_conf=$(mktemp /tmp/scapy_openldapconf_XXXXXX.ldif)
-pkipath=$(realpath "$CUR/../../../test/scapy/layers/tls/pki")
+echo "Creating OpenLDAP config..."
+openldap_conf=${OLDAPPATH}/openldap_config.ldif
 cp $CUR/config.ldif $openldap_conf
-sed -i "s@{{CAFILE}}@${pkipath}/ca_cert.pem@g" $openldap_conf
-sed -i "s@{{CRTFILE}}@${pkipath}/srv_cert.pem@g" $openldap_conf
-sed -i "s@{{KEYFILE}}@${pkipath}/srv_key.pem@g" $openldap_conf
-echo "Temporary config stored in $openldap_conf"
+sed -i "s@{{CAFILE}}@${OLDAPPATH}/ca_cert.pem@g" $openldap_conf
+sed -i "s@{{CRTFILE}}@${OLDAPPATH}/srv_cert.pem@g" $openldap_conf
+sed -i "s@{{KEYFILE}}@${OLDAPPATH}/srv_key.pem@g" $openldap_conf
 
+echo "Applying OpenLDAP config..."
 sudo ldapmodify -Y EXTERNAL -H "ldapi:///" -w Bonjour1 -f $openldap_conf -c
+echo "Adding initial dummy data..."
 sudo ldapadd    -D "cn=admin,dc=scapy,dc=net" -w Bonjour1 -H "ldapi:///" -f $CUR/testdata.ldif -c
