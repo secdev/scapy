965f7ec3cf62b14357485096bd4bb8da0c13f98b
Jackson Sippe <jackson.sippe@colorado.edu>
2025-06-11 12:21:15 -0600

    Refactor QUIC into dedicated directory.

---
+++ scapy/layers/quic/all.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# This file is part of Scapy
+# See https://scapy.net/ for more information
+
+"""
+Aggregate top level objects from all QUIC modules.
+"""
+
+from scapy.layers.quic.quic import *
