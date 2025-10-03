secdev/scapy

commit 90c5ff4e514627594644b5aac0d58c38b0c90f20

Remove duplicate arguments in call to dns_resolve() (#4847)
Co-authored-by: Xavier Mehrenberger <xavier.mehrenberger@ssi.gouv.fr>

scapy/layers/dns.py
@@ -1418,7 +1418,7 @@ def dns_resolve(qname, qtype="A", raw=False, tcp=False, verbose=1, timeout=3, **
         return result
 
     kwargs.setdefault("timeout", timeout)
-    kwargs.setdefault("verbose", 0)
+    kwargs.setdefault("verbose", verbose)
     res = None
     for nameserver in conf.nameservers:
         # Try all nameservers
@@ -1455,8 +1455,6 @@ def dns_resolve(qname, qtype="A", raw=False, tcp=False, verbose=1, timeout=3, **
                         qtype=qtype,
                         raw=raw,
                         tcp=True,
-                        verbose=verbose,
-                        timeout=timeout,
                         **kwargs,
                     )
                 elif verbose:

commit 9aff3e262bda42a90cceff00e3068c770255f3f2

Fix regression caused by #4847

scapy/layers/dns.py
@@ -1418,7 +1418,7 @@ def dns_resolve(qname, qtype="A", raw=False, tcp=False, verbose=1, timeout=3, **
         return result
 
     kwargs.setdefault("timeout", timeout)
-    kwargs.setdefault("verbose", verbose)
+    kwargs.setdefault("verbose", 0)  # hide sr1() output
     res = None
     for nameserver in conf.nameservers:
         # Try all nameservers