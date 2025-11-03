7877e76d2dd3f87fbc26ba611658f25c305e97b4
Ebrix <Ebrix@github.com>
2025-11-03 08:49:18 +0100

    import

--- scapy/layers/windows/__init__.py
+++ scapy/layers/windows/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # This file is part of Scapy
 # See https://scapy.net/ for more information
-# Copyright (C) Philippe Biondi <phil@secdev.org>
+# Copyright (C) Gabriel Potter
 
 """
 This package implements Windows-specific layers helpers.
