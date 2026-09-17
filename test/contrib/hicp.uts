secdev/scapy

commit 48f03ffb66e0d7acd3cccdbe861159a4c46340de

contrib/hicp: terminate Configure fields on the correct delimiter (#5100)

scapy/contrib/hicp.py
@@ -98,7 +98,7 @@ class HICPConfigure(Packet):
         return "".join(p).encode('utf-8') + b"\x00" + pay
 
     def do_dissect(self, s):
-        res = match(".*: ([^;]+);", s.decode('utf-8'))
+        res = match(": ([^;]+);", s.decode('utf-8'))
         if res:
             self.target = TO_MACFIELD(res.group(1))
         s = s[len(self.target) + 3:]

test/contrib/hicp.uts
@@ -75,6 +75,10 @@ assert(pkt.target == "aa:aa:aa:aa:aa:aa")
 assert(pkt.ip_address == "255.255.255.255")
 assert(pkt.hostname == b"llama")
 
+= Keep a later value delimiter out of the target field
+pkt = HICP(b"Configure: aa-aa-aa-aa-aa-aa;HN = llama: one;\x00")
+assert(pkt.target == "aa:aa:aa:aa:aa:aa")
+
 + HICP Configure response
 = Build and dissect successful response to configure request
 

commit 28b2313ed301e9fc1f6fb8cb9cb4f18e74626c07

contrib/hicp: match the Configure target with or without the command prefix
Fixes a regression introduced in #5100.

AI-Assisted: yes (gpt-5.6-sol)

scapy/contrib/hicp.py
@@ -98,7 +98,7 @@ class HICPConfigure(Packet):
         return "".join(p).encode('utf-8') + b"\x00" + pay
 
     def do_dissect(self, s):
-        res = match(": ([^;]+);", s.decode('utf-8'))
+        res = match("[^:;]*: ([^;]+);", s.decode('utf-8'))
         if res:
             self.target = TO_MACFIELD(res.group(1))
         s = s[len(self.target) + 3:]

test/contrib/hicp.uts
@@ -79,6 +79,12 @@ assert(pkt.hostname == b"llama")
 pkt = HICP(b"Configure: aa-aa-aa-aa-aa-aa;HN = llama: one;\x00")
 assert(pkt.target == "aa:aa:aa:aa:aa:aa")
 
+= Dissect a Configure target with and without the command prefix
+target = "aa:bb:cc:dd:ee:ff"
+message = raw(HICPConfigure(target=target, hostname="llama"))
+assert(HICP(message).target == target)
+assert(HICPConfigure(message).target == target)
+
 + HICP Configure response
 = Build and dissect successful response to configure request
 